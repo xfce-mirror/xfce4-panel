2325b2b74cd5b34b06411be51ddcc86dcc80d132
Jasper Huijsmans <jbhuijsmans@home.nl>
2003-03-19 07:12:04 +0000

    Only add extra autogen parameters when none are given
    (Old svn revision: 3592)

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+if test -z "$*"; then
+  echo "**Message**: I am going to add --enable-maintainer-mode to \`configure'."
+  echo "If you wish to pass any other to it, please specify them on the"
+  echo \`$0\'" command line."
+  echo
+
+  conf_flags="--enable-maintainer-mode"
+else
+  unset conf_flags
+fi
+
 libtoolize --copy --force \
   && aclocal --verbose \
   && autoheader \
   && automake --add-missing --copy --include-deps --foreign --gnu --verbose \
   && autoconf \
-  && ./configure $@
+  && ./configure $conf_flags $@
