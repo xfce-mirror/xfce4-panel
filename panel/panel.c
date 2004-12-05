735881071fa667c73091863e709dc057a98660ad
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2004-12-05 23:38:11 +0000

    Changing back to gtk_window_set_type_hint() Set property before the window is realized/mapped (as stated in gtk documentation "This function should be called before the window becomes visible")
    (Old svn revision: 4241)

--- panel/panel.c
+++ panel/panel.c
@@ -1026,7 +1026,7 @@ set_panel_window_properties (Panel * p)
 {
     GtkWindow *window = GTK_WINDOW (p->toplevel);
     
-    netk_gtk_window_set_dock_type (window);
+    gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_DOCK);
 
     gtk_window_set_decorated (window, FALSE);
     gtk_window_set_resizable (window, FALSE);
@@ -1048,8 +1048,7 @@ create_panel_window (Panel * p)
 
     gtk_window_set_title (window, _("Xfce Panel"));
     gtk_window_stick (window);
-    netk_gtk_window_set_dock_type (window);
-
+    gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_DOCK);
 
     pb = get_panel_pixbuf ();
     gtk_window_set_icon (window, pb);
@@ -1160,12 +1159,13 @@ create_panel (void)
      * This function creates the panel items and popup menus */
     get_panel_config ();
 
+    set_panel_window_properties (p);
+
     if (!GTK_WIDGET_REALIZED (p->toplevel))
-	gtk_widget_realize (p->toplevel);
+        gtk_widget_realize (p->toplevel);
 
     p->priv->is_created = TRUE;
 
-    set_panel_window_properties (p);
     
     panel_set_position (p);
 
