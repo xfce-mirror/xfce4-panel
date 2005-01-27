6885d5694531d338b2d679d70986cd9b747671b1
Jasper Huijsmans <jbhuijsmans@home.nl>
2005-01-27 14:54:17 +0000

    Use intltool for .desktop files.
    (Old svn revision: 4313)

--- autoclean.sh
+++ autoclean.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+(type xdt-autogen) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autoclean.sh: You don't seem to have the Xfce development tools installed on
+              your system, which are required to build this software.
+              Please install the xfce4-dev-tools package first, it is 
+	      available from http://www.xfce.org/.
+EOF
+  exit 1
+}
 
-make distclean
-
-echo -n "Cleaning generated files..."
-rm -f config.* configure aclocal.m4
-rm -f compile depcomp ltmain.sh missing install-sh
-rm -f po/*.gmo
-rm -f $(find -name Makefile.in) po/Makefile.in.in
-echo "done."
-
+exec xdt-autogen clean
