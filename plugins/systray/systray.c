241dfe53110a334f92e79ba83c6028015c5b33d7
Jasper Huijsmans <jbhuijsmans@home.nl>
2005-05-14 09:48:32 +0000

    Add (optional) time to sysytray, similar to the taskbar. XEMBED icons still don't play nice with resizing/moving :(
    (Old svn revision: 14303)

--- plugins/systray/systray.c
+++ plugins/systray/systray.c
@@ -22,6 +22,7 @@
 #include <config.h>
 
 #include <stdio.h>
+#include <time.h>
 #include <gtk/gtk.h>
 
 #include <libxfce4util/libxfce4util.h>
@@ -31,26 +32,48 @@
 #include <panel/settings.h>
 #include <panel/plugins.h>
 
-#define BORDER 6
+#define BORDER 8
 
 typedef struct
 {
-    XfceSystemTray *tray;
-    gboolean tray_registered;
-
     int orientation;
     int size;
     
-    GdkScreen *screen;
-    
     GtkWidget *eventbox;
     GtkWidget *frame;
+    GtkWidget *box;
+    
+    /* systray */
+    GdkScreen *screen;
+    XfceSystemTray *tray;
+    gboolean tray_registered;
+
     GtkWidget *iconbox;
+
+    /* time */
+    GtkTooltips *timetips;
+    GtkWidget *timebox;
+    GtkWidget *timelabel;
+    int time_id;
+    
+    gboolean show_time;
+    gboolean before_icons;
+    
+    int mday;
+    int min;
 }
 Systray;
 
 /* border width */
 static int systray_border [] = { 1, 2, 3, 4 };
+
+
+/* prototypes */
+static GtkWidget *systray_create_options_widget (Systray *systray);
+
+static void systray_read_xml (Systray *systray, xmlNodePtr node);
+
+static void systray_write_xml (Systray *systray, xmlNodePtr node);
 
 
 /* tray */
@@ -82,10 +105,11 @@ icon_docked (XfceSystemTray * tray, GtkWidget * icon, Systray * systray)
 {
     if (systray->tray_registered)
     {
+        gtk_widget_hide (systray->iconbox);
         gtk_box_pack_start (GTK_BOX (systray->iconbox), icon, 
                             FALSE, FALSE, 0);
         gtk_widget_show (icon);
-        gtk_widget_queue_resize (icon);
+        gtk_widget_show (systray->iconbox);
     }
 }
 
@@ -94,7 +118,9 @@ icon_undocked (XfceSystemTray * tray, GtkWidget * icon, Systray *systray)
 {
     if (systray->tray_registered)
     {
+        gtk_widget_hide (systray->iconbox);
         gtk_container_remove (GTK_CONTAINER (systray->iconbox), icon);
+        gtk_widget_show (systray->iconbox);
     }
 }
 
@@ -124,6 +150,131 @@ systray_stop (Systray *systray)
     }
 }
 
+/* time */
+static gboolean
+time_timeout (Systray *systray)
+{
+    time_t ticks;
+    struct tm *tm;
+    char date_s[255];
+
+    ticks = time (0);
+    tm = localtime (&ticks);
+
+    if (tm->tm_mday != systray->mday)
+    {
+        systray->mday = tm->tm_mday;
+        /* Use format characters from strftime(3)
+         * to get the proper string for your locale.
+         * I used these:
+         * %A  : full weekday name
+         * %d  : day of the month
+         * %B  : full month name
+         * %Y  : four digit year
+         */
+        strftime (date_s, 255, _("%A %d %B %Y"), tm);
+
+        /* Conversion to utf8
+         * patch by Oliver M. Bolzer <oliver@fakeroot.net>
+         */
+        if (!g_utf8_validate (date_s, -1, NULL))
+        {
+            char *utf8date;
+            
+            utf8date = g_locale_to_utf8 (date_s, -1, NULL, NULL, NULL);
+            gtk_tooltips_set_tip (systray->timetips, systray->timebox, 
+                                  utf8date, NULL);
+            g_free (utf8date);
+        }
+        else
+        {
+            gtk_tooltips_set_tip (systray->timetips, systray->timebox, 
+                                  date_s, NULL);
+        }
+    }
+
+    if (tm->tm_min != systray->min)
+    {
+        systray->min = tm->tm_min;
+        
+        strftime (date_s, 255, _("%H:%M"), tm);
+
+        if (!g_utf8_validate (date_s, -1, NULL))
+        {
+            char *utf8date;
+            
+            utf8date = g_locale_to_utf8 (date_s, -1, NULL, NULL, NULL);
+            gtk_label_set_text (GTK_LABEL (systray->timelabel), utf8date);
+            g_free (utf8date);
+        }
+        else
+        {
+            gtk_label_set_text (GTK_LABEL (systray->timelabel), date_s);
+        }
+    }
+    
+    return TRUE;
+}
+
+static void
+systray_set_show_time (Systray *systray, gboolean show)
+{
+    if (show == systray->show_time)
+        return;
+
+    systray->show_time = show;
+    
+    if (show)
+    {
+        if (!systray->timetips)
+            systray->timetips = gtk_tooltips_new ();
+        
+        systray->timebox = gtk_event_box_new ();
+        gtk_widget_show (systray->timebox);
+        gtk_box_pack_start (GTK_BOX (systray->box), systray->timebox,
+                            FALSE, FALSE, 0);
+        if (systray->before_icons)
+            gtk_box_reorder_child (GTK_BOX (systray->box), 
+                                   systray->timebox, 0);
+        
+        systray->timelabel = gtk_label_new (NULL);
+        gtk_widget_show (systray->timelabel);
+        gtk_container_add (GTK_CONTAINER (systray->timebox), 
+                           systray->timelabel);
+
+        systray->mday = -1;
+        systray->min = -1;
+
+        time_timeout (systray);
+        systray->time_id = g_timeout_add (1000, (GSourceFunc) time_timeout,
+                                          systray);
+    }
+    else
+    {
+        g_source_remove (systray->time_id);
+        systray->time_id = 0;
+
+        gtk_widget_destroy (systray->timebox);
+        systray->timebox = NULL;
+        systray->timelabel = NULL;
+    }
+}
+
+static void
+systray_set_before_icons (Systray *systray, gboolean before_icons)
+{
+    if (before_icons == systray->before_icons)
+        return;
+
+    systray->before_icons = before_icons;
+
+    if (systray->show_time)
+    {
+        gtk_box_reorder_child (GTK_BOX (systray->box), systray->timebox,
+                               before_icons ? 0 : 2);
+    }
+}
+
 /* plugin */
 static void
 systray_set_size (Control *control, int size)
@@ -142,12 +293,12 @@ systray_set_size (Control *control, int size)
         
         if (systray->orientation == HORIZONTAL)
         {
-            gtk_widget_set_size_request (systray->iconbox,
+            gtk_widget_set_size_request (systray->eventbox,
                                          -1, icon_size [size] + border_width);
         }
         else
         {
-            gtk_widget_set_size_request (systray->iconbox,
+            gtk_widget_set_size_request (systray->eventbox,
                                          icon_size [size] + border_width, -1);
         }
     }
@@ -166,15 +317,47 @@ systray_set_orientation (Control *control, int orientation)
 
         systray_stop (systray);
 
-        gtk_widget_destroy (systray->iconbox);
+        if (systray->show_time)
+        {
+            g_object_ref (systray->timebox);
+            gtk_container_remove (GTK_CONTAINER (systray->box), 
+                                  systray->timebox);
+        }
+        
+        gtk_widget_destroy (systray->box);
 
         if (orientation == HORIZONTAL)
+        {
+            systray->box = gtk_hbox_new (FALSE, 0);
             systray->iconbox = gtk_hbox_new (TRUE, 3);
+        }
         else
+        {
+            systray->box = gtk_hbox_new (FALSE, 0);
             systray->iconbox = gtk_vbox_new (TRUE, 3);
+        }
 
+        gtk_widget_show (systray->box);
         gtk_widget_show (systray->iconbox);
-        gtk_container_add (GTK_CONTAINER (systray->frame), systray->iconbox);
+
+        gtk_container_add (GTK_CONTAINER (systray->frame), systray->box);
+        
+        if (systray->show_time && systray->before_icons)
+        {
+            gtk_box_pack_start (GTK_BOX (systray->box), systray->timebox,
+                                FALSE, FALSE, 0);
+            g_object_unref (systray->timebox);
+        }
+
+        gtk_box_pack_start (GTK_BOX (systray->box), systray->iconbox,
+                            TRUE, TRUE, 0);
+        
+        if (systray->show_time && !systray->before_icons)
+        {
+            gtk_box_pack_start (GTK_BOX (systray->box), systray->timebox,
+                                FALSE, FALSE, 0);
+            g_object_unref (systray->timebox);
+        }
 
         /* force update */
         systray->size = -1;
@@ -182,6 +365,31 @@ systray_set_orientation (Control *control, int orientation)
 
         systray_start (systray);
     }
+}
+
+static void
+systray_create_options (Control *control, GtkContainer *container, 
+                        GtkWidget *close)
+{
+    Systray *systray = control->data;
+
+    gtk_container_add (container, systray_create_options_widget (systray));
+}
+
+static void
+systray_read_config (Control *control, xmlNodePtr node)
+{
+    Systray *systray = control->data;
+
+    systray_read_xml (systray, node);
+}
+
+static void
+systray_write_config (Control *control, xmlNodePtr node)
+{
+    Systray *systray = control->data;
+
+    systray_write_xml (systray, node);
 }
 
 static void
@@ -219,12 +427,21 @@ systray_new (void)
                                     systray_border [systray->size]);
     gtk_container_add (GTK_CONTAINER (systray->eventbox), systray->frame);
     
+    /* systray */
+    systray->box = gtk_hbox_new (FALSE, 0);
+    gtk_widget_show (systray->box);
+    gtk_container_set_border_width (GTK_CONTAINER (systray->box), 
+                                    systray_border [systray->size]);
+    gtk_container_add (GTK_CONTAINER (systray->frame), systray->box);
+    
     systray->iconbox = gtk_hbox_new (TRUE, 3);
     gtk_widget_show (systray->iconbox);
-    gtk_container_set_border_width (GTK_CONTAINER (systray->iconbox), 
-                                    systray_border [systray->size]);
-    gtk_container_add (GTK_CONTAINER (systray->frame), systray->iconbox);
+    gtk_box_pack_start (GTK_BOX (systray->box), systray->iconbox, 
+                        TRUE, TRUE, 0);
 
+    /* time: off by default */
+    systray->show_time = FALSE;
+    
     g_signal_connect (systray->tray, "icon_docked", G_CALLBACK (icon_docked),
                       systray);
     
@@ -246,6 +463,8 @@ systray_free (Control * control)
 
     /* free resources, stop timeouts, etc. */
     systray_stop (systray);
+
+    systray_set_show_time (systray, FALSE);
 
     /* don't forget to free the memory for the Systray struct itself */
     g_free (systray);
@@ -270,15 +489,20 @@ xfce_control_class_init (ControlClass * cc)
     xfce_textdomain (GETTEXT_PACKAGE, LOCALEDIR, "UTF-8");
 
     cc->name = "systray";
-    cc->caption = _("Notification Area");
+    cc->caption = _("Status Area");
 
     cc->create_control = (CreateControlFunc) create_systray_control;
 
     cc->attach_callback = systray_attach_callback;
     cc->free = systray_free;
 
+    cc->read_config = systray_read_config;
+    cc->write_config = systray_write_config;
+    
     cc->set_size = systray_set_size;
     cc->set_orientation = systray_set_orientation;
+
+    cc->create_options = systray_create_options;
 
     /* Additional API calls */
     control_class_set_unique (cc, TRUE);
@@ -286,3 +510,98 @@ xfce_control_class_init (ControlClass * cc)
 
 /* Macro that checks panel API version */
 XFCE_PLUGIN_CHECK_INIT
+
+/* configuration */
+static void
+toggle_show_time (GtkToggleButton *b, Systray *systray)
+{
+    systray_set_show_time (systray, gtk_toggle_button_get_active (b));
+}
+
+static void
+toggle_before_icons (GtkToggleButton *b, Systray *systray)
+{
+    systray_set_before_icons (systray, gtk_toggle_button_get_active (b));
+}
+
+static GtkWidget *
+systray_create_options_widget (Systray *systray)
+{
+    GtkWidget *vbox, *button, *hbox, *align;
+    
+    vbox = gtk_vbox_new (FALSE, BORDER);
+    gtk_widget_show (vbox);
+
+    button = gtk_check_button_new_with_mnemonic (_("Show _time"));
+    gtk_widget_show (button);
+    gtk_box_pack_start (GTK_BOX (vbox), button, FALSE, FALSE, 0);
+    
+    if (systray->show_time)
+        gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), TRUE);
+
+    g_signal_connect (button, "toggled", G_CALLBACK (toggle_show_time),
+                      systray);
+    
+    hbox = gtk_hbox_new (FALSE, 0);
+    gtk_widget_show (hbox);
+    gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
+    
+    align = gtk_alignment_new (0,0,0,0);
+    gtk_widget_set_size_request (align, 2 * BORDER, -1);
+    gtk_widget_show (align);
+    gtk_box_pack_start (GTK_BOX (hbox), align, FALSE, FALSE, 0);
+    
+    button = 
+        gtk_check_button_new_with_mnemonic (_("Display time before icons"));
+    gtk_widget_show (button);
+    gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, FALSE, 0);
+    
+    if (systray->before_icons)
+        gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), TRUE);
+
+    g_signal_connect (button, "toggled", G_CALLBACK (toggle_before_icons), 
+                      systray);
+    
+    return vbox;
+}
+
+static void 
+systray_read_xml (Systray *systray, xmlNodePtr node)
+{
+    xmlChar *value;
+    int n;
+    
+    if (!node)
+        return;
+
+    if ((value = xmlGetProp (node, "before")))
+    {
+        n = (int) strtol (value, NULL, 0);
+
+        systray_set_before_icons (systray, (n == 1));
+        
+        g_free (value);
+    }
+
+    if ((value = xmlGetProp (node, "time")))
+    {
+        n = (int) strtol (value, NULL, 0);
+
+        systray_set_show_time (systray, (n == 1));
+        
+        g_free (value);
+    }
+}
+
+static void 
+systray_write_xml (Systray *systray, xmlNodePtr node)
+{
+    char value[2];
+    
+    snprintf (value, 2, "%d", systray->show_time);
+    xmlSetProp (node, "time", value);
+    
+    snprintf (value, 2, "%d", systray->before_icons);
+    xmlSetProp (node, "before", value);
+}
+
