b84a2a0d931249e24623b9f315b2c29b5972b956
Nick Schermer <nick@xfce.org>
2007-03-27 12:03:06 +0000

    * config/systray-4.rc, config/panels.xml: Show system tray border and increase panel size 	  to 30px, so people can actually see the system tray. 	* panel/panel-properties.c: Allow freely movable panels on the screen edges (bug #3039).
    (Old svn revision: 25299)

--- panel/panel-properties.c
+++ panel/panel-properties.c
@@ -1000,7 +1000,7 @@ panel_init_position (Panel *panel)
     x = xmon->geometry.x;
     y = xmon->geometry.y;
 
-    if (priv->xoffset > 0 && priv->yoffset > 0)
+    if (priv->xoffset > -1 && priv->yoffset > -1)
     {
         x += priv->xoffset;
         y += priv->yoffset;
