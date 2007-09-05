f2dbae2a8112cd92e7887fb0277e8bdf8b5e6645
Nick Schermer <nick@xfce.org>
2007-09-05 11:55:04 +0000

    * Same fix as in xfce_4_4 branch.
    (Old svn revision: 26047)

--- panel/panel-dialogs.c
+++ panel/panel-dialogs.c
@@ -1535,7 +1535,7 @@ panel_selected (GtkComboBox        *combo,
 {
     gint n = gtk_combo_box_get_active (combo);
 
-    if (n == pmd->current)
+    if (n == pmd->current || n < 0)
         return;
 
     pmd->current = n;
