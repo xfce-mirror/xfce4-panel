ebbd829731b92f48e0c7cde6f8f09f86e527b7a6
Nick Schermer <nick@xfce.org>
2010-02-26 14:08:11 +0100

    Use the arrow button instead of the blink button.

--- plugins/tasklist/tasklist-widget.c
+++ plugins/tasklist/tasklist-widget.c
@@ -29,7 +29,6 @@
 #include <exo/exo.h>
 #include <libwnck/libwnck.h>
 #include <libxfce4panel/libxfce4panel.h>
-#include <libxfce4panel/xfce-blink-button.h>
 #include <common/panel-private.h>
 
 #include "tasklist-widget.h"
@@ -1248,7 +1247,7 @@ tasklist_button_state_changed (WnckWindow        *window,
   /* update the blinking state */
   if (PANEL_HAS_FLAG (changed_state, WNCK_WINDOW_STATE_DEMANDS_ATTENTION)
       || PANEL_HAS_FLAG (changed_state, WNCK_WINDOW_STATE_URGENT))
-    xfce_blink_button_set_blinking (XFCE_BLINK_BUTTON (child->button),
+    xfce_arrow_button_set_blinking (XFCE_ARROW_BUTTON (child->button),
         wnck_window_or_transient_needs_attention (child->window));
 }
 
@@ -1400,7 +1399,7 @@ tasklist_button_new (XfceTasklistChild *child)
   WnckWindow *window = child->window;
 
   /* create the application button */
-  child->button = xfce_blink_button_new ();
+  child->button = xfce_arrow_button_new (GTK_ARROW_NONE);
   gtk_button_set_relief (GTK_BUTTON (child->button),
                          child->tasklist->button_relief);
   g_signal_connect (G_OBJECT (child->button), "toggled",
