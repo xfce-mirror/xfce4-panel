355bb03adfee7f1c06ce7bbdaf28ec37d1b66ffe
Nick Schermer <nick@xfce.org>
2010-02-26 14:08:21 +0100

    Use the new developer tools macros.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #
-# $Id: autogen.sh 25025 2007-02-24 09:54:07Z jasper $
-#
-# Copyright (c) 2002-2005
+# Copyright (c) 2002-2009
 #         The Xfce development team. All rights reserved.
 #
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>.
@@ -18,34 +16,4 @@ EOF
   exit 1
 }
 
-# verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
-
-# substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
-if test -d .git/svn; then
- revision=`LC_ALL=C git-svn find-rev HEAD`
-elif test -d .git; then
- revision=`LC_ALL=C git rev-parse --short HEAD`
-else
- revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n",
-$2}'`
-fi
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-   < "configure.in.in" > "configure.in"
-
-xdt-autogen $@
-
-# xdt-autogen clean does not remove all generated files
-(test x"clean" = x"$1") && {
-  rm -f configure.in
-  rm -f INSTALL
-} || true
-# vi:set ts=2 sw=2 et ai:
+XDT_AUTOGEN_REQUIRED_VERSION="4.7.2" exec xdt-autogen $@
