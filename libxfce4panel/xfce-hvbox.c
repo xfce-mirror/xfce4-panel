1f112bfe7b443dce7f62536959bc4b8458941a4d
Nick Schermer <nick@xfce.org>
2010-02-26 14:08:05 +0100

    License updates.

--- libxfce4panel/xfce-hvbox.c
+++ libxfce4panel/xfce-hvbox.c
@@ -1,23 +1,25 @@
 /* $Id$ */
 /*
- *  OBox Copyright (c) 2002      Red Hat Inc. based on GtkHBox
- *  Copyright      (c) 2006      Jani Monoses <jani@ubuntu.com>
- *  Copyright      (c) 2006-2007 Jasper Huijsmans <jasper@xfce.org>
+ * This file is partly based on OBox
+ * Copyright (C) 2002      Red Hat Inc. based on GtkHBox
+ * 
+ * Copyright (C) 2006-2007 Jasper Huijsmans <jasper@xfce.org>
+ * Copyright (C) 2006      Jani Monoses <jani@ubuntu.com>
+ * Copyright (C) 2007-2008 Nick Schermer <nick@xfce.org>
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifdef HAVE_CONFIG_H
