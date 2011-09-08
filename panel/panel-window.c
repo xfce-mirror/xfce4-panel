4c571727ea14eb6fb21219b7a30c030d320025ae
Nick Schermer <nick@xfce.org>
2011-09-08 11:11:25 +0200

    Panel: Print string in case of an invalid position.
    (cherry picked from commit 48382773474583719b59753c3d4e3e74a346f449)

--- panel/panel-window.c
+++ panel/panel-window.c
@@ -638,7 +638,7 @@ panel_window_set_property (GObject      *object,
         }
       else
         {
-          g_message ("no valid position defined");
+          g_message ("no valid position defined: %s", val_string);
         }
       break;
 
