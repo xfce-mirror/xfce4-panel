6df7c6339ee5aba39a075dc8cf0a1ef21f689a87
Andrzej <ndrwrdck@gmail.com>
2012-02-06 11:57:04 +0900

    Launcher: small=FALSE in deskbar mode with label visible.
    Regression fix. This error has been triggered by commit:
    f362692 panel-itembar: Removed item size checks.
    
    This commit should be harmless. It can be used without commit f362692
    and may speed up itembar layout a bit.

--- plugins/launcher/launcher.c
+++ plugins/launcher/launcher.c
@@ -1709,10 +1709,18 @@ launcher_plugin_button_update (LauncherPlugin *plugin)
   if (G_LIKELY (plugin->items != NULL))
     item = GARCON_MENU_ITEM (plugin->items->data);
 
+  mode = xfce_panel_plugin_get_mode (XFCE_PANEL_PLUGIN (plugin));
+
+  /* disable the "small" property in the deskbar mode and the label visible */
+  if (G_UNLIKELY (plugin->show_label && mode == XFCE_PANEL_PLUGIN_MODE_DESKBAR))
+    xfce_panel_plugin_set_small (XFCE_PANEL_PLUGIN (plugin), FALSE);
+  else
+    xfce_panel_plugin_set_small (XFCE_PANEL_PLUGIN (plugin), TRUE);
+
   if (G_UNLIKELY (plugin->show_label))
     {
       panel_return_if_fail (GTK_IS_LABEL (plugin->child));
-      mode = xfce_panel_plugin_get_mode (XFCE_PANEL_PLUGIN (plugin));
+
       gtk_label_set_angle (GTK_LABEL (plugin->child),
                            (mode == XFCE_PANEL_PLUGIN_MODE_VERTICAL) ? 270 : 0);
       gtk_label_set_text (GTK_LABEL (plugin->child),
