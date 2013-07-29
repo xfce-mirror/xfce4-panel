db012e9468c61f60cb4e0f2a9a48d3d62f36560c
Nick Schermer <nick@xfce.org>
2013-07-29 10:20:12 +0200

    Directorymenu: Pass path to argument (bug #10270).

--- plugins/directorymenu/directorymenu.c
+++ plugins/directorymenu/directorymenu.c
@@ -709,7 +709,7 @@ directory_menu_plugin_menu_open (GtkWidget   *mi,
     }
 
   if (!result
-      && !exo_execute_preferred_application_on_screen (category, NULL, working_dir, NULL,
+      && !exo_execute_preferred_application_on_screen (category, working_dir, working_dir, NULL,
                                                        gtk_widget_get_screen (mi), &error))
     {
       xfce_dialog_show_error (NULL, error,
