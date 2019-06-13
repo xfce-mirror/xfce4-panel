b77dcfd015ced279f34ec66d00380e00756ef970
Felipe Contreras <felipe.contreras@gmail.com>
2019-06-13 02:04:49 -0500

    clock: binary: add new binary time mode
    Typical binary clocks simply take the normal time and display it in
    binary, this provides no value.
    
    In binary bits should have meaning: the most significant bit should be
    significant. If a day is 0x10, half a day should be 0x8.
    
    If we divide the day in 16 equal parts that number can be represented
    with four bits, noon is 0b1000 and therefore everything after noon has
    the most significant bit on.
    
    Binary time divides the day in 2 ** 16 equal parts, with 4 bits we have
    a resolution of hours, and 8 bits a resolution of minutes (~6 minutes),
    for seconds we need all 16 bits.
    
    In binary time every bit is useful and it means something, now there's
    actually a reason for a binary clock.
    
    More explanation can be found by reading of hexadecimal time [1], which
    is another representation of the same thing.
    
    https://en.wikipedia.org/wiki/Hexadecimal_time
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- plugins/clock/clock-binary.c
+++ plugins/clock/clock-binary.c
@@ -64,7 +64,8 @@ enum
 enum
 {
   MODE_DECIMAL,
-  MODE_SEXADECIMAL
+  MODE_SEXADECIMAL,
+  MODE_BINARY_TIME
 };
 
 struct _XfceClockBinaryClass
@@ -131,7 +132,7 @@ xfce_clock_binary_class_init (XfceClockBinaryClass *klass)
   g_object_class_install_property (gobject_class,
                                    PROP_MODE,
                                    g_param_spec_uint ("binary-mode", NULL, NULL,
-                                                      MODE_DECIMAL, MODE_SEXADECIMAL, MODE_DECIMAL,
+                                                      MODE_DECIMAL, MODE_BINARY_TIME, MODE_DECIMAL,
                                                       G_PARAM_READWRITE
                                                       | G_PARAM_STATIC_STRINGS));
 
@@ -243,6 +244,9 @@ xfce_clock_binary_get_property (GObject    *object,
       case MODE_SEXADECIMAL:
         ratio = binary->show_seconds ? 2.0 : 3.0;
         break;
+      case MODE_BINARY_TIME:
+        ratio = binary->show_seconds ? 1.5 : 2.5;
+        break;
       default:
         return;
       }
@@ -328,6 +332,27 @@ xfce_clock_binary_algo_main (gulong          *table,
 
 
 
+static void
+xfce_clock_binary_algo_time (gulong    *table,
+                             GDateTime *time,
+                             gboolean   seconds,
+                             gint       rows,
+                             gint       cols)
+{
+  guint n;
+
+  n = g_date_time_get_hour (time) * 60 * 60 +
+    g_date_time_get_minute (time) * 60 +
+    g_date_time_get_second (time);
+
+  *table = (n * 512) / 675; // 2 ** 16 / (24 * 60 * 60)
+
+  if (!seconds)
+    *table >>= 8;
+}
+
+
+
 static gboolean
 xfce_clock_binary_draw (GtkWidget *widget,
                         cairo_t   *cr)
@@ -370,6 +395,10 @@ xfce_clock_binary_draw (GtkWidget *widget,
   case MODE_SEXADECIMAL:
     cols = 6;
     rows = binary->show_seconds ? 3 : 2;
+    break;
+  case MODE_BINARY_TIME:
+    cols = 4;
+    rows = binary->show_seconds ? 4 : 2;
     break;
   default:
     return FALSE;
@@ -420,6 +449,9 @@ xfce_clock_binary_draw (GtkWidget *widget,
     break;
   case MODE_SEXADECIMAL:
     xfce_clock_binary_algo_true (&table, time, binary->show_seconds, rows, cols);
+    break;
+  case MODE_BINARY_TIME:
+    xfce_clock_binary_algo_time (&table, time, binary->show_seconds, rows, cols);
     break;
   }
 
