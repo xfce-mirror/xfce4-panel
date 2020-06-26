261d58e4327d6e7cd9cf5ec2cceb0fc03e3b89ee
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-06-26 03:49:50 +0200

    Drop dead code

--- plugins/statusnotifier/systray.h
+++ plugins/statusnotifier/systray.h
@@ -30,16 +30,7 @@ typedef struct _SnPlugin      SnPlugin;
 typedef struct _SystrayChild       SystrayChild;
 typedef enum   _SystrayChildState  SystrayChildState;
 
-//#define XFCE_TYPE_SYSTRAY_PLUGIN            (systray_plugin_get_type ())
-//#define XFCE_SN_PLUGIN(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), XFCE_TYPE_SYSTRAY_PLUGIN, SnPlugin))
-//#define XFCE_SN_PLUGIN_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), XFCE_TYPE_SYSTRAY_PLUGIN, SnPluginClass))
-//#define XFCE_IS_SN_PLUGIN(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), XFCE_TYPE_SYSTRAY_PLUGIN))
-//#define XFCE_IS_SN_PLUGIN_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), XFCE_TYPE_SYSTRAY_PLUGIN))
-//#define XFCE_SN_PLUGIN_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), XFCE_TYPE_SYSTRAY_PLUGIN, SnPluginClass))
-
 GType systray_plugin_get_type      (void) G_GNUC_CONST;
-
-//void  systray_plugin_register_type    (XfcePanelTypeModule *type_module);
 
 void  systray_plugin_box_draw            (GtkWidget             *box,
                                           cairo_t               *cr,
