ab9a6157b0f5926cc09f6c6820e17fd3ee2fab37
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-06-26 03:54:11 +0200

    Hook up systray size changed signal

--- plugins/statusnotifier/sn-plugin.c
+++ plugins/statusnotifier/sn-plugin.c
@@ -153,6 +153,8 @@ sn_plugin_size_changed (XfcePanelPlugin *panel_plugin,
   SnPlugin *plugin = XFCE_SN_PLUGIN (panel_plugin);
 
   sn_config_set_size (plugin->config, size, xfce_panel_plugin_get_nrows (panel_plugin));
+  systray_plugin_size_changed (panel_plugin,
+                               xfce_panel_plugin_get_size (panel_plugin));
 
   return TRUE;
 }
