cc7bebf21198fa87e206c1052cc815f2dd6fcecf
Gaël Bonithon <gael@xfce.org>
2022-04-03 17:24:13 +0200

    Use GLIB_VERSION_MIN/MAX_REQUIRED/ALLOWED

--- plugins/systray/sn-item.c
+++ plugins/systray/sn-item.c
@@ -655,7 +655,9 @@ sn_item_extract_pixbuf (GVariant *variant)
                   if (array != NULL)
                     g_free (array);
 #if GLIB_CHECK_VERSION(2, 68, 0)
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
                   array = g_memdup2 (data, size);
+G_GNUC_END_IGNORE_DEPRECATIONS
 #else
                   array = g_memdup (data, size);
 #endif
