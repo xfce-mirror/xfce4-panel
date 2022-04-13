xfce-mirror/xfce4-panel

commit a24b0214c9218ab39581d0e40b1b441913d631f7

Add support for XRandR's primary monitor feature (Bug #9338)
If there is no output name set in xfconf it behaves like before,
i.e. "Automatic" mode.
However if the driver does not return a monitor name it now falls
back to the primary display.

panel/panel-window.c
@@ -2079,13 +2079,21 @@ panel_window_screen_layout_changed (GdkScreen   *screen,
     }
   else
     {
-      if (panel_str_is_empty (window->output_name))
+      if (g_strcmp0 (window->output_name, "Automatic") == 0
+          || window->output_name == NULL)
         {
-          normal_monitor_positioning:
-
           /* get the monitor geometry based on the panel position */
           monitor_num = gdk_screen_get_monitor_at_point (screen, window->base_x,
                                                          window->base_y);
+          gdk_screen_get_monitor_geometry (screen, monitor_num, &a);
+          panel_return_if_fail (a.width > 0 && a.height > 0);
+        }
+      else if (g_strcmp0 (window->output_name, "Primary") == 0)
+        {
+          normal_monitor_positioning:
+          /* get the primary monitor */
+          monitor_num = gdk_screen_get_primary_monitor (screen);
+
           gdk_screen_get_monitor_geometry (screen, monitor_num, &a);
           panel_return_if_fail (a.width > 0 && a.height > 0);
         }

panel/panel-preferences-dialog.c
@@ -490,8 +490,11 @@ panel_preferences_dialog_bindings_update (PanelPreferencesDialog *dialog)
       || !panel_str_is_empty (output_name))
     {
       gtk_list_store_insert_with_values (GTK_LIST_STORE (store), &iter, n++,
-                                         OUTPUT_NAME, NULL,
+                                         OUTPUT_NAME, "Automatic",
                                          OUTPUT_TITLE, _("Automatic"), -1);
+      gtk_list_store_insert_with_values (GTK_LIST_STORE (store), &iter, n++,
+                                         OUTPUT_NAME, "Primary",
+                                         OUTPUT_TITLE, _("Primary"), -1);
       if (panel_str_is_empty (output_name))
         {
           gtk_combo_box_set_active_iter  (GTK_COMBO_BOX (object), &iter);

commit 39a0154ad82d02dae355bf2e67f6049c1117c0b3

panel: Fix regression "'Span Monitor' has no effect"
Regression introduced in a24b0214c9218ab39581d0e40b1b441913d631f7,
where the string comparison with "Automatic" has been forgotten at this
place.

The feature could sometimes work despite this since then, because when
a new panel is created, the "output-name" property is left empty in
Xfconf, even though it appears as "Automatic" in the panel preferences
dialog. But as soon as the preference was changed, the feature was
broken.

Fixes #405.

panel/panel-window.c
@@ -2183,7 +2183,7 @@ panel_window_screen_layout_changed (GdkScreen   *screen,
                PANEL_DEBUG_BOOL (window->span_monitors),
                window->base_x, window->base_y);
 
-  if (window->output_name == NULL
+  if ((window->output_name == NULL || g_strcmp0 (window->output_name, "Automatic") == 0)
       && (window->span_monitors || n_monitors == 1))
     {
       /* get the screen geometry we also use this if there is only