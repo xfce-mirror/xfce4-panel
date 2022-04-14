xfce-mirror/xfce4-panel

commit 29a637fe2084799601ad1e431ccd7c670162eb92

intellihide: Check if cursor is over the panel (Fixes #311)

panel/panel-window.c
@@ -1018,7 +1018,7 @@ panel_window_leave_notify_event (GtkWidget        *widget,
 {
   PanelWindow *window = PANEL_WINDOW (widget);
 
-   /* queue an autohide timeout if needed */
+  /* queue an autohide timeout if needed */
   if (event->detail != GDK_NOTIFY_INFERIOR
       && window->autohide_state != AUTOHIDE_DISABLED
       && window->autohide_state != AUTOHIDE_BLOCKED)
@@ -2437,7 +2437,21 @@ panel_window_active_window_geometry_changed (WnckWindow  *active_window,
           if (window->autohide_state != AUTOHIDE_HIDDEN)
             {
               if (gdk_rectangle_intersect (&panel_area,  &window_area, NULL))
-                panel_window_autohide_queue (window, AUTOHIDE_HIDDEN);
+                {
+                  /* get the pointer position */
+                  GdkDisplay* display = gdk_display_get_default ();
+                  GdkSeat* seat = gdk_display_get_default_seat (display);
+                  GdkDevice* device = gdk_seat_get_pointer (seat);
+                  gint pointer_x, pointer_y;
+                  gdk_device_get_position (device, NULL, &pointer_x, &pointer_y);
+
+                  /* check if the cursor is outside the panel area before proceeding */
+                  if (pointer_x < panel_area.x
+                      || pointer_y < panel_area.y
+                      || pointer_x > panel_area.x + panel_area.width
+                      || pointer_y > panel_area.y + panel_area.height)
+                    panel_window_autohide_queue (window, AUTOHIDE_HIDDEN);
+                }
             }
           else
             {

commit 9976a45fa97353257a83ee95f9435b61549041a3

panel: Fix regression "intellihide does not hide when leaving slowly"
Regression introduced in 29a637fe2084799601ad1e431ccd7c670162eb92.
The "leave-notify-event" signal can be emitted when the pointer is on
the border of the panel.

Fixes #388, related to !7.

panel/panel-window.c
@@ -2471,10 +2471,10 @@ panel_window_active_window_geometry_changed (WnckWindow  *active_window,
                   gdk_device_get_position (device, NULL, &pointer_x, &pointer_y);
 
                   /* check if the cursor is outside the panel area before proceeding */
-                  if (pointer_x < panel_area.x
-                      || pointer_y < panel_area.y
-                      || pointer_x > panel_area.x + panel_area.width
-                      || pointer_y > panel_area.y + panel_area.height)
+                  if (pointer_x <= panel_area.x
+                      || pointer_y <= panel_area.y
+                      || pointer_x >= panel_area.x + panel_area.width
+                      || pointer_y >= panel_area.y + panel_area.height)
                     panel_window_autohide_queue (window, AUTOHIDE_HIDDEN);
                 }
             }