1b88e01ab76f4c97249e6157e5013e63c7d2885a
carlos-ramos-73 <carlos.a.ramos.73@gmail.com>
2022-05-05 14:59:16 -0700

    tasklist: Removed unneeded g_free calls
    Fixes: #166
    MR: !72

--- plugins/tasklist/tasklist-widget.c
+++ plugins/tasklist/tasklist-widget.c
@@ -3030,10 +3030,7 @@ xfce_tasklist_button_start_new_instance_clicked (GtkWidget         *widget,
   gchar *path = xfce_tasklist_button_get_child_path (child);
 
   if (path == NULL)
-    {
-      g_free (path);
-      return;
-    }
+    return;
 
   if (!g_spawn_command_line_async (path, &error))
     {
@@ -3070,10 +3067,7 @@ xfce_tasklist_button_add_launch_new_instance_item (XfceTasklistChild *child,
   path = xfce_tasklist_button_get_child_path (child);
 
   if (path == NULL)
-    {
-      g_free (path);
-      return;
-    }
+    return;
 
   sep = gtk_separator_menu_item_new ();
   gtk_widget_show (sep);
