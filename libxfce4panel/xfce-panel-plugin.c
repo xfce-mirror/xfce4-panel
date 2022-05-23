7a24ea0c1966ccdd07cd8774811fbafe694f028d
Gaël Bonithon <gael@xfce.org>
2022-05-23 12:14:27 +0200

    libxfce4panel: Add new function `xfce_panel_plugin_popup_menu()`
    MR: !80

--- libxfce4panel/xfce-panel-plugin.c
+++ libxfce4panel/xfce-panel-plugin.c
@@ -2730,6 +2730,93 @@ xfce_panel_plugin_position_menu (GtkMenu  *menu,
 
 
 /**
+ * xfce_panel_plugin_popup_menu:
+ * @plugin        : an #XfcePanelPlugin.
+ * @menu          : a #GtkMenu.
+ * @widget        : (allow-none): the #GtkWidget to align @menu with or %NULL
+ *                  to pop up @menu at pointer.
+ * @trigger_event : (allow-none): the #GdkEvent that initiated this request or
+ *                  %NULL if it's the current event.
+ *
+ * Pops up @menu at @widget if @widget is non-%NULL and if appropriate given
+ * the panel position, otherwise pops up @menu at pointer.
+ *
+ * As a convenience, xfce_panel_plugin_popup_menu() calls
+ * xfce_panel_plugin_register_menu() for the @menu.
+ *
+ * For a custom widget that will be used as a popup menu, use
+ * xfce_panel_plugin_position_widget() instead.
+ *
+ * See also: gtk_menu_popup_at_widget() and gtk_menu_popup_at_pointer().
+ *
+ * Since: 4.17.2
+ **/
+void
+xfce_panel_plugin_popup_menu (XfcePanelPlugin *plugin,
+                              GtkMenu         *menu,
+                              GtkWidget       *widget,
+                              const GdkEvent  *trigger_event)
+{
+  GdkGravity widget_anchor, menu_anchor;
+  gboolean   popup_at_widget = TRUE;
+
+  g_return_if_fail (XFCE_IS_PANEL_PLUGIN (plugin));
+  g_return_if_fail (GTK_IS_MENU (menu));
+
+  /* check if conditions are met to pop up menu at widget */
+  if (widget != NULL)
+    {
+      switch (plugin->priv->screen_position)
+        {
+          case XFCE_SCREEN_POSITION_NW_H:
+          case XFCE_SCREEN_POSITION_N:
+          case XFCE_SCREEN_POSITION_NE_H:
+            widget_anchor = GDK_GRAVITY_SOUTH_WEST;
+            menu_anchor = GDK_GRAVITY_NORTH_WEST;
+            break;
+
+          case XFCE_SCREEN_POSITION_NW_V:
+          case XFCE_SCREEN_POSITION_W:
+          case XFCE_SCREEN_POSITION_SW_V:
+            widget_anchor = GDK_GRAVITY_NORTH_EAST;
+            menu_anchor = GDK_GRAVITY_NORTH_WEST;
+            break;
+
+          case XFCE_SCREEN_POSITION_NE_V:
+          case XFCE_SCREEN_POSITION_E:
+          case XFCE_SCREEN_POSITION_SE_V:
+            widget_anchor = GDK_GRAVITY_NORTH_WEST;
+            menu_anchor = GDK_GRAVITY_NORTH_EAST;
+            break;
+
+          case XFCE_SCREEN_POSITION_SW_H:
+          case XFCE_SCREEN_POSITION_S:
+          case XFCE_SCREEN_POSITION_SE_H:
+            widget_anchor = GDK_GRAVITY_NORTH_WEST;
+            menu_anchor = GDK_GRAVITY_SOUTH_WEST;
+            break;
+
+          default:
+            popup_at_widget = FALSE;
+            break;
+        }
+    }
+  else
+    popup_at_widget = FALSE;
+
+  /* register the menu */
+  xfce_panel_plugin_register_menu (plugin, menu);
+
+  /* pop up the menu */
+  if (popup_at_widget)
+    gtk_menu_popup_at_widget (menu, widget, widget_anchor, menu_anchor, trigger_event);
+  else
+    gtk_menu_popup_at_pointer (menu, trigger_event);
+}
+
+
+
+/**
  * xfce_panel_plugin_focus_widget:
  * @plugin : an #XfcePanelPlugin.
  * @widget : a #GtkWidget inside the plugins that should be focussed.
