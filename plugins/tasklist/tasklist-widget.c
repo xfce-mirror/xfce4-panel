xfce-mirror/xfce4-panel

commit 8e36c4e463ea84ad1a465a93a0bdf29da53686f1

tasklist: Properly set window groups id
This allows window groups to be included in the timestamp sort,
identifying the group's timestamp with that of its most recent window.

Fixes: #126, #323
MR: !81

plugins/tasklist/tasklist-widget.c
@@ -4284,11 +4284,17 @@ xfce_tasklist_group_button_child_visible_changed (XfceTasklistChild *group_child
   panel_return_if_fail (XFCE_IS_TASKLIST (group_child->tasklist));
   panel_return_if_fail (group_child->tasklist->grouping != XFCE_TASKLIST_GROUPING_NEVER);
 
+  /* the group id is defined below as that of the last added window */
+  group_child->unique_id = 0;
+
   for (li = group_child->windows; li != NULL; li = li->next)
     {
       child = li->data;
       if (gtk_widget_get_visible (child->button))
-        visible_counter++;
+        {
+          visible_counter++;
+          group_child->unique_id = MAX (group_child->unique_id, child->unique_id);
+        }
     }
 
   if (visible_counter > 1)
@@ -4311,9 +4317,7 @@ xfce_tasklist_group_button_child_visible_changed (XfceTasklistChild *group_child
         child->type = type;
     }
 
-  gtk_widget_queue_resize (GTK_WIDGET (group_child->tasklist));
-
-  xfce_tasklist_group_button_name_changed (NULL, group_child);
+  xfce_tasklist_group_button_name_changed (group_child->class_group, group_child);
 }
 
 

commit dd202448e7d919c097f3845b2c1409ba87a4be24

tasklist: Delay sorting if window list is in use
Here when browsing the list to determine the visibility of window
buttons.

Regression introduced in 8e36c4e463ea84ad1a465a93a0bdf29da53686f1.

Fixes: #594

plugins/tasklist/tasklist-widget.c
@@ -238,6 +238,9 @@ struct _XfceTasklistChild
    * simply increased for each new button */
   guint                   unique_id;
 
+  /* delay sorting so as not to change window list if it is in use */
+  guint                   sort_idle_id;
+
   /* last time this window was focused */
   gint64                  last_focused;
 
@@ -4283,6 +4286,22 @@ xfce_tasklist_group_button_remove (XfceTasklistChild *group_child)
   /* destroy the button, this will free the remaining child
    * data in the container remove function */
   gtk_widget_destroy (group_child->button);
+
+  if (group_child->sort_idle_id != 0)
+    g_source_remove (group_child->sort_idle_id);
+}
+
+
+
+static gboolean
+xfce_tasklist_group_button_name_changed_idle (gpointer data)
+{
+  XfceTasklistChild *group_child = data;
+
+  xfce_tasklist_group_button_name_changed (group_child->class_group, group_child);
+  group_child->sort_idle_id = 0;
+
+  return FALSE;
 }
 
 
@@ -4333,7 +4352,9 @@ xfce_tasklist_group_button_child_visible_changed (XfceTasklistChild *group_child
         child->type = type;
     }
 
-  xfce_tasklist_group_button_name_changed (group_child->class_group, group_child);
+  if (group_child->sort_idle_id == 0)
+    group_child->sort_idle_id =
+      g_idle_add (xfce_tasklist_group_button_name_changed_idle, group_child);
 }
 
 