xfce-mirror/xfce4-panel

commit e654d048dbd5c4dddb212246d1167a0ed503a3f1

libxfce4panel: Block autohide when remove plugin dialog is shown
MR: !78

libxfce4panel/xfce-panel-plugin.c
@@ -1072,6 +1072,8 @@ xfce_panel_plugin_menu_remove (XfcePanelPlugin *plugin)
 
   panel_return_if_fail (XFCE_IS_PANEL_PLUGIN (plugin));
 
+  xfce_panel_plugin_block_autohide (plugin, TRUE);
+
   /* create question dialog (same code is also in panel-preferences-dialog.c) */
   dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_MODAL,
       GTK_MESSAGE_QUESTION, GTK_BUTTONS_NONE,
@@ -1095,6 +1097,8 @@ xfce_panel_plugin_menu_remove (XfcePanelPlugin *plugin)
     }
 
   gtk_widget_destroy (dialog);
+
+  xfce_panel_plugin_block_autohide (plugin, FALSE);
 }
 
 
@@ -1260,8 +1264,8 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 G_GNUC_BEGIN_IGNORE_DEPRECATIONS
           item = gtk_image_menu_item_new_with_mnemonic (_("_Remove"));
 G_GNUC_END_IGNORE_DEPRECATIONS
-          g_signal_connect_swapped (G_OBJECT (item), "activate",
-              G_CALLBACK (xfce_panel_plugin_menu_remove), plugin);
+          g_signal_connect_object (G_OBJECT (item), "activate",
+              G_CALLBACK (xfce_panel_plugin_menu_remove), plugin, G_CONNECT_SWAPPED);
           gtk_menu_shell_append (GTK_MENU_SHELL (menu), item);
           gtk_widget_show (item);
 

commit 5b1d4ffde601fac97489933a01b6b4007a47cca7

libxfce4panel: Unblock autohide before removing the plugin
Regression introduced in e654d048dbd5c4dddb212246d1167a0ed503a3f1.

Fixes: #592
Related: !78

libxfce4panel/xfce-panel-plugin.c
@@ -1091,13 +1091,16 @@ xfce_panel_plugin_menu_remove (XfcePanelPlugin *plugin)
     {
       gtk_widget_hide (dialog);
 
+      /* send signal to unlock the panel before removing the plugin */
+      xfce_panel_plugin_block_autohide (plugin, FALSE);
+
       /* ask the panel or wrapper to remove the plugin */
       xfce_panel_plugin_remove (plugin);
     }
+  else
+    xfce_panel_plugin_block_autohide (plugin, FALSE);
 
   gtk_widget_destroy (dialog);
-
-  xfce_panel_plugin_block_autohide (plugin, FALSE);
 }
 
 