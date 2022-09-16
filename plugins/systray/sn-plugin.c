xfce-mirror/xfce4-panel

commit beef2eed1389cbd67be548201c6e5c776e1da592

systray: Replace deprecated "GtkWidget::composited-changed" signal

plugins/systray/sn-plugin.c
@@ -349,7 +349,7 @@ sn_plugin_construct (XfcePanelPlugin *panel_plugin)
   systray_plugin_screen_changed (GTK_WIDGET (plugin), NULL);
 
   /* restart internally if compositing changed */
-  g_signal_connect (G_OBJECT (plugin), "composited-changed",
+  g_signal_connect (gdk_screen_get_default (), "composited-changed",
      G_CALLBACK (systray_plugin_composited_changed), NULL);
 
   /* Add statusnotifier box */

commit 3f68a7309b0fc776f47bf0ae37115753b3e86967

systray: Fix previous commit
The signal handler is supposed to receive the plugin as the only
parameter.

Fixes: beef2eed1389cbd67be548201c6e5c776e1da592

plugins/systray/sn-plugin.c
@@ -349,8 +349,8 @@ sn_plugin_construct (XfcePanelPlugin *panel_plugin)
   systray_plugin_screen_changed (GTK_WIDGET (plugin), NULL);
 
   /* restart internally if compositing changed */
-  g_signal_connect (gdk_screen_get_default (), "composited-changed",
-     G_CALLBACK (systray_plugin_composited_changed), NULL);
+  g_signal_connect_swapped (gdk_screen_get_default (), "composited-changed",
+     G_CALLBACK (systray_plugin_composited_changed), plugin);
 
   /* Add statusnotifier box */
   plugin->sn_box = sn_box_new (plugin->config);