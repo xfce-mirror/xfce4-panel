xfce-mirror/xfce4-panel

commit d3cfa2742d373452e51f240291ad7651c629e5d8

tasklist: Properly update active window when it changes workspace
This includes in particular the fix for bug 6474. As for the call to
`xfce_tasklist_active_workspace_changed()` it is useless, since by
assumption, if one passes in `xfce_tasklist_button_workspace_changed()`,
a workspace change signal is already sent.

Fixes: #207

plugins/tasklist/tasklist-widget.c
@@ -2928,12 +2928,7 @@ xfce_tasklist_button_workspace_changed (WnckWindow        *window,
   panel_return_if_fail (XFCE_IS_TASKLIST (child->tasklist));
 
   xfce_tasklist_sort (tasklist, FALSE);
-
-  /* make sure we don't have two active windows (bug #6474) */
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (child->button), FALSE);
-
-  if (!tasklist->all_workspaces)
-    xfce_tasklist_active_workspace_changed (tasklist->screen, NULL, tasklist);
+  xfce_tasklist_active_window_changed (tasklist->screen, window, tasklist);
 }
 
 

commit 3461797e2caa7971063d1c4815fe4d41b3b14dd2

tasklist: Update button visibility when sent to another workspace
Regression introduced in d3cfa274. Contrary to what is said there, the
call to `xfce_tasklist_active_workspace_changed()` is not useless, since
a window can change workspace without the current workspace changing.

plugins/tasklist/tasklist-widget.c
@@ -2930,6 +2930,8 @@ xfce_tasklist_button_workspace_changed (WnckWindow        *window,
 
   xfce_tasklist_sort (tasklist, FALSE);
   xfce_tasklist_active_window_changed (tasklist->screen, window, tasklist);
+  if (!tasklist->all_workspaces)
+    xfce_tasklist_active_workspace_changed (tasklist->screen, NULL, tasklist);
 }
 
 