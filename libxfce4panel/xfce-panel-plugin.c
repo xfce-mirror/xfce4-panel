9913e8607d2a2036cc2a19d30b923f40e3c74a61
Gaël Bonithon <gael@xfce.org>
2022-11-04 19:08:24 +0100

    build: Make all X11/Wayland-specific dependencies optional

--- libxfce4panel/xfce-panel-plugin.c
+++ libxfce4panel/xfce-panel-plugin.c
@@ -28,12 +28,14 @@
 #endif
 
 #include <gtk/gtk.h>
-#ifdef GDK_WINDOWING_X11
+#ifdef HAVE_GTK_X11
 #include <gtk/gtkx.h>
 #endif
 #include <glib.h>
 #include <libxfce4util/libxfce4util.h>
+#ifdef HAVE_GTK_LAYER_SHELL
 #include <gtk-layer-shell/gtk-layer-shell.h>
+#endif
 
 #include <common/panel-private.h>
 #include <libxfce4panel/xfce-panel-macros.h>
@@ -2481,6 +2483,7 @@ xfce_panel_plugin_arrow_type (XfcePanelPlugin *plugin)
       gdk_monitor_get_geometry (monitor, &geometry);
 
       /* get the plugin root origin */
+#ifdef HAVE_GTK_LAYER_SHELL
       if (gtk_layer_is_supported ())
         {
           GtkWindow *toplevel = GTK_WINDOW (gtk_widget_get_toplevel (GTK_WIDGET (plugin)));
@@ -2488,6 +2491,7 @@ xfce_panel_plugin_arrow_type (XfcePanelPlugin *plugin)
           y = geometry.y + gtk_layer_get_margin (toplevel, GTK_LAYER_SHELL_EDGE_TOP);
         }
       else
+#endif
         gdk_window_get_root_origin (window, &x, &y);
 
       /* detect arrow type */
@@ -2529,7 +2533,7 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
                                    gint            *x,
                                    gint            *y)
 {
-#ifdef GDK_WINDOWING_X11
+#ifdef HAVE_GTK_X11
   GtkWidget      *plug;
   gint            px, py;
 #endif
@@ -2563,6 +2567,7 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
 
   /* get the root position of the attach widget */
   toplevel = GTK_WINDOW (gtk_widget_get_toplevel (attach_widget));
+#ifdef HAVE_GTK_LAYER_SHELL
   if (gtk_layer_is_supported ())
     {
       monitor = gdk_display_get_monitor_at_window (gdk_display_get_default (),
@@ -2572,10 +2577,11 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
       *y = geometry.y + gtk_layer_get_margin (toplevel, GTK_LAYER_SHELL_EDGE_TOP);
     }
   else
+#endif
     gtk_window_get_position (toplevel, x, y);
 
   /* correct position for external plugins on X11 */
-#ifdef GDK_WINDOWING_X11
+#ifdef HAVE_GTK_X11
   plug = gtk_widget_get_ancestor (attach_widget, GTK_TYPE_PLUG);
   if (plug != NULL)
     {
@@ -2856,6 +2862,7 @@ xfce_panel_plugin_popup_window_reposition (gpointer data)
   gint x, y;
 
   xfce_panel_plugin_position_widget (data, GTK_WIDGET (window), widget, &x, &y);
+#ifdef HAVE_GTK_LAYER_SHELL
   if (gtk_layer_is_supported ())
     {
       GdkRectangle geom;
@@ -2867,6 +2874,7 @@ xfce_panel_plugin_popup_window_reposition (gpointer data)
       gtk_layer_set_margin (window, GTK_LAYER_SHELL_EDGE_TOP, y - geom.y);
     }
   else
+#endif
     gtk_window_move (window, x, y);
 
   return FALSE;
@@ -2922,6 +2930,7 @@ xfce_panel_plugin_popup_window (XfcePanelPlugin *plugin,
   gtk_window_set_keep_above (window, TRUE);
   gtk_window_stick (window);
 
+#ifdef HAVE_GTK_LAYER_SHELL
   if (gtk_layer_is_supported ())
     {
       GdkMonitor *monitor;
@@ -2938,6 +2947,7 @@ xfce_panel_plugin_popup_window (XfcePanelPlugin *plugin,
       gtk_layer_set_anchor (window, GTK_LAYER_SHELL_EDGE_LEFT, TRUE);
       gtk_layer_set_anchor (window, GTK_LAYER_SHELL_EDGE_RIGHT, FALSE);
     }
+#endif
 
   g_signal_connect (window, "hide",
                     G_CALLBACK (xfce_panel_plugin_popup_window_hide), plugin);
