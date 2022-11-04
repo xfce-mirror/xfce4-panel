5f217a3f030b67221cd802bd4c9fb2aa67c13702
Gaël Bonithon <gael@xfce.org>
2022-11-04 19:08:24 +0100

    build: Make all X11/Wayland-specific dependencies optional

--- panel/panel-plugin-external-wrapper-wayland.c
+++ panel/panel-plugin-external-wrapper-wayland.c
@@ -20,9 +20,6 @@
 #include <config.h>
 #endif
 
-#include <gtk/gtk.h>
-#ifdef GDK_WINDOWING_WAYLAND
-
 #include <gtk-layer-shell/gtk-layer-shell.h>
 
 #include <common/panel-private.h>
@@ -370,5 +367,3 @@ panel_plugin_external_wrapper_wayland_pointer_is_outside (PanelPluginExternal *e
 
   return ret;
 }
-
-#endif /* ! GDK_WINDOWING_WAYLAND */
