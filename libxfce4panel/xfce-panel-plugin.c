xfce-mirror/xfce4-panel

commit 7d0976db9ea1a97b2ef253a18800582506b2fa3d

libxfce4panel: Fix menu widget positioning when the panel is hidden
It is enough to purge the current events once to show the panel, and the
use of `gdk_window_get_position()` was wrong and in the wrong place.

libxfce4panel/xfce-panel-plugin.c
@@ -2543,6 +2543,13 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
   /* get the menu/widget size request */
   gtk_widget_get_preferred_size (menu_widget, &requisition, NULL);
 
+  /* if the panel is hidden (auto hide is enabled) and we requested a
+   * panel lock, wait for gtk to position the panel before we actually
+   * use the coordinates */
+  if (plugin->priv->panel_lock > 0)
+    while (gtk_events_pending ())
+      gtk_main_iteration ();
+
   /* get the root position of the attach widget */
   toplevel = gtk_widget_get_toplevel (attach_widget);
   gtk_window_get_position (GTK_WINDOW (toplevel), x, y);
@@ -2555,26 +2562,6 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
                                 &px, &py, NULL, NULL);
        *x += px;
        *y += py;
-    }
-
-  /* if the panel is hidden (auto hide is enabled) and we requested a
-   * panel lock, wait for gtk to position the panel before we actually
-   * use the coordinates */
-  if (plugin->priv->panel_lock > 0)
-    {
-      gint64 end_t = g_get_monotonic_time () + G_USEC_PER_SEC / 2;
-
-      while (*x == -9999 && *y == -9999)
-        {
-          while (gtk_events_pending ())
-            gtk_main_iteration ();
-
-          gdk_window_get_position (gtk_widget_get_window (attach_widget), x, y);
-
-          /* don't try longer then 1/2 a second */
-          if (g_get_monotonic_time () > end_t)
-            break;
-        }
     }
 
   /* add the widgets allocation */

commit 03f01527a3c7bd8c03ad79788476a99d36b35348

Fix previous commits about menu positioning
Avoid iterating the main loop by hand. This is bad practice that may
change the order in which things should normally happen and lead to bad
surprises.

For popup_menu() use g_idle_add() instead. For position_widget(), just
delete this code since it was broken before and probably never called,
and put a hint in the doc instead.

Fixes: 7d0976db, 42b6032e

libxfce4panel/xfce-panel-plugin.c
@@ -2502,6 +2502,9 @@ xfce_panel_plugin_arrow_type (XfcePanelPlugin *plugin)
  * relative to @attach_widget. If @attach_widget is NULL, the computed
  * position will be relative to @plugin.
  *
+ * Note that if the panel is hidden (autohide), you should delay calling this
+ * function until the panel is shown, so that it returns the correct coordinates.
+ *
  * This function is intended for custom menu widgets.
  * For a regular #GtkMenu you should use xfce_panel_plugin_popup_menu() instead.
  *
@@ -2542,13 +2545,6 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
 
   /* get the menu/widget size request */
   gtk_widget_get_preferred_size (menu_widget, &requisition, NULL);
-
-  /* if the panel is hidden (auto hide is enabled) and we requested a
-   * panel lock, wait for gtk to position the panel before we actually
-   * use the coordinates */
-  if (plugin->priv->panel_lock > 0)
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
 
   /* get the root position of the attach widget */
   toplevel = gtk_widget_get_toplevel (attach_widget);
@@ -2679,6 +2675,15 @@ xfce_panel_plugin_position_menu (GtkMenu  *menu,
 
 
 
+static gboolean
+xfce_panel_plugin_popup_menu_reposition (gpointer data)
+{
+  gtk_menu_reposition (data);
+  return FALSE;
+}
+
+
+
 /**
  * xfce_panel_plugin_popup_menu:
  * @plugin        : an #XfcePanelPlugin.
@@ -2749,10 +2754,8 @@ xfce_panel_plugin_popup_menu (XfcePanelPlugin *plugin,
   /* register the menu */
   xfce_panel_plugin_register_menu (plugin, menu);
 
-  /* since we requested a panel lock, wait for gtk to position the panel if it is hidden
-   * before popping up the menu */
-  while (gtk_events_pending ())
-    gtk_main_iteration ();
+  /* since we requested a panel lock, queue a menu repositioning in case the panel is hidden */
+  g_idle_add (xfce_panel_plugin_popup_menu_reposition, menu);
 
   /* pop up the menu */
   if (popup_at_widget)