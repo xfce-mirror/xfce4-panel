xfce-mirror/xfce4-panel

commit e17d6a35148e4d60cd1af43329af2e99de6c5cfa

tasklist: Fix update of group button urgency blinking
MR: !90

plugins/tasklist/tasklist-widget.c
@@ -2840,7 +2840,7 @@ xfce_tasklist_button_state_changed (WnckWindow        *window,
   WnckScreen        *screen;
   XfceTasklist      *tasklist;
   WnckWorkspace     *active_ws;
-  XfceTasklistChild *group_child;
+  XfceTasklistChild *group_child, *temp_child;
 
   panel_return_if_fail (WNCK_IS_WINDOW (window));
   panel_return_if_fail (child->window == window);
@@ -2923,6 +2923,19 @@ xfce_tasklist_button_state_changed (WnckWindow        *window,
               g_hash_table_lookup_extended (child->tasklist->class_groups,
                                             child->class_group,
                                             NULL, (gpointer *) &group_child);
+
+              /* stop blinking only if no window in group needs attention */
+              if (! blink)
+                for (GSList *lp = group_child->windows; lp != NULL; lp = lp->next)
+                  {
+                    temp_child = lp->data;
+                    if (wnck_window_needs_attention (temp_child->window))
+                      {
+                        blink = TRUE;
+                        break;
+                      }
+                  }
+
               xfce_arrow_button_set_blinking (XFCE_ARROW_BUTTON (group_child->button), blink);
             }
 
@@ -4387,6 +4400,11 @@ xfce_tasklist_group_button_child_visible_changed (XfceTasklistChild *group_child
           && ! gtk_widget_get_visible (group_child->button))
         xfce_tasklist_group_button_keep_dnd_position (group_child, group_child->windows->data,
                                                       group_child);
+
+      /* update urgency blinking if needed */
+      xfce_tasklist_button_state_changed (child->window, URGENT_FLAGS,
+                                          wnck_window_needs_attention (child->window) ? URGENT_FLAGS : 0,
+                                          child);
 
       /* show the button and take the windows */
       gtk_widget_show (group_child->button);

commit eb27111dd3921a9a6ebcacde4fe49bd552e7cc06

tasklist: Fix group button visibility
Regression introduced in e17d6a35148e4d60cd1af43329af2e99de6c5cfa.

Fixes: #693

plugins/tasklist/tasklist-widget.c
@@ -4344,11 +4344,6 @@ xfce_tasklist_group_button_child_visible_changed (XfceTasklistChild *group_child
         xfce_tasklist_group_button_keep_dnd_position (group_child, group_child->windows->data,
                                                       group_child);
 
-      /* update urgency blinking if needed */
-      xfce_tasklist_button_state_changed (child->window, XFW_WINDOW_STATE_URGENT,
-                                          xfw_window_is_urgent (child->window) ? XFW_WINDOW_STATE_URGENT : 0,
-                                          child);
-
       /* show the button and take the windows */
       gtk_widget_show (group_child->button);
       type = CHILD_TYPE_GROUP_MENU;
@@ -4373,6 +4368,13 @@ xfce_tasklist_group_button_child_visible_changed (XfceTasklistChild *group_child
     }
 
   xfce_tasklist_group_button_name_changed (group_child->app, NULL, group_child);
+
+  /* update group button urgency blinking if needed: do this last as it may change window
+   * buttons visibility and therefore be recursive */
+  if (visible_counter > 1)
+    xfce_tasklist_button_state_changed (child->window, XFW_WINDOW_STATE_URGENT,
+                                        xfw_window_is_urgent (child->window) ? XFW_WINDOW_STATE_URGENT : 0,
+                                        child);
 }
 
 