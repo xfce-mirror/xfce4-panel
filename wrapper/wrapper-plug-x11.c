24af49d79c1b7ec92a41d524c22da25ed4f216ae
Gaël Bonithon <gael@xfce.org>
2023-01-25 19:51:01 +0100

    Revert "Use RGBA visual only if compositing is enabled (Fixes #251)"
    This reverts commit 87c609e7a576425f69b4c56786dbf657c278ae6a.
    
    While this fixed #251, it had the side effect of forcing a restart of
    the panel to get the transparency working if compositing was not enabled
    before (since gtk_widget_set_visual() must be called before the panel is
    realized). Since #251 is a rather special case, the lesser evil seems to
    be to use the RGBA visual as soon as possible.
    
    Fixes: #672

--- wrapper/wrapper-plug-x11.c
+++ wrapper/wrapper-plug-x11.c
@@ -88,7 +88,7 @@ wrapper_plug_x11_init (WrapperPlugX11 *plug)
   /* set the colormap */
   screen = gtk_window_get_screen (GTK_WINDOW (plug));
   visual = gdk_screen_get_rgba_visual (screen);
-  if (visual != NULL && gdk_screen_is_composited (screen))
+  if (visual != NULL)
     gtk_widget_set_visual (GTK_WIDGET (plug), visual);
 
   /* set the panel class */
