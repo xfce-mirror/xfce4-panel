xfce-mirror/xfce4-panel

commit 8b604611c2f657cbf0fd05760a2b180b6b41bb87

Use a normalized URI internally for the background image
This avoids character escaping problems, for example when using the URI
in CSS code. Before this change, it was not possible to use an image
whose name contained a single quote.

It is still possible to specify a file path via Xfconf as before, and it
becomes additionally possible to specify an URI (in fact all
possibilities accepted by `g_file_new_for_commandline_arg()` to build a
GFile). So this should not cause any backward compatibility problems.

panel/panel-base-window.c
@@ -301,6 +301,7 @@ panel_base_window_set_property (GObject      *object,
   PanelBaseWindow        *window = PANEL_BASE_WINDOW (object);
   PanelBaseWindowPrivate *priv = window->priv;
   PanelBgStyle            bg_style;
+  GFile                  *file;
 
   switch (prop_id)
     {
@@ -367,9 +368,11 @@ panel_base_window_set_property (GObject      *object,
       break;
 
     case PROP_BACKGROUND_IMAGE:
-      /* store new filename */
+      /* store new uri, built and escaped through a GFile */
       g_free (window->background_image);
-      window->background_image = g_value_dup_string (value);
+      file = g_file_new_for_commandline_arg (g_value_get_string (value));
+      window->background_image = g_file_get_uri (file);
+      g_object_unref (file);
 
       if (window->background_style == PANEL_BG_STYLE_IMAGE)
         {
@@ -620,7 +623,7 @@ static void
 panel_base_window_set_background_image_css (PanelBaseWindow *window) {
   gchar                  *css_string;
   panel_return_if_fail (window->background_image != NULL);
-  css_string = g_strdup_printf (".xfce4-panel.background { background: url('%s');"
+  css_string = g_strdup_printf (".xfce4-panel.background { background: url(\"%s\");"
                                                           "border-color: transparent; } %s",
                                 window->background_image, PANEL_BASE_CSS);
   panel_base_window_set_background_css (window, css_string);

panel/panel-preferences-dialog.c
@@ -726,7 +726,7 @@ static void
 panel_preferences_dialog_bg_image_file_set (GtkFileChooserButton   *button,
                                             PanelPreferencesDialog *dialog)
 {
-  gchar *filename;
+  gchar *uri;
 
   panel_return_if_fail (GTK_IS_FILE_CHOOSER_BUTTON (button));
   panel_return_if_fail (PANEL_IS_PREFERENCES_DIALOG (dialog));
@@ -735,9 +735,9 @@ panel_preferences_dialog_bg_image_file_set (GtkFileChooserButton   *button,
   g_signal_handler_block (G_OBJECT (dialog->active),
       dialog->bg_image_notify_handler_id);
 
-  filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (button));
-  g_object_set (G_OBJECT (dialog->active), "background-image", filename, NULL);
-  g_free (filename);
+  uri = gtk_file_chooser_get_uri (GTK_FILE_CHOOSER (button));
+  g_object_set (G_OBJECT (dialog->active), "background-image", uri, NULL);
+  g_free (uri);
 
   g_signal_handler_unblock (G_OBJECT (dialog->active),
       dialog->bg_image_notify_handler_id);
@@ -748,7 +748,7 @@ panel_preferences_dialog_bg_image_file_set (GtkFileChooserButton   *button,
 static void
 panel_preferences_dialog_bg_image_notified (PanelPreferencesDialog *dialog)
 {
-  gchar   *filename;
+  gchar   *uri;
   GObject *button;
 
   panel_return_if_fail (PANEL_IS_PREFERENCES_DIALOG (dialog));
@@ -760,9 +760,9 @@ panel_preferences_dialog_bg_image_notified (PanelPreferencesDialog *dialog)
   g_signal_handlers_block_by_func (G_OBJECT (button),
       G_CALLBACK (panel_preferences_dialog_bg_image_file_set), dialog);
 
-  g_object_get (G_OBJECT (dialog->active), "background-image", &filename, NULL);
-  gtk_file_chooser_set_filename (GTK_FILE_CHOOSER (button), filename != NULL ? filename : "");
-  g_free (filename);
+  g_object_get (G_OBJECT (dialog->active), "background-image", &uri, NULL);
+  gtk_file_chooser_set_uri (GTK_FILE_CHOOSER (button), uri != NULL ? uri : "");
+  g_free (uri);
 
   g_signal_handlers_unblock_by_func (G_OBJECT (button),
       G_CALLBACK (panel_preferences_dialog_bg_image_file_set), dialog);

wrapper/wrapper-plug.c
@@ -174,7 +174,7 @@ wrapper_plug_set_background_image (WrapperPlug *plug,
 
   panel_return_if_fail (WRAPPER_IS_PLUG (plug));
 
-  css = g_strdup_printf ("* { background: url('%s'); }", image);
+  css = g_strdup_printf ("* { background: url(\"%s\"); }", image);
 
   gtk_css_provider_load_from_data (GTK_CSS_PROVIDER (plug->style_provider), css, -1, NULL);
 

commit 94f2d71f227af7606729c57df3509fca4f3de727

panel: Properly handle null string as property default value
Xfconf uses this default value when reseting a property, which happens
for example when xfce4-panel-profiles resets all properties before
restoring a profile.

Fixes: 8b604611c2f657cbf0fd05760a2b180b6b41bb87

panel/panel-base-window.c
@@ -303,6 +303,7 @@ panel_base_window_set_property (GObject      *object,
   PanelBgStyle            bg_style;
   GFile                  *file;
   gdouble                 opacity;
+  const gchar            *str;
 
   switch (prop_id)
     {
@@ -379,11 +380,17 @@ panel_base_window_set_property (GObject      *object,
       break;
 
     case PROP_BACKGROUND_IMAGE:
-      /* store new uri, built and escaped through a GFile */
       g_free (window->background_image);
-      file = g_file_new_for_commandline_arg (g_value_get_string (value));
-      window->background_image = g_file_get_uri (file);
-      g_object_unref (file);
+      str = g_value_get_string (value);
+      if (str != NULL)
+        {
+          /* store new uri, built and escaped through a GFile */
+          file = g_file_new_for_commandline_arg (str);
+          window->background_image = g_file_get_uri (file);
+          g_object_unref (file);
+        }
+      else
+        window->background_image = NULL;
 
       if (window->background_style == PANEL_BG_STYLE_IMAGE)
         {

panel/panel-window.c
@@ -914,10 +914,6 @@ panel_window_set_property (GObject      *object,
           /* send the new screen position to the panel plugins */
           panel_window_plugins_update (window, PLUGIN_PROP_SCREEN_POSITION);
         }
-      else
-        {
-          g_message ("Not a valid position defined: %s", val_string);
-        }
       break;
 
     case PROP_DISABLE_STRUTS: