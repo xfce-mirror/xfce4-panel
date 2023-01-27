xfce-mirror/xfce4-panel

commit bdf5f7a66efc6ad4e1dff7b402f397901e978ca5

wayland: panel: Add Wayland impl for external wrapper

panel/panel-plugin-external-wrapper-wayland.c
@@ -1,0 +1,288 @@
+/*
+ * Copyright (C) 2022 Gaël Bonithon <gael@xfce.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+#ifdef GDK_WINDOWING_WAYLAND
+
+#include <gtk-layer-shell/gtk-layer-shell.h>
+
+#include <common/panel-private.h>
+#include <common/panel-debug.h>
+#include <common/panel-dbus.h>
+#include <panel/panel-plugin-external-wrapper-wayland.h>
+#include <panel/panel-plugin-external-wrapper-exported.h>
+
+
+
+static void         panel_plugin_external_wrapper_wayland_constructed             (GObject                              *object);
+static void         panel_plugin_external_wrapper_wayland_finalize                (GObject                              *object);
+static void         panel_plugin_external_wrapper_wayland_size_allocate           (GtkWidget                            *widget,
+                                                                                   GtkAllocation                        *allocation);
+static gchar      **panel_plugin_external_wrapper_wayland_get_argv                (PanelPluginExternal                  *external,
+                                                                                   gchar                               **arguments);
+static void         panel_plugin_external_wrapper_wayland_set_background_color    (PanelPluginExternal                  *external,
+                                                                                   const GdkRGBA                        *color);
+static void         panel_plugin_external_wrapper_wayland_set_background_image    (PanelPluginExternal                  *external,
+                                                                                   const gchar                          *image);
+static void         panel_plugin_external_wrapper_wayland_set_geometry            (PanelPluginExternal                  *external,
+                                                                                   PanelWindow                          *window);
+static void         panel_plugin_external_wrapper_wayland_proxy_set_requisition   (GDBusProxy                           *proxy,
+                                                                                   gchar                                *sender_name,
+                                                                                   gchar                                *signal_name,
+                                                                                   GVariant                             *parameters,
+                                                                                   PanelPluginExternalWrapperWayland    *wrapper);
+static void         panel_plugin_external_wrapper_wayland_proxy_embedded          (GDBusProxy                           *proxy,
+                                                                                   gchar                                *sender_name,
+                                                                                   gchar                                *signal_name,
+                                                                                   GVariant                             *parameters,
+                                                                                   PanelPluginExternalWrapperWayland    *wrapper);
+
+
+
+struct _PanelPluginExternalWrapperWayland
+{
+  PanelPluginExternalWrapper __parent__;
+
+  GDBusProxy *proxy;
+  GdkMonitor *monitor;
+  GdkRectangle geometry;
+};
+
+
+
+G_DEFINE_FINAL_TYPE (PanelPluginExternalWrapperWayland, panel_plugin_external_wrapper_wayland, PANEL_TYPE_PLUGIN_EXTERNAL_WRAPPER)
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_class_init (PanelPluginExternalWrapperWaylandClass *klass)
+{
+  GObjectClass *gobject_class;
+  GtkWidgetClass *gtkwidget_class;
+  PanelPluginExternalClass *external_class;
+
+  gobject_class = G_OBJECT_CLASS (klass);
+  gobject_class->constructed = panel_plugin_external_wrapper_wayland_constructed;
+  gobject_class->finalize = panel_plugin_external_wrapper_wayland_finalize;
+
+  gtkwidget_class = GTK_WIDGET_CLASS (klass);
+  gtkwidget_class->size_allocate = panel_plugin_external_wrapper_wayland_size_allocate;
+
+  external_class = PANEL_PLUGIN_EXTERNAL_CLASS (klass);
+  external_class->get_argv = panel_plugin_external_wrapper_wayland_get_argv;
+  external_class->set_background_color = panel_plugin_external_wrapper_wayland_set_background_color;
+  external_class->set_background_image = panel_plugin_external_wrapper_wayland_set_background_image;
+  external_class->set_geometry = panel_plugin_external_wrapper_wayland_set_geometry;
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_init (PanelPluginExternalWrapperWayland *wrapper)
+{
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_constructed (GObject *object)
+{
+  PanelPluginExternalWrapperWayland *wrapper = PANEL_PLUGIN_EXTERNAL_WRAPPER_WAYLAND (object);
+  XfcePanelPluginWrapperExported *skeleton;
+  GDBusConnection *connection;
+  GError *error = NULL;
+  gchar *path, *name;
+
+  /* first let ExternalWrapper do its part of the D-Bus job */
+  G_OBJECT_CLASS (panel_plugin_external_wrapper_wayland_parent_class)->constructed (object);
+  g_object_get (object, "skeleton", &skeleton, NULL);
+  if (skeleton == NULL)
+    return;
+
+  connection = g_dbus_interface_skeleton_get_connection (G_DBUS_INTERFACE_SKELETON (skeleton));
+  if (connection == NULL)
+    return;
+
+  name = g_strdup_printf (PANEL_DBUS_PLUGIN_NAME, PANEL_PLUGIN_EXTERNAL (object)->unique_id);
+  path = g_strdup_printf (PANEL_DBUS_PLUGIN_PATH, PANEL_PLUGIN_EXTERNAL (object)->unique_id);
+  wrapper->proxy = g_dbus_proxy_new_sync (connection, G_DBUS_PROXY_FLAGS_NONE, NULL, name, path,
+                                          PANEL_DBUS_EXTERNAL_INTERFACE, NULL, &error);
+  if (wrapper->proxy != NULL)
+    {
+      /* PluginExternal gives WrapperPlug its geometry (position and size), which in turn
+       * gives it its requisition (size only) */
+      g_signal_connect (wrapper->proxy, "g-signal::SetRequisition",
+                        G_CALLBACK (panel_plugin_external_wrapper_wayland_proxy_set_requisition), object);
+
+      /* when the plug is ready to receive its geometry, in particular */
+      g_signal_connect (wrapper->proxy, "g-signal::Embedded",
+                        G_CALLBACK (panel_plugin_external_wrapper_wayland_proxy_embedded), object);
+
+      panel_debug (PANEL_DEBUG_EXTERNAL, "Created D-BUS proxy at path %s", path);
+    }
+  else
+    {
+      g_critical ("Failed to create D-BUS proxy at path %s: %s", path, error->message);
+      g_error_free (error);
+    }
+
+  g_free (name);
+  g_free (path);
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_finalize (GObject *object)
+{
+  PanelPluginExternalWrapperWayland *wrapper = PANEL_PLUGIN_EXTERNAL_WRAPPER_WAYLAND (object);
+
+  if (wrapper->proxy != NULL)
+    g_object_unref (wrapper->proxy);
+
+  G_OBJECT_CLASS (panel_plugin_external_wrapper_wayland_parent_class)->finalize (object);
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_size_allocate (GtkWidget *widget,
+                                                     GtkAllocation *allocation)
+{
+  PanelWindow *window = PANEL_WINDOW (gtk_widget_get_ancestor (widget, PANEL_TYPE_WINDOW));
+
+  GTK_WIDGET_CLASS (panel_plugin_external_wrapper_wayland_parent_class)->size_allocate (widget, allocation);
+
+  panel_plugin_external_wrapper_wayland_set_geometry (PANEL_PLUGIN_EXTERNAL (widget), window);
+}
+
+
+
+static gchar **
+panel_plugin_external_wrapper_wayland_get_argv (PanelPluginExternal *external,
+                                                gchar **arguments)
+{
+  gchar **argv;
+
+  argv = PANEL_PLUGIN_EXTERNAL_CLASS (panel_plugin_external_wrapper_wayland_parent_class)->get_argv (external, arguments);
+  if (argv != NULL)
+    argv[PLUGIN_ARGV_SOCKET_ID] = g_strdup_printf ("%lu", 0LU);
+
+  return argv;
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_set_background_color (PanelPluginExternal *external,
+                                                            const GdkRGBA *color)
+{
+  /* X11 only */
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_set_background_image (PanelPluginExternal *external,
+                                                            const gchar *image)
+{
+  /* X11 only */
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_set_geometry (PanelPluginExternal *external,
+                                                    PanelWindow *window)
+{
+  PanelPluginExternalWrapperWayland *wrapper = PANEL_PLUGIN_EXTERNAL_WRAPPER_WAYLAND (external);
+  GtkAllocation alloc;
+  GdkRectangle *geom = &wrapper->geometry;
+  GdkDisplay *display;
+  GdkMonitor *monitor;
+  gint x, y;
+
+  display = gdk_display_get_default ();
+  monitor = gdk_display_get_monitor_at_window (display, gtk_widget_get_window (GTK_WIDGET (external)));
+  if (monitor != wrapper->monitor)
+    {
+      GValue value = G_VALUE_INIT;
+      gint n, n_monitors;
+
+      wrapper->monitor = monitor;
+      n_monitors = gdk_display_get_n_monitors (display);
+      for (n = 0; n < n_monitors; n++)
+        if (gdk_display_get_monitor (display, n) == monitor)
+          break;
+
+      g_value_init (&value, G_TYPE_INT);
+      g_value_set_int (&value, n);
+      panel_plugin_external_queue_add (external, PROVIDER_PROP_TYPE_SET_MONITOR, &value);
+      g_value_unset (&value);
+    }
+
+  gtk_widget_get_allocation (GTK_WIDGET (external), &alloc);
+  x = alloc.x + gtk_layer_get_margin (GTK_WINDOW (window), GTK_LAYER_SHELL_EDGE_LEFT);
+  y = alloc.y + gtk_layer_get_margin (GTK_WINDOW (window), GTK_LAYER_SHELL_EDGE_TOP);
+  if (x != geom->x || y != geom->y || alloc.width != geom->width || alloc.height != geom->height)
+    {
+      GValue value = G_VALUE_INIT;
+
+      geom->x = x;
+      geom->y = y;
+      geom->width = alloc.width;
+      geom->height = alloc.height;
+      g_value_init (&value, G_TYPE_VARIANT);
+      g_value_set_variant (&value, g_variant_new ("(iiii)", x, y, alloc.width, alloc.height));
+      panel_plugin_external_queue_add (external, PROVIDER_PROP_TYPE_SET_GEOMETRY, &value);
+      g_value_unset (&value);
+    }
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_proxy_set_requisition (GDBusProxy *proxy,
+                                                             gchar *sender_name,
+                                                             gchar *signal_name,
+                                                             GVariant *parameters,
+                                                             PanelPluginExternalWrapperWayland *wrapper)
+{
+  gint width, height;
+
+  g_variant_get (parameters, "((ii))", &width, &height);
+  gtk_widget_set_size_request (GTK_WIDGET (wrapper), width, height);
+}
+
+
+
+static void
+panel_plugin_external_wrapper_wayland_proxy_embedded (GDBusProxy *proxy,
+                                                      gchar *sender_name,
+                                                      gchar *signal_name,
+                                                      GVariant *parameters,
+                                                      PanelPluginExternalWrapperWayland *wrapper)
+{
+  panel_plugin_external_set_embedded (PANEL_PLUGIN_EXTERNAL (wrapper), TRUE);
+}
+
+#endif /* ! GDK_WINDOWING_WAYLAND */

.gitignore
@@ -86,4 +86,6 @@ plugins/systray/sn-watcher.c
 plugins/systray/sn-watcher.h
 wrapper/wrapper-1.0
 wrapper/wrapper-2.0
+wrapper/wrapper-external-exported.c
+wrapper/wrapper-external-exported.h
 *~

common/panel-dbus.h
@@ -20,11 +20,13 @@
 #define __PANEL_DBUS_H__
 
 /* panel dbus names */
-#define PANEL_DBUS_NAME              "org.xfce.Panel"
-#define PANEL_DBUS_PATH              "/org/xfce/Panel"
-#define PANEL_DBUS_INTERFACE         PANEL_DBUS_NAME
-#define PANEL_DBUS_WRAPPER_PATH      PANEL_DBUS_PATH "/Wrapper/%d"
-#define PANEL_DBUS_WRAPPER_INTERFACE PANEL_DBUS_INTERFACE ".Wrapper"
+#define PANEL_DBUS_NAME               "org.xfce.Panel"
+#define PANEL_DBUS_PATH               "/org/xfce/Panel"
+#define PANEL_DBUS_WRAPPER_PATH       PANEL_DBUS_PATH "/Wrapper/%d"
+#define PANEL_DBUS_WRAPPER_INTERFACE  PANEL_DBUS_NAME ".Wrapper"
+#define PANEL_DBUS_PLUGIN_NAME        PANEL_DBUS_NAME "Plugin%d"
+#define PANEL_DBUS_PLUGIN_PATH        PANEL_DBUS_PATH "Plugin%d"
+#define PANEL_DBUS_EXTERNAL_INTERFACE PANEL_DBUS_NAME ".External"
 
 enum
 {

libxfce4panel/xfce-panel-plugin-provider.h
@@ -127,6 +127,9 @@ typedef enum /*< skip >*/
   /* X11 only */
   PROVIDER_PROP_TYPE_SET_BACKGROUND_COLOR,    /* string */
   PROVIDER_PROP_TYPE_SET_BACKGROUND_IMAGE,    /* string */
+  /* Wayland only */
+  PROVIDER_PROP_TYPE_SET_MONITOR,             /* gint */
+  PROVIDER_PROP_TYPE_SET_GEOMETRY,            /* GdkRectangle */
 }
 XfcePanelPluginProviderPropType;
 

panel/Makefile.am
@@ -47,6 +47,8 @@ xfce4_panel_SOURCES = \
 	panel-plugin-external.h \
 	panel-plugin-external-wrapper.c \
 	panel-plugin-external-wrapper.h \
+	panel-plugin-external-wrapper-wayland.c \
+	panel-plugin-external-wrapper-wayland.h \
 	panel-plugin-external-wrapper-x11.c \
 	panel-plugin-external-wrapper-x11.h \
 	panel-preferences-dialog.c \

panel/panel-plugin-external-wrapper-wayland.h
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2022 Gaël Bonithon <gael@xfce.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __PANEL_PLUGIN_EXTERNAL_WRAPPER_WAYLAND_H__
+#define __PANEL_PLUGIN_EXTERNAL_WRAPPER_WAYLAND_H__
+
+#include <panel/panel-plugin-external-wrapper.h>
+
+G_BEGIN_DECLS
+
+#define PANEL_TYPE_PLUGIN_EXTERNAL_WRAPPER_WAYLAND (panel_plugin_external_wrapper_wayland_get_type ())
+G_DECLARE_FINAL_TYPE (PanelPluginExternalWrapperWayland, panel_plugin_external_wrapper_wayland, PANEL, PLUGIN_EXTERNAL_WRAPPER_WAYLAND, PanelPluginExternalWrapper)
+
+G_END_DECLS
+
+#endif /* ! __PANEL_PLUGIN_EXTERNAL_WRAPPER_WAYLAND_H__ */

panel/panel-plugin-external-wrapper-x11.c
@@ -37,6 +37,8 @@ static void         panel_plugin_external_wrapper_x11_set_background_color    (P
                                                                                const GdkRGBA                    *color);
 static void         panel_plugin_external_wrapper_x11_set_background_image    (PanelPluginExternal              *external,
                                                                                const gchar                      *image);
+static void         panel_plugin_external_wrapper_x11_set_geometry            (PanelPluginExternal              *external,
+                                                                               PanelWindow                      *window);
 static void         panel_plugin_external_wrapper_x11_socket_size_allocate    (GtkWidget                        *widget,
                                                                                GtkAllocation                    *allocation);
 static void         panel_plugin_external_wrapper_x11_socket_plug_added       (GtkSocket                        *socket,
@@ -77,6 +79,7 @@ panel_plugin_external_wrapper_x11_class_init (PanelPluginExternalWrapperX11Class
   external_class->get_argv = panel_plugin_external_wrapper_x11_get_argv;
   external_class->set_background_color = panel_plugin_external_wrapper_x11_set_background_color;
   external_class->set_background_image = panel_plugin_external_wrapper_x11_set_background_image;
+  external_class->set_geometry = panel_plugin_external_wrapper_x11_set_geometry;
 
   /* GtkWidget::size-allocate is flagged G_SIGNAL_RUN_FIRST so we need to overwrite it */
   g_signal_override_class_handler ("size-allocate", GTK_TYPE_SOCKET,
@@ -179,6 +182,15 @@ panel_plugin_external_wrapper_x11_set_background_image (PanelPluginExternal *ext
       panel_plugin_external_queue_add_action (external,
                                               PROVIDER_PROP_TYPE_ACTION_BACKGROUND_UNSET);
     }
+}
+
+
+
+static void
+panel_plugin_external_wrapper_x11_set_geometry (PanelPluginExternal *external,
+                                                PanelWindow *window)
+{
+  /* Wayland only */
 }
 
 

panel/panel-plugin-external-wrapper.c
@@ -33,6 +33,7 @@
 
 #include <gdk/gdk.h>
 #include <libxfce4util/libxfce4util.h>
+#include <gtk-layer-shell/gtk-layer-shell.h>
 
 #include <common/panel-private.h>
 #include <common/panel-dbus.h>
@@ -45,6 +46,7 @@
 #include <panel/panel-plugin-external.h>
 #include <panel/panel-plugin-external-wrapper.h>
 #include <panel/panel-plugin-external-wrapper-x11.h>
+#include <panel/panel-plugin-external-wrapper-wayland.h>
 #include <panel/panel-plugin-external-wrapper-exported.h>
 #include <panel/panel-window.h>
 #include <panel/panel-dialogs.h>
@@ -59,6 +61,10 @@
 #define get_instance_private(instance) \
   panel_plugin_external_wrapper_get_instance_private (PANEL_PLUGIN_EXTERNAL_WRAPPER (instance))
 
+static void       panel_plugin_external_wrapper_get_property             (GObject                        *object,
+                                                                          guint                           prop_id,
+                                                                          GValue                         *value,
+                                                                          GParamSpec                     *pspec);
 static void       panel_plugin_external_wrapper_constructed              (GObject                        *object);
 static void       panel_plugin_external_wrapper_finalize                 (GObject                        *object);
 static void       panel_plugin_external_wrapper_set_properties           (PanelPluginExternal            *external,
@@ -93,6 +99,12 @@ enum
   LAST_SIGNAL
 };
 
+enum
+{
+  PROP_0,
+  PROP_SKELETON,
+};
+
 
 
 static guint external_signals[LAST_SIGNAL];
@@ -110,6 +122,7 @@ panel_plugin_external_wrapper_class_init (PanelPluginExternalWrapperClass *klass
   PanelPluginExternalClass *plugin_external_class;
 
   gobject_class = G_OBJECT_CLASS (klass);
+  gobject_class->get_property = panel_plugin_external_wrapper_get_property;
   gobject_class->finalize = panel_plugin_external_wrapper_finalize;
   gobject_class->constructed = panel_plugin_external_wrapper_constructed;
 
@@ -117,6 +130,11 @@ panel_plugin_external_wrapper_class_init (PanelPluginExternalWrapperClass *klass
   plugin_external_class->get_argv = panel_plugin_external_wrapper_get_argv;
   plugin_external_class->set_properties = panel_plugin_external_wrapper_set_properties;
   plugin_external_class->remote_event = panel_plugin_external_wrapper_remote_event;
+
+  g_object_class_install_property (gobject_class, PROP_SKELETON,
+                                   g_param_spec_object ("skeleton", NULL, NULL,
+                                                        XFCE_PANEL_PLUGIN_WRAPPER_TYPE_EXPORTED,
+                                                        G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
 
   external_signals[REMOTE_EVENT_RESULT] =
     g_signal_new (g_intern_static_string ("remote-event-result"),
@@ -133,6 +151,28 @@ panel_plugin_external_wrapper_class_init (PanelPluginExternalWrapperClass *klass
 static void
 panel_plugin_external_wrapper_init (PanelPluginExternalWrapper *wrapper)
 {
+}
+
+
+
+static void
+panel_plugin_external_wrapper_get_property (GObject *object,
+                                            guint prop_id,
+                                            GValue *value,
+                                            GParamSpec *pspec)
+{
+  PanelPluginExternalWrapperPrivate *priv = get_instance_private (object);
+
+  switch (prop_id)
+    {
+    case PROP_SKELETON:
+      g_value_set_object (value, priv->skeleton);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
 }
 
 
@@ -263,6 +303,9 @@ panel_plugin_external_wrapper_gvalue_prop_to_gvariant (const GValue *value)
       break;
     case G_TYPE_STRING:
       type = G_VARIANT_TYPE_STRING;
+      break;
+    case G_TYPE_VARIANT:
+      type = G_VARIANT_TYPE_TUPLE;
       break;
     /* only throw a warning (instead of an assertion) here as otherwise invalid
        types sent to the panel via dbus would let the panel crash */
@@ -427,6 +470,13 @@ panel_plugin_external_wrapper_new (PanelModule  *module,
 #ifdef GDK_WINDOWING_X11
   if (GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
     return g_object_new (PANEL_TYPE_PLUGIN_EXTERNAL_WRAPPER_X11,
+                         "module", module,
+                         "unique-id", unique_id,
+                         "arguments", arguments, NULL);
+#endif
+#ifdef GDK_WINDOWING_WAYLAND
+  if (gtk_layer_is_supported ())
+    return g_object_new (PANEL_TYPE_PLUGIN_EXTERNAL_WRAPPER_WAYLAND,
                          "module", module,
                          "unique-id", unique_id,
                          "arguments", arguments, NULL);

commit 8464b432fcfd8e8359715feb89651c02b5d8dd98

wayland: panel: Properly release interface skeleton
This caused a crash when removing and then adding an external plugin,
because the signal handlers were not disconnected, with a warning that
an object was already exported to the requested address.

Fixes: bdf5f7a66efc6ad4e1dff7b402f397901e978ca5
Related: !103

panel/panel-plugin-external-wrapper-wayland.c
@@ -128,6 +128,7 @@ panel_plugin_external_wrapper_wayland_constructed (GObject *object)
     return;
 
   connection = g_dbus_interface_skeleton_get_connection (G_DBUS_INTERFACE_SKELETON (skeleton));
+  g_object_unref (skeleton);
   if (connection == NULL)
     return;
 