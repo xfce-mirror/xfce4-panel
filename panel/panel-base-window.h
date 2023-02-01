xfce-mirror/xfce4-panel

commit c7f32ce82f01738948dc522c2a48881d05c33341

cleanup: panel: Use G_DECLARE_FINAL_TYPE
PanelPluginExternal is left as is for the same reasons as in the
previous commit, and its other public members would involve even more
refactoring.

panel/panel-base-window.h
@@ -23,18 +23,12 @@
 
 G_BEGIN_DECLS
 
-typedef struct _PanelBaseWindowClass   PanelBaseWindowClass;
-typedef struct _PanelBaseWindow        PanelBaseWindow;
 typedef struct _PanelBaseWindowPrivate PanelBaseWindowPrivate;
 typedef enum   _PanelBorders           PanelBorders;
 typedef enum   _PanelBgStyle           PanelBgStyle;
 
-#define PANEL_TYPE_BASE_WINDOW            (panel_base_window_get_type ())
-#define PANEL_BASE_WINDOW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PANEL_TYPE_BASE_WINDOW, PanelBaseWindow))
-#define PANEL_BASE_WINDOW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PANEL_TYPE_BASE_WINDOW, PanelBaseWindowClass))
-#define PANEL_IS_BASE_WINDOW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PANEL_TYPE_BASE_WINDOW))
-#define PANEL_IS_BASE_WINDOW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PANEL_TYPE_BASE_WINDOW))
-#define PANEL_BASE_WINDOW_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), PANEL_TYPE_BASE_WINDOW, PanelBaseWindowClass))
+#define PANEL_TYPE_BASE_WINDOW (panel_base_window_get_type ())
+G_DECLARE_FINAL_TYPE (PanelBaseWindow, panel_base_window, PANEL, BASE_WINDOW, GtkWindow)
 
 enum _PanelBorders
 {
@@ -50,11 +44,6 @@ enum _PanelBgStyle
   PANEL_BG_STYLE_NONE,
   PANEL_BG_STYLE_COLOR,
   PANEL_BG_STYLE_IMAGE
-};
-
-struct _PanelBaseWindowClass
-{
-  GtkWindowClass __parent__;
 };
 
 struct _PanelBaseWindow
@@ -75,8 +64,6 @@ struct _PanelBaseWindow
   gdouble                  leave_opacity;
   gboolean                 opacity_is_enter;
 };
-
-GType        panel_base_window_get_type                    (void) G_GNUC_CONST;
 
 void         panel_base_window_reset_background_css        (PanelBaseWindow *window);
 void         panel_base_window_orientation_changed         (PanelBaseWindow *window,

panel/main.c
@@ -47,6 +47,7 @@
 
 #include <common/panel-private.h>
 #include <common/panel-debug.h>
+#include <common/panel-dbus.h>
 #include <libxfce4panel/libxfce4panel.h>
 #include <panel/panel-application.h>
 #include <panel/panel-dbus-service.h>

panel/panel-application.c
@@ -109,11 +109,6 @@ enum
   PROP_ITEMS_CHANGED
 };
 
-struct _PanelApplicationClass
-{
-  GObjectClass __parent__;
-};
-
 struct _PanelApplication
 {
   GObject  __parent__;
@@ -239,7 +234,7 @@ panel_application_init (PanelApplication *application)
   application->factory = panel_module_factory_get ();
 
   /* start the autosave timer for plugins */
-  application->autosave_timer_id = g_timeout_add_seconds (60 * 10,
+  application->autosave_timer_id = g_timeout_add_seconds (AUTOSAVE_INTERVAL,
       panel_application_autosave_timer, application);
 
   /* warn the user about restricted features on Wayland */

panel/panel-application.h
@@ -24,15 +24,8 @@
 
 G_BEGIN_DECLS
 
-typedef struct _PanelApplicationClass PanelApplicationClass;
-typedef struct _PanelApplication      PanelApplication;
-
-#define PANEL_TYPE_APPLICATION            (panel_application_get_type ())
-#define PANEL_APPLICATION(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PANEL_TYPE_APPLICATION, PanelApplication))
-#define PANEL_APPLICATION_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PANEL_TYPE_APPLICATION, PanelApplicationClass))
-#define PANEL_IS_APPLICATION(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PANEL_TYPE_APPLICATION))
-#define PANEL_IS_APPLICATION_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PANEL_TYPE_APPLICATION))
-#define PANEL_APPLICATION_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), PANEL_TYPE_APPLICATION, PanelApplicationClass))
+#define PANEL_TYPE_APPLICATION (panel_application_get_type ())
+G_DECLARE_FINAL_TYPE (PanelApplication, panel_application, PANEL, APPLICATION, GObject)
 
 typedef enum
 {
@@ -43,8 +36,6 @@ typedef enum
 PanelSaveTypes;
 #define SAVE_EVERYTHING (SAVE_PLUGIN_PROVIDERS | SAVE_PLUGIN_IDS | SAVE_PANEL_IDS)
 
-
-GType             panel_application_get_type          (void) G_GNUC_CONST;
 
 PanelApplication *panel_application_get               (void);
 

panel/panel-dbus-service.c
@@ -38,8 +38,6 @@
 #include <panel/panel-item-dialog.h>
 #include <panel/panel-module-factory.h>
 
-#include <panel/panel-gdbus-exported-service.h>
-
 static void      panel_dbus_service_finalize                   (GObject                 *object);
 static void      panel_dbus_service_plugin_event_result        (XfcePanelPluginProvider *prev_provider,
                                                                 guint                    handle,
@@ -77,11 +75,6 @@ static gboolean  panel_dbus_service_terminate                  (XfcePanelExporte
 
 
 
-
-struct _PanelDBusServiceClass
-{
-  XfcePanelExportedServiceSkeletonClass __parent__;
-};
 
 struct _PanelDBusService
 {

panel/panel-dbus-service.h
@@ -20,21 +20,10 @@
 #define __PANEL_DBUS_SERVICE_H__
 
 #include <glib.h>
-#include <common/panel-dbus.h>
+#include <panel/panel-gdbus-exported-service.h>
 
-typedef struct _PanelDBusServiceClass PanelDBusServiceClass;
-typedef struct _PanelDBusService      PanelDBusService;
-
-#define PANEL_TYPE_DBUS_SERVICE            (panel_dbus_service_get_type ())
-#define PANEL_DBUS_SERVICE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PANEL_TYPE_DBUS_SERVICE, PanelDBusService))
-#define PANEL_DBUS_CLASS_SERVICE(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PANEL_TYPE_DBUS_SERVICE, PanelDBusServiceClass))
-#define PANEL_IS_DBUS_SERVICE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PANEL_TYPE_DBUS_SERVICE))
-#define PANEL_IS_DBUS_SERVICE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PANEL_TYPE_DBUS_SERVICE))
-#define PANEL_DBUS_SERVICE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), PANEL_TYPE_DBUS_SERVICE, PanelDBusServiceClass))
-
-
-
-GType               panel_dbus_service_get_type    (void) G_GNUC_CONST;
+#define PANEL_TYPE_DBUS_SERVICE (panel_dbus_service_get_type ())
+G_DECLARE_FINAL_TYPE (PanelDBusService, panel_dbus_service, PANEL, DBUS_SERVICE, XfcePanelExportedServiceSkeleton)
 
 PanelDBusService   *panel_dbus_service_get         (void);
 

panel/panel-item-dialog.c
@@ -24,9 +24,6 @@
 #include <string.h>
 #endif
 
-#include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4util/libxfce4util.h>
-
 #include <common/panel-private.h>
 #include <common/panel-utils.h>
 #include <libxfce4panel/libxfce4panel.h>
@@ -40,8 +37,7 @@
 
 
 
-#define BORDER         (6)
-#define ITEMS_HELP_URL "http://www.xfce.org"
+#define BORDER (6)
 
 
 
@@ -97,11 +93,6 @@ static void         panel_item_dialog_text_renderer          (GtkTreeViewColumn 
                                                               gpointer            user_data);
 
 
-
-struct _PanelItemDialogClass
-{
-  XfceTitledDialogClass __parent__;
-};
 
 struct _PanelItemDialog
 {

panel/panel-item-dialog.h
@@ -21,19 +21,13 @@
 
 #include <gtk/gtk.h>
 
+/* includes libxfce4ui and the definition of glib_autoptr_clear_XfceTitledDialog */
+#include <panel/panel-tic-tac-toe.h>
+
 G_BEGIN_DECLS
 
-typedef struct _PanelItemDialogClass PanelItemDialogClass;
-typedef struct _PanelItemDialog      PanelItemDialog;
-
-#define PANEL_TYPE_ITEM_DIALOG            (panel_item_dialog_get_type ())
-#define PANEL_ITEM_DIALOG(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PANEL_TYPE_ITEM_DIALOG, PanelItemDialog))
-#define PANEL_ITEM_DIALOG_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PANEL_TYPE_ITEM_DIALOG, PanelItemDialogClass))
-#define PANEL_IS_ITEM_DIALOG(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PANEL_TYPE_ITEM_DIALOG))
-#define PANEL_IS_ITEM_DIALOG_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PANEL_TYPE_ITEM_DIALOG))
-#define PANEL_ITEM_DIALOG_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), PANEL_TYPE_ITEM_DIALOG, PanelItemDialogClass))
-
-GType      panel_item_dialog_get_type     (void) G_GNUC_CONST;
+#define PANEL_TYPE_ITEM_DIALOG (panel_item_dialog_get_type ())
+G_DECLARE_FINAL_TYPE (PanelItemDialog, panel_item_dialog, PANEL, ITEM_DIALOG, XfceTitledDialog)
 
 void       panel_item_dialog_show         (PanelWindow *active);
 

commit 344b7ceb29162a77f5d6605730b802a9caa99c61

cleanup: Revert use of G_DECLARE_FINAL_TYPE for PanelBaseWindow
It worked as is but it's misleading since PanelBaseWindow is derivable.
A clean refactoring would be to use G_DECLARE_DERIVABLE_TYPE by
replacing the public fields of the instance structure with accessors and
`priv` with `get_instance_private()`. But that will be for later, as for
PanelPluginExternal in a previous commit.

Fixes: c7f32ce82f01738948dc522c2a48881d05c33341

panel/panel-base-window.h
@@ -23,12 +23,19 @@
 
 G_BEGIN_DECLS
 
+typedef struct _PanelBaseWindowClass   PanelBaseWindowClass;
+typedef struct _PanelBaseWindow        PanelBaseWindow;
 typedef struct _PanelBaseWindowPrivate PanelBaseWindowPrivate;
 typedef enum   _PanelBorders           PanelBorders;
 typedef enum   _PanelBgStyle           PanelBgStyle;
 
-#define PANEL_TYPE_BASE_WINDOW (panel_base_window_get_type ())
-G_DECLARE_FINAL_TYPE (PanelBaseWindow, panel_base_window, PANEL, BASE_WINDOW, GtkWindow)
+#define PANEL_TYPE_BASE_WINDOW            (panel_base_window_get_type ())
+#define PANEL_BASE_WINDOW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PANEL_TYPE_BASE_WINDOW, PanelBaseWindow))
+#define PANEL_BASE_WINDOW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PANEL_TYPE_BASE_WINDOW, PanelBaseWindowClass))
+#define PANEL_IS_BASE_WINDOW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PANEL_TYPE_BASE_WINDOW))
+#define PANEL_IS_BASE_WINDOW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PANEL_TYPE_BASE_WINDOW))
+#define PANEL_BASE_WINDOW_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), PANEL_TYPE_BASE_WINDOW, PanelBaseWindowClass))
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (PanelBaseWindow, g_object_unref)
 
 enum _PanelBorders
 {
@@ -44,6 +51,11 @@ enum _PanelBgStyle
   PANEL_BG_STYLE_NONE,
   PANEL_BG_STYLE_COLOR,
   PANEL_BG_STYLE_IMAGE
+};
+
+struct _PanelBaseWindowClass
+{
+  GtkWindowClass __parent__;
 };
 
 struct _PanelBaseWindow
@@ -64,6 +76,8 @@ struct _PanelBaseWindow
   gdouble                  leave_opacity;
   gboolean                 opacity_is_enter;
 };
+
+GType        panel_base_window_get_type                    (void) G_GNUC_CONST;
 
 void         panel_base_window_reset_background_css        (PanelBaseWindow *window);
 void         panel_base_window_orientation_changed         (PanelBaseWindow *window,