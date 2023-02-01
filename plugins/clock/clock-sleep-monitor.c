xfce-mirror/xfce4-panel

commit 5da7e405cbc47b2d0f18a139043c3369cd6510bc

cleanup: plugins: Use G_DECLARE_FINAL_TYPE

plugins/clock/clock-sleep-monitor.c
@@ -57,13 +57,6 @@ struct _ClockSleepMonitor
   GObject parent_instance;
 };
 
-struct _ClockSleepMonitorClass
-{
-  GObjectClass parent_class;
-};
-
-typedef struct _ClockSleepMonitorClass ClockSleepMonitorClass;
-
 G_DEFINE_TYPE (ClockSleepMonitor, clock_sleep_monitor, G_TYPE_OBJECT)
 
 static void clock_sleep_monitor_finalize (GObject *object);
@@ -106,19 +99,8 @@ struct _ClockSleepMonitorLogind
   GDBusProxy *logind_proxy;
 };
 
-struct _ClockSleepMonitorLogindClass
-{
-  ClockSleepMonitorClass parent_class;
-};
-
-typedef struct _ClockSleepMonitorLogind ClockSleepMonitorLogind;
-typedef struct _ClockSleepMonitorLogindClass ClockSleepMonitorLogindClass;
-
-GType clock_sleep_monitor_logind_get_type (void) G_GNUC_CONST;
-
 #define XFCE_TYPE_CLOCK_SLEEP_MONITOR_LOGIND (clock_sleep_monitor_logind_get_type ())
-#define XFCE_CLOCK_SLEEP_MONITOR_LOGIND(object) (G_TYPE_CHECK_INSTANCE_CAST ((object), XFCE_TYPE_CLOCK_SLEEP_MONITOR_LOGIND, ClockSleepMonitorLogind))
-#define XFCE_IS_CLOCK_SLEEP_MONITOR_LOGIND(object) (G_TYPE_CHECK_INSTANCE_TYPE ((object), XFCE_TYPE_CLOCK_SLEEP_MONITOR_LOGIND))
+G_DECLARE_FINAL_TYPE (ClockSleepMonitorLogind, clock_sleep_monitor_logind, XFCE, CLOCK_SLEEP_MONITOR_LOGIND, ClockSleepMonitor)
 
 G_DEFINE_TYPE (ClockSleepMonitorLogind, clock_sleep_monitor_logind, XFCE_TYPE_CLOCK_SLEEP_MONITOR)
 

plugins/clock/clock-sleep-monitor.h
@@ -18,18 +18,12 @@
 
 #pragma once
 
-#include <glib.h>
 #include <glib-object.h>
 
 G_BEGIN_DECLS
 
-typedef struct _ClockSleepMonitor ClockSleepMonitor;
-
-GType clock_sleep_monitor_get_type (void) G_GNUC_CONST;
-
 #define XFCE_TYPE_CLOCK_SLEEP_MONITOR (clock_sleep_monitor_get_type ())
-#define XFCE_CLOCK_SLEEP_MONITOR(object) (G_TYPE_CHECK_INSTANCE_CAST((object), XFCE_TYPE_CLOCK_SLEEP_MONITOR, ClockSleepMonitor))
-#define XFCE_IS_CLOCK_SLEEP_MONITOR(object) (G_TYPE_CHECK_INSTANCE_TYPE((object), XFCE_TYPE_CLOCK_SLEEP_MONITOR))
+G_DECLARE_FINAL_TYPE (ClockSleepMonitor, clock_sleep_monitor, XFCE, CLOCK_SLEEP_MONITOR, GObject)
 
 /* Factory function that tries to instantiate a sleep monitor. Returns
  * NULL if no implementation could be found or instantiated.

commit a5e6d9f532677ab7766122d97e2ccbcad8cc6814

cleanup: clock: Use G_DECLARE_DERIVABLE_TYPE for SleepMonitor
Fixes: 5da7e405cbc47b2d0f18a139043c3369cd6510bc

plugins/clock/clock-sleep-monitor.c
@@ -52,12 +52,11 @@
 
 static guint clock_sleep_monitor_woke_up_signal = 0;
 
-struct _ClockSleepMonitor
+typedef struct _ClockSleepMonitorPrivate
 {
-  GObject parent_instance;
-};
+} ClockSleepMonitorPrivate;
 
-G_DEFINE_TYPE (ClockSleepMonitor, clock_sleep_monitor, G_TYPE_OBJECT)
+G_DEFINE_ABSTRACT_TYPE (ClockSleepMonitor, clock_sleep_monitor, G_TYPE_OBJECT)
 
 static void clock_sleep_monitor_finalize (GObject *object);
 

plugins/clock/clock-sleep-monitor.h
@@ -23,7 +23,12 @@
 G_BEGIN_DECLS
 
 #define CLOCK_TYPE_SLEEP_MONITOR (clock_sleep_monitor_get_type ())
-G_DECLARE_FINAL_TYPE (ClockSleepMonitor, clock_sleep_monitor, CLOCK, SLEEP_MONITOR, GObject)
+G_DECLARE_DERIVABLE_TYPE (ClockSleepMonitor, clock_sleep_monitor, CLOCK, SLEEP_MONITOR, GObject)
+
+struct _ClockSleepMonitorClass
+{
+  GObjectClass parent_class;
+};
 
 /* Factory function that tries to instantiate a sleep monitor. Returns
  * NULL if no implementation could be found or instantiated.