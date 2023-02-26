xfce-mirror/xfce4-panel

commit 03f01527a3c7bd8c03ad79788476a99d36b35348

Fix previous commits about menu positioning
Avoid iterating the main loop by hand. This is bad practice that may
change the order in which things should normally happen and lead to bad
surprises.

For popup_menu() use g_idle_add() instead. For position_widget(), just
delete this code since it was broken before and probably never called,
and put a hint in the doc instead.

Fixes: 7d0976db, 42b6032e

libxfce4panel/xfce-panel-plugin.c
@@ -2502,6 +2502,9 @@ xfce_panel_plugin_arrow_type (XfcePanelPlugin *plugin)
  * relative to @attach_widget. If @attach_widget is NULL, the computed
  * position will be relative to @plugin.
  *
+ * Note that if the panel is hidden (autohide), you should delay calling this
+ * function until the panel is shown, so that it returns the correct coordinates.
+ *
  * This function is intended for custom menu widgets.
  * For a regular #GtkMenu you should use xfce_panel_plugin_popup_menu() instead.
  *
@@ -2542,13 +2545,6 @@ xfce_panel_plugin_position_widget (XfcePanelPlugin *plugin,
 
   /* get the menu/widget size request */
   gtk_widget_get_preferred_size (menu_widget, &requisition, NULL);
-
-  /* if the panel is hidden (auto hide is enabled) and we requested a
-   * panel lock, wait for gtk to position the panel before we actually
-   * use the coordinates */
-  if (plugin->priv->panel_lock > 0)
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
 
   /* get the root position of the attach widget */
   toplevel = gtk_widget_get_toplevel (attach_widget);
@@ -2679,6 +2675,15 @@ xfce_panel_plugin_position_menu (GtkMenu  *menu,
 
 
 
+static gboolean
+xfce_panel_plugin_popup_menu_reposition (gpointer data)
+{
+  gtk_menu_reposition (data);
+  return FALSE;
+}
+
+
+
 /**
  * xfce_panel_plugin_popup_menu:
  * @plugin        : an #XfcePanelPlugin.
@@ -2749,10 +2754,8 @@ xfce_panel_plugin_popup_menu (XfcePanelPlugin *plugin,
   /* register the menu */
   xfce_panel_plugin_register_menu (plugin, menu);
 
-  /* since we requested a panel lock, wait for gtk to position the panel if it is hidden
-   * before popping up the menu */
-  while (gtk_events_pending ())
-    gtk_main_iteration ();
+  /* since we requested a panel lock, queue a menu repositioning in case the panel is hidden */
+  g_idle_add (xfce_panel_plugin_popup_menu_reposition, menu);
 
   /* pop up the menu */
   if (popup_at_widget)

commit de07cca27d6d5a4c5fd1f5b43f0c7d76fa895dcf

libxfce4panel: Fix memory management of source for menu positioning
This is unfortunately not safe as it is, because if two pointer events
to show and hide the menu are close enough, the second one will be
processed before the idle func is executed. And if hiding the menu
triggers its destruction, it causes a use-after-free. This is for
example the case with the DirectoryMenu plugin.

Fixes: 03f01527a3c7bd8c03ad79788476a99d36b35348

libxfce4panel/xfce-panel-plugin.c
@@ -1389,12 +1389,22 @@ static void
 xfce_panel_plugin_unregister_menu (GtkMenu         *menu,
                                    XfcePanelPlugin *plugin)
 {
+  guint id;
+
   panel_return_if_fail (XFCE_IS_PANEL_PLUGIN (plugin));
   panel_return_if_fail (GTK_IS_MENU (menu));
 
   /* disconnect this signal */
   g_signal_handlers_disconnect_by_func (G_OBJECT (menu),
       G_CALLBACK (xfce_panel_plugin_unregister_menu), plugin);
+
+  /* remove pending source */
+  id = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT (menu), "menu-reposition-id"));
+  if (id != 0)
+    {
+      g_source_remove (id);
+      g_object_set_data (G_OBJECT (menu), "menu-reposition-id", GUINT_TO_POINTER (0));
+    }
 
   /* tell panel it needs to unlock */
   xfce_panel_plugin_block_autohide (plugin, FALSE);
@@ -2721,6 +2731,8 @@ static gboolean
 xfce_panel_plugin_popup_menu_reposition (gpointer data)
 {
   gtk_menu_reposition (data);
+  g_object_set_data (data, "menu-reposition-id", GUINT_TO_POINTER (0));
+
   return FALSE;
 }
 
@@ -2757,6 +2769,7 @@ xfce_panel_plugin_popup_menu (XfcePanelPlugin *plugin,
 {
   GdkGravity widget_anchor, menu_anchor;
   gboolean   popup_at_widget = TRUE;
+  guint      id;
 
   g_return_if_fail (XFCE_IS_PANEL_PLUGIN (plugin));
   g_return_if_fail (GTK_IS_MENU (menu));
@@ -2798,7 +2811,12 @@ xfce_panel_plugin_popup_menu (XfcePanelPlugin *plugin,
   xfce_panel_plugin_register_menu (plugin, menu);
 
   /* since we requested a panel lock, queue a menu repositioning in case the panel is hidden */
-  g_idle_add (xfce_panel_plugin_popup_menu_reposition, menu);
+  id = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT (menu), "menu-reposition-id"));
+  if (id != 0)
+    g_source_remove (id);
+
+  id = g_idle_add (xfce_panel_plugin_popup_menu_reposition, menu);
+  g_object_set_data (G_OBJECT (menu), "menu-reposition-id", GUINT_TO_POINTER (id));
 
   /* pop up the menu */
   if (popup_at_widget)