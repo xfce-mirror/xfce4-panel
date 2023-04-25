9f2c65755fed7ec85d8b088a48ab285c80bd7ad5
Brian J. Tarricone <brian@tarricone.org>
2023-04-25 14:23:20 -0700

    Fix memory leaks around gtk_get_current_event()
    gtk_get_current_event() makes a copy of the event and returns the copy,
    which needs to be freed.
    
    For the cases where we create a dummy event, we need to ref the
    GdkWindow we set on it, as gdk_event_free() will unref it.

--- plugins/windowmenu/windowmenu.c
+++ plugins/windowmenu/windowmenu.c
@@ -1400,8 +1400,8 @@ static void
 window_menu_plugin_menu (GtkWidget        *button,
                          WindowMenuPlugin *plugin)
 {
-  GtkWidget      *menu;
-  GdkEventButton *event = NULL;
+  GtkWidget *menu;
+  GdkEvent  *event = NULL;
 
   panel_return_if_fail (WINDOW_MENU_IS_PLUGIN (plugin));
   panel_return_if_fail (button == NULL || plugin->button == button);
@@ -1412,11 +1412,11 @@ window_menu_plugin_menu (GtkWidget        *button,
 
   /* Panel plugin remote events don't send actual GdkEvents, so construct a minimal one so that
    * gtk_menu_popup_at_pointer/rect can extract a location correctly from a GdkWindow */
-  if (gtk_get_current_event () == NULL)
+  event = gtk_get_current_event ();
+  if (event == NULL)
     {
-      event = g_slice_new0 (GdkEventButton);
-      event->type = GDK_BUTTON_PRESS;
-      event->window = gdk_get_default_root_window ();
+      event = gdk_event_new (GDK_BUTTON_PRESS);
+      event->button.window = g_object_ref( gdk_get_default_root_window ());
     }
 
   /* popup the menu */
@@ -1426,4 +1426,6 @@ window_menu_plugin_menu (GtkWidget        *button,
 
   xfce_panel_plugin_popup_menu (XFCE_PANEL_PLUGIN (plugin), GTK_MENU (menu),
                                 button, (GdkEvent *) event);
+
+  gdk_event_free (event);
 }
