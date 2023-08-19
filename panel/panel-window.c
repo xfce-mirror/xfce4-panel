xfce-mirror/xfce4-panel

commit fc42275642ae5b55c745f04b95a3c03eee275251

panel: Always include borders to compute snap position
If a motion event is sent twice in a row for the same pointer position
around `SNAP_DISTANCE` (this can happen if the pointer moves slowly),
the computation returns a snap position once, and `SNAP_POSITION_NONE`
the other time, because the panel has lost a pixel in between. This
makes the panel blink or bounce against the edge of the screen. Easier
to reproduce on Wayland.

panel/panel-window.c
@@ -2063,14 +2063,33 @@ panel_window_snap_edge_gravity (gint value,
 static SnapPosition
 panel_window_snap_position (PanelWindow *window)
 {
-  guint         snap_horz, snap_vert;
-  GdkRectangle *alloc = &window->alloc;
+  guint snap_horz, snap_vert;
+  GdkRectangle alloc = window->alloc;
+  PanelBorders borders;
+
+  /* make the same calculation whether the panel is snapped or not (avoids flickering
+   * when the pointer moves slowly) */
+  borders = panel_base_window_get_borders (PANEL_BASE_WINDOW (window));
+  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_TOP))
+    alloc.height++;
+  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_BOTTOM))
+    {
+      alloc.y--;
+      alloc.height++;
+    }
+  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_LEFT))
+    alloc.width++;
+  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_RIGHT))
+    {
+      alloc.x--;
+      alloc.width++;
+    }
 
   /* get the snap offsets */
-  snap_horz = panel_window_snap_edge_gravity (alloc->x, window->area.x,
-      window->area.x + window->area.width - alloc->width);
-  snap_vert = panel_window_snap_edge_gravity (alloc->y, window->area.y,
-      window->area.y + window->area.height - alloc->height);
+  snap_horz = panel_window_snap_edge_gravity (alloc.x, window->area.x,
+      window->area.x + window->area.width - alloc.width);
+  snap_vert = panel_window_snap_edge_gravity (alloc.y, window->area.y,
+      window->area.y + window->area.height - alloc.height);
 
   /* detect the snap mode */
   if (snap_horz == EDGE_GRAVITY_START)

commit 9a48ae8fa691380a03655e27e638a0e412677dbd

panel: Change panel border only if opposite border is set
In particular, do not change borders if none are set, as is the case
when the background is a solid color or an image. This caused a
regression in the application of struts or autohide, as the panel could
end up one pixel after the edge of the screen.

Fixes: fc42275642ae5b55c745f04b95a3c03eee275251

panel/panel-window.c
@@ -2259,16 +2259,16 @@ panel_window_snap_position (PanelWindow *window)
   /* make the same calculation whether the panel is snapped or not (avoids flickering
    * when the pointer moves slowly) */
   borders = panel_base_window_get_borders (PANEL_BASE_WINDOW (window));
-  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_TOP))
+  if (!PANEL_HAS_FLAG (borders, PANEL_BORDER_TOP) && PANEL_HAS_FLAG (borders, PANEL_BORDER_BOTTOM))
     alloc.height++;
-  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_BOTTOM))
+  else if (PANEL_HAS_FLAG (borders, PANEL_BORDER_TOP) && !PANEL_HAS_FLAG (borders, PANEL_BORDER_BOTTOM))
     {
       alloc.y--;
       alloc.height++;
     }
-  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_LEFT))
+  if (!PANEL_HAS_FLAG (borders, PANEL_BORDER_LEFT) && PANEL_HAS_FLAG (borders, PANEL_BORDER_RIGHT))
     alloc.width++;
-  if (! PANEL_HAS_FLAG (borders, PANEL_BORDER_RIGHT))
+  else if (PANEL_HAS_FLAG (borders, PANEL_BORDER_LEFT) && !PANEL_HAS_FLAG (borders, PANEL_BORDER_RIGHT))
     {
       alloc.x--;
       alloc.width++;