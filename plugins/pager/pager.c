a62a20a3091a88524169e64dc5a626a873ffab9f
Gaël Bonithon <gael@xfce.org>
2023-08-20 19:27:15 +0200

    pager: Delay GdkScreen signal handlers
    This no longer seems necessary after the switch to libxfce4windowing for
    some reason, but it could become necessary again, and it's not a problem
    to give libwnck a little time to update the layout.
    
    Fixes: #784
    (cherry picked from commit 69a8bdf1834740570d7bf8064dd8869d353c3642)

--- plugins/pager/pager.c
+++ plugins/pager/pager.c
@@ -74,7 +74,8 @@ static void     pager_plugin_mode_changed                 (XfcePanelPlugin     *
 static void     pager_plugin_configure_workspace_settings (GtkWidget         *button);
 static void     pager_plugin_configure_plugin             (XfcePanelPlugin   *panel_plugin);
 static gpointer pager_plugin_get_master_plugin            (PagerPlugin       *plugin);
-static void     pager_plugin_screen_layout_changed        (PagerPlugin       *plugin);
+static void     pager_plugin_screen_layout_changed        (PagerPlugin       *plugin,
+                                                           GdkScreen         *screen);
 static void     pager_plugin_get_preferred_width          (GtkWidget           *widget,
                                                            gint                *minimum_width,
                                                            gint                *natural_width);
@@ -293,7 +294,7 @@ pager_plugin_set_property (GObject      *object,
     case PROP_MINIATURE_VIEW:
       plugin->miniature_view = g_value_get_boolean (value);
       if (plugin->wnck_screen != NULL)
-        pager_plugin_screen_layout_changed (plugin);
+        pager_plugin_screen_layout_changed (plugin, NULL);
       break;
 
     case PROP_ROWS:
@@ -330,7 +331,7 @@ pager_plugin_set_property (GObject      *object,
               g_object_set (master_plugin, "rows", rows, NULL);
             }
           else
-            pager_plugin_screen_layout_changed (plugin);
+            pager_plugin_screen_layout_changed (plugin, NULL);
         }
       break;
 
@@ -535,7 +536,7 @@ pager_plugin_screen_layout_changed_idle (gpointer data)
       return TRUE;
     }
 
-  pager_plugin_screen_layout_changed (plugin);
+  pager_plugin_screen_layout_changed (plugin, NULL);
   plugin->sync_wait = TRUE;
   plugin->sync_idle_id = 0;
 
@@ -545,7 +546,8 @@ pager_plugin_screen_layout_changed_idle (gpointer data)
 
 
 static void
-pager_plugin_screen_layout_changed (PagerPlugin *plugin)
+pager_plugin_screen_layout_changed (PagerPlugin *plugin,
+                                    GdkScreen *screen)
 {
   XfcePanelPluginMode mode;
   GtkOrientation      orientation;
@@ -555,7 +557,8 @@ pager_plugin_screen_layout_changed (PagerPlugin *plugin)
 
   /* changing workspace layout is delayed in Libwnck code, so we have to give time
    * to the master plugin request to be processed */
-  if (plugin != pager_plugin_get_master_plugin (plugin) && plugin->sync_idle_id == 0)
+  if ((plugin != pager_plugin_get_master_plugin (plugin) || screen != NULL)
+      && plugin->sync_idle_id == 0)
     {
       plugin->sync_idle_id =
         g_idle_add_full (G_PRIORITY_LOW, pager_plugin_screen_layout_changed_idle, plugin, NULL);
@@ -634,7 +637,7 @@ pager_plugin_screen_changed (GtkWidget *widget,
     {
       plugin->wnck_screen = wnck_screen;
 
-      pager_plugin_screen_layout_changed (plugin);
+      pager_plugin_screen_layout_changed (plugin, NULL);
 
       g_signal_connect_object (G_OBJECT (screen), "monitors-changed",
          G_CALLBACK (pager_plugin_screen_layout_changed), plugin, G_CONNECT_SWAPPED);
