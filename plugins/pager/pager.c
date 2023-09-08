xfce-mirror/xfce4-panel

commit 8f310b868646036b6eb1694005b667f22229b0b1

pager: Fix aspect ratio for viewport (Fixes #119)

plugins/pager/pager.c
@@ -488,6 +488,33 @@ pager_plugin_get_master_plugin (PagerPlugin *plugin)
 
 
 
+static void
+pager_plugin_set_ratio (PagerPlugin *plugin)
+{
+  WnckWorkspace *workspace;
+
+  g_signal_handlers_disconnect_by_func (plugin->wnck_screen, pager_plugin_set_ratio, plugin);
+
+  workspace = wnck_screen_get_active_workspace (plugin->wnck_screen);
+  if (workspace == NULL)
+    {
+      /* this is the right signal to get relevant information about the virtual
+       * nature of active workspace, instead of "active-workspace-changed" */
+      g_signal_connect_swapped (plugin->wnck_screen, "window-manager-changed",
+                                G_CALLBACK (pager_plugin_set_ratio), plugin);
+      return;
+    }
+
+  if (wnck_workspace_is_virtual (workspace))
+    plugin->ratio = (gfloat) wnck_workspace_get_width (workspace)
+                    / (gfloat) wnck_workspace_get_height (workspace);
+  else
+    plugin->ratio = (gfloat) wnck_screen_get_width (plugin->wnck_screen)
+                    / (gfloat) wnck_screen_get_height (plugin->wnck_screen);
+}
+
+
+
 static gboolean
 pager_plugin_screen_layout_changed_idle (gpointer data)
 {
@@ -542,10 +569,9 @@ pager_plugin_screen_layout_changed (PagerPlugin *plugin)
 
   if (plugin->miniature_view)
     {
+      pager_plugin_set_ratio (plugin);
+
       plugin->pager = wnck_pager_new ();
-G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-      plugin->ratio = (gfloat) gdk_screen_width () / (gfloat) gdk_screen_height ();
-G_GNUC_END_IGNORE_DEPRECATIONS
       g_signal_connect_after (G_OBJECT (plugin->pager), "drag-begin",
                               G_CALLBACK (pager_plugin_drag_begin_event), plugin);
       g_signal_connect_after (G_OBJECT (plugin->pager), "drag-end",

commit b997c483b590c4b3b89b63dfa3fc596f0e3f0a40

pager: Fix aspect ratio for viewport (V2)
It seems that the number of workspaces contained in a viewport is the
ratio of widths, at least when there is only one line. Not sure we
really support other cases.

Closes: #789
Fixes: 8f310b868646036b6eb1694005b667f22229b0b1

plugins/pager/pager.c
@@ -509,14 +509,14 @@ pager_plugin_set_ratio (PagerPlugin *plugin)
       return;
     }
 
+  plugin->ratio = (gfloat) panel_screen_get_width (screen)
+                  / (gfloat) panel_screen_get_height (screen);
   if (xfw_workspace_get_state (workspace) & XFW_WORKSPACE_STATE_VIRTUAL)
     {
       GdkRectangle *rect = xfw_workspace_get_geometry (workspace);
-      plugin->ratio = (gfloat) rect->width / (gfloat) rect->height;
+      gint scale_factor = gtk_widget_get_scale_factor (GTK_WIDGET (plugin));
+      plugin->ratio *= rect->width / (panel_screen_get_width (screen) * scale_factor);
     }
-  else
-    plugin->ratio = (gfloat) panel_screen_get_width (screen)
-                    / (gfloat) panel_screen_get_height (screen);
 }
 #endif
 