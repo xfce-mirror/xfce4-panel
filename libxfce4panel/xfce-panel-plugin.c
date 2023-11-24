xfce-mirror/xfce4-panel

commit 4de9f769d079ffd4971369277d64fb0503a8bb6e

wayland: Avoid context menu overlay by external plugins

libxfce4panel/xfce-panel-plugin.c
@@ -1029,7 +1029,15 @@ xfce_panel_plugin_button_press_event (GtkWidget      *widget,
         gtk_widget_set_sensitive (item, plugin->priv->menu_blocked == 0);
 
       /* popup the menu */
-      gtk_menu_popup_at_pointer (menu, (GdkEvent *) event);
+      if (gtk_layer_is_supported ())
+        {
+          /* on Wayland the menu might be covered by external plugins when they are
+           * usable, i.e. if layer-shell is supported, so pop up it at widget */
+          xfce_panel_plugin_popup_menu (plugin, menu, widget, (GdkEvent *) event);
+        }
+      else
+        gtk_menu_popup_at_pointer (menu, (GdkEvent *) event);
+
       return TRUE;
     }
 

panel/panel-window.c
@@ -3656,7 +3656,38 @@ panel_window_menu_popup (PanelWindow    *window,
       gtk_widget_show (item);
     }
 
-  gtk_menu_popup_at_pointer (GTK_MENU (menu), (GdkEvent *) event);
+  if (gtk_layer_is_supported ())
+    {
+      /* on Wayland the menu might be covered by external plugins when they are
+       * usable, i.e. if layer-shell is supported, so pop up it at rect */
+      GdkRectangle rect;
+      GdkGravity rect_anchor, menu_anchor;
+
+      if (IS_HORIZONTAL (window))
+        {
+          /* only set one typical case, for the others GTK should manage via anchor-hints */
+          rect.x = event->x;
+          rect.y = 0;
+          rect.width = window->alloc.width - event->x;
+          rect.height = window->alloc.height;
+          rect_anchor = GDK_GRAVITY_NORTH_WEST;
+          menu_anchor = GDK_GRAVITY_SOUTH_WEST;
+        }
+      else
+        {
+          rect.x = 0;
+          rect.y = event->y;
+          rect.width = window->alloc.width;
+          rect.height = window->alloc.height - event->y;
+          rect_anchor = GDK_GRAVITY_NORTH_WEST;
+          menu_anchor = GDK_GRAVITY_NORTH_EAST;
+        }
+
+      gtk_menu_popup_at_rect (GTK_MENU (menu), gtk_widget_get_window (GTK_WIDGET (window)),
+                              &rect, rect_anchor, menu_anchor, (GdkEvent *) event);
+    }
+  else
+    gtk_menu_popup_at_pointer (GTK_MENU (menu), (GdkEvent *) event);
 }
 
 

commit 74ba1b0945f72c3a033c4b4903eee1b350dce829

wayland: libxfce4panel: Fix broken autohide when showing context menu
Fixes: 4de9f769d079ffd4971369277d64fb0503a8bb6e

libxfce4panel/xfce-panel-plugin.c
@@ -1037,6 +1037,7 @@ xfce_panel_plugin_button_press_event (GtkWidget      *widget,
         {
           /* on Wayland the menu might be covered by external plugins when they are
            * usable, i.e. if layer-shell is supported, so pop up it at widget */
+          xfce_panel_plugin_unregister_menu (menu, plugin);
           xfce_panel_plugin_popup_menu (plugin, menu, widget, (GdkEvent *) event);
         }
       else