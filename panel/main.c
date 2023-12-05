xfce-mirror/xfce4-panel

commit 3e59a4e4cfad668415275d21d6e290d8fd81056b

build: Restrict XfceSMClient to X11
Libxfce4ui >= 4.19.4 can be built without X11 support and therefore
whithout XfceSMClient.

panel/main.c
@@ -147,6 +147,7 @@ panel_signal_handler (gint signum)
 
 
 
+#ifdef ENABLE_X11
 static void
 panel_sm_client_quit (XfceSMClient *sm_client)
 {
@@ -158,6 +159,7 @@ panel_sm_client_quit (XfceSMClient *sm_client)
 
   gtk_main_quit ();
 }
+#endif
 
 
 
@@ -238,7 +240,9 @@ main (gint argc, gchar **argv)
   guint             i;
   const gint        signums[] = { SIGINT, SIGQUIT, SIGTERM, SIGABRT, SIGUSR1 };
   const gchar      *error_msg;
+#ifdef ENABLE_X11
   XfceSMClient     *sm_client;
+#endif
 
   panel_debug (PANEL_DEBUG_MAIN,
                "version %s on gtk+ %d.%d.%d (%d.%d.%d), glib %d.%d.%d (%d.%d.%d)",
@@ -274,7 +278,10 @@ main (gint argc, gchar **argv)
   context = g_option_context_new (_("[ARGUMENTS...]"));
   g_option_context_add_main_entries (context, option_entries, GETTEXT_PACKAGE);
   g_option_context_add_group (context, gtk_get_option_group (TRUE));
-  g_option_context_add_group (context, xfce_sm_client_get_option_group (argc, argv));
+#ifdef ENABLE_X11
+  if (WINDOWING_IS_X11 ())
+    g_option_context_add_group (context, xfce_sm_client_get_option_group (argc, argv));
+#endif
   if (!g_option_context_parse (context, &argc, &argv, &error))
     {
       g_print ("%s: %s.\n", PACKAGE_NAME, error->message);
@@ -360,18 +367,21 @@ main (gint argc, gchar **argv)
   /* start dbus service */
   dbus_service = panel_dbus_service_get ();
 
+#ifdef ENABLE_X11
   /* start session management */
-  sm_client = xfce_sm_client_get ();
-  xfce_sm_client_set_restart_style (sm_client, XFCE_SM_CLIENT_RESTART_IMMEDIATELY);
-  xfce_sm_client_set_priority (sm_client, XFCE_SM_CLIENT_PRIORITY_CORE);
-  g_signal_connect (G_OBJECT (sm_client), "quit",
-      G_CALLBACK (panel_sm_client_quit), NULL);
-  if (!xfce_sm_client_connect (sm_client, &error))
+  if (WINDOWING_IS_X11 ())
     {
-      g_printerr ("%s: Failed to connect to session manager: %s\n",
-                  G_LOG_DOMAIN, error->message);
-      g_clear_error (&error);
+      sm_client = xfce_sm_client_get ();
+      xfce_sm_client_set_restart_style (sm_client, XFCE_SM_CLIENT_RESTART_IMMEDIATELY);
+      xfce_sm_client_set_priority (sm_client, XFCE_SM_CLIENT_PRIORITY_CORE);
+      g_signal_connect (G_OBJECT (sm_client), "quit", G_CALLBACK (panel_sm_client_quit), NULL);
+      if (!xfce_sm_client_connect (sm_client, &error))
+        {
+          g_warning ("Failed to connect to session manager: %s", error->message);
+          g_clear_error (&error);
+        }
     }
+#endif
 
   /* setup signal handlers to properly quit the main loop */
   for (i = 0; i < G_N_ELEMENTS (signums); i++)
@@ -404,7 +414,10 @@ G_GNUC_END_IGNORE_DEPRECATIONS
       g_print ("%s: %s\n\n", G_LOG_DOMAIN, _("There is already a running instance"));
     }
 
-  g_object_unref (G_OBJECT (sm_client));
+#ifdef ENABLE_X11
+  if (WINDOWING_IS_X11 ())
+    g_object_unref (G_OBJECT (sm_client));
+#endif
 
   if (panel_dbus_service_get_restart ())
     {

panel/panel-application.c
@@ -1764,23 +1764,32 @@ panel_application_get_locked (PanelApplication *application)
 void
 panel_application_logout (void)
 {
-  XfceSMClient *sm_client;
-  GError       *error = NULL;
-  const gchar  *command = "xfce4-session-logout";
+  GError *error = NULL;
+  const gchar *command = "xfce4-session-logout";
 
+#ifdef ENABLE_X11
   /* first try to session client to logout else fallback and spawn xfce4-session-logout */
-  sm_client = xfce_sm_client_get ();
-  if (xfce_sm_client_is_connected (sm_client))
+  if (WINDOWING_IS_X11 ())
     {
-      xfce_sm_client_request_shutdown (sm_client, XFCE_SM_CLIENT_SHUTDOWN_HINT_ASK);
-
-      return;
+      XfceSMClient *sm_client = xfce_sm_client_get ();
+      if (xfce_sm_client_is_connected (sm_client))
+        {
+          xfce_sm_client_request_shutdown (sm_client, XFCE_SM_CLIENT_SHUTDOWN_HINT_ASK);
+          g_object_unref (sm_client);
+          return;
+        }
+      g_object_unref (sm_client);
     }
-  else if (g_getenv ("SESSION_MANAGER") == NULL)
+#endif
+
+  if (g_getenv ("SESSION_MANAGER") == NULL)
     {
-      if (xfce_dialog_confirm (NULL, "application-exit", _("Quit"),
-          _("You have started X without session manager. Clicking Quit will close the X server."),
-          _("Are you sure you want to quit the panel?")))
+      const gchar *text = NULL;
+      if (WINDOWING_IS_X11 ())
+        text = _("You have started X without session manager. Clicking Quit will close the X server.");
+
+      if (xfce_dialog_confirm (NULL, "application-exit", _("Quit"), text,
+                               _("Are you sure you want to quit the panel?")))
         command = "xfce4-panel --quit";
       else
         return;

panel/panel-dbus-service.c
@@ -469,10 +469,14 @@ panel_dbus_service_get (void)
 void
 panel_dbus_service_exit_panel (gboolean restart)
 {
-  XfceSMClient *sm_client;
-
-  sm_client = xfce_sm_client_get ();
-  xfce_sm_client_set_restart_style (sm_client, XFCE_SM_CLIENT_RESTART_NORMAL);
+#ifdef ENABLE_X11
+  if (WINDOWING_IS_X11 ())
+    {
+      XfceSMClient *sm_client = xfce_sm_client_get ();
+      xfce_sm_client_set_restart_style (sm_client, XFCE_SM_CLIENT_RESTART_NORMAL);
+      g_object_unref (sm_client);
+    }
+#endif
 
   dbus_exit_restart = !!restart;
 

commit c3c7a9ec522fb2caaa8b1fff630d1197d100798d

panel: Call gtk_init() early enough
Fixes: 3e59a4e4cfad668415275d21d6e290d8fd81056b

panel/main.c
@@ -274,6 +274,9 @@ main (gint argc, gchar **argv)
 
   g_setenv ("GDK_CORE_DEVICE_EVENTS", "1", TRUE);
 
+  /* we need to do this right now to be able to determine the windowing system used below */
+  gtk_init (&argc, &argv);
+
   /* parse context options */
   context = g_option_context_new (_("[ARGUMENTS...]"));
   g_option_context_add_main_entries (context, option_entries, GETTEXT_PACKAGE);
@@ -292,8 +295,6 @@ main (gint argc, gchar **argv)
       return EXIT_FAILURE;
     }
   g_option_context_free (context);
-
-  gtk_init (&argc, &argv);
 
   if (opt_version)
     {