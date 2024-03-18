xfce-mirror/xfce4-panel

commit 8d2b1eae5c3297e17b7add3fcf86e34f2b5d6650

pager: Use WnckHandle if available
In the current state of the code on the master branch, this is probably
not necessary, as Libxfce4windowing >= 4.19.1 no longer uses wnck icons.
But it does reduce the distance from the 4.18 branch, where this change
is made necessary by the use of `wnck_set_default_icon_size()` in
tasklist and windowmenu.

Also, if plugins are forced to run as internal via "force-all-internal",
it is possible that one of them will change this icon size. And in any
case, it is probably better to use this API when possible, since it
allows to fix a deprecation.

Related: #695

plugins/pager/pager.c
@@ -114,6 +114,11 @@ struct _PagerPlugin
 
   XfwScreen     *xfw_screen;
   XfwWorkspaceGroup *workspace_group;
+#ifdef HAVE_LIBWNCK
+#if WNCK_CHECK_VERSION (43, 0, 0)
+  WnckHandle    *wnck_handle;
+#endif
+#endif
 
   /* settings */
   guint          scrolling : 1;
@@ -225,6 +230,11 @@ pager_plugin_init (PagerPlugin *plugin)
   plugin->pager = NULL;
   plugin->sync_idle_id = 0;
   plugin->sync_wait = TRUE;
+#ifdef HAVE_LIBWNCK
+#if WNCK_CHECK_VERSION (43, 0, 0)
+  plugin->wnck_handle = wnck_handle_new (WNCK_CLIENT_TYPE_PAGER);
+#endif
+#endif
 
   master_plugin = pager_plugin_get_master_plugin (plugin);
   if (master_plugin == NULL)
@@ -587,9 +597,13 @@ pager_plugin_screen_layout_changed (PagerPlugin *plugin)
     {
       pager_plugin_set_ratio (plugin);
 
-G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+#if WNCK_CHECK_VERSION (43, 0, 0)
+      /* using a handle allows in particular the pager not to be affected by a possible
+       * change of default wnck icon size in other plugins */
+      plugin->pager = wnck_pager_new_with_handle (plugin->wnck_handle);
+#else
       plugin->pager = wnck_pager_new ();
-G_GNUC_END_IGNORE_DEPRECATIONS
+#endif
       g_signal_connect_after (G_OBJECT (plugin->pager), "drag-begin",
                               G_CALLBACK (pager_plugin_drag_begin_event), plugin);
       g_signal_connect_after (G_OBJECT (plugin->pager), "drag-end",
@@ -705,6 +719,12 @@ pager_plugin_free_data (XfcePanelPlugin *panel_plugin)
 
   g_signal_handlers_disconnect_by_func (G_OBJECT (plugin),
       pager_plugin_screen_changed, NULL);
+
+#ifdef HAVE_LIBWNCK
+#if WNCK_CHECK_VERSION (43, 0, 0)
+  g_object_unref (plugin->wnck_handle);
+#endif
+#endif
 
   plugin_list = g_slist_remove (plugin_list, plugin);
   if (plugin->sync_idle_id != 0)

commit ddd7eb8504249bc2084123d476c815d04c713373

wayland: pager: Add missing runtime check
Fixes: 8d2b1eae5c3297e17b7add3fcf86e34f2b5d6650

plugins/pager/pager.c
@@ -221,7 +221,8 @@ pager_plugin_init (PagerPlugin *plugin)
   plugin->sync_wait = TRUE;
 #ifdef ENABLE_X11
 #if WNCK_CHECK_VERSION (43, 0, 0)
-  plugin->wnck_handle = wnck_handle_new (WNCK_CLIENT_TYPE_PAGER);
+  if (WINDOWING_IS_X11 ())
+    plugin->wnck_handle = wnck_handle_new (WNCK_CLIENT_TYPE_PAGER);
 #endif
 #endif
 
@@ -716,7 +717,8 @@ pager_plugin_free_data (XfcePanelPlugin *panel_plugin)
 
 #ifdef ENABLE_X11
 #if WNCK_CHECK_VERSION (43, 0, 0)
-  g_object_unref (plugin->wnck_handle);
+  if (plugin->wnck_handle != NULL)
+    g_object_unref (plugin->wnck_handle);
 #endif
 #endif
 