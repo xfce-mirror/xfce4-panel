xfce-mirror/xfce4-panel

commit 005aeb2ddeead5b5fadd4c40eeb92206e09a80c4

Check for xfconf init in main.c and exit on failure
No need to complicate things with panel_properties_get_channel(), we
need xfconf anyway.

common/panel-xfconf.c
@@ -65,29 +65,6 @@ panel_properties_store_value (XfconfChannel *channel,
 
 
 
-XfconfChannel *
-panel_properties_get_channel (GObject *object_for_weak_ref)
-{
-  GError        *error = NULL;
-  XfconfChannel *channel;
-
-  panel_return_val_if_fail (G_IS_OBJECT (object_for_weak_ref), NULL);
-
-  if (!xfconf_init (&error))
-    {
-      g_critical ("Failed to initialize Xfconf: %s", error->message);
-      g_error_free (error);
-      return NULL;
-    }
-
-  channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
-  g_object_weak_ref (object_for_weak_ref, (GWeakNotify) xfconf_shutdown, NULL);
-
-  return channel;
-}
-
-
-
 void
 panel_properties_bind (XfconfChannel       *channel,
                        GObject             *object,
@@ -104,8 +81,7 @@ panel_properties_bind (XfconfChannel       *channel,
   panel_return_if_fail (properties != NULL);
 
   if (G_LIKELY (channel == NULL))
-    channel = panel_properties_get_channel (object);
-  panel_return_if_fail (XFCONF_IS_CHANNEL (channel));
+    channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
 
   /* walk the properties array */
   for (prop = properties; prop->property != NULL; prop++)

common/panel-xfconf.h
@@ -32,8 +32,6 @@ struct _PanelProperty
 
 
 
-XfconfChannel *panel_properties_get_channel          (GObject             *object_for_weak_ref);
-
 void           panel_properties_bind                 (XfconfChannel       *channel,
                                                       GObject             *object,
                                                       const gchar         *property_base,

plugins/clock/clock-digital.c
@@ -384,7 +384,7 @@ xfce_clock_digital_anchored (XfceClockDigital *digital)
   g_signal_handlers_disconnect_by_func (digital, xfce_clock_digital_anchored, NULL);
 
   plugin = gtk_widget_get_ancestor (GTK_WIDGET (digital), XFCE_TYPE_PANEL_PLUGIN);
-  channel = panel_properties_get_channel (G_OBJECT (plugin));
+  channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
   prop_base = xfce_panel_plugin_get_property_base (XFCE_PANEL_PLUGIN (plugin));
 
   /* see if any of the new properties are set */

panel/main.c
@@ -349,6 +349,13 @@ main (gint argc, gchar **argv)
 
   launch_panel:
 
+  if (!xfconf_init (&error))
+    {
+      g_critical ("Failed to initialize Xfconf: %s", error->message);
+      g_error_free (error);
+      return EXIT_FAILURE;
+    }
+
   g_bus_own_name (G_BUS_TYPE_SESSION,
                   PANEL_DBUS_NAME,
                   G_BUS_NAME_OWNER_FLAGS_NONE,
@@ -413,6 +420,8 @@ main (gint argc, gchar **argv)
       g_print ("%s: %s\n\n", G_LOG_DOMAIN, _("Restarting..."));
       g_spawn_command_line_async (argv[0], NULL);
     }
+
+  xfconf_shutdown ();
 
   return EXIT_SUCCESS;
 

panel/panel-application.c
@@ -205,7 +205,7 @@ panel_application_init (PanelApplication *application)
   application->autohide_block = 0;
 
   /* get the xfconf channel (singleton) */
-  application->xfconf = panel_properties_get_channel (G_OBJECT (application));
+  application->xfconf = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
 
   /* check if we need to migrate configuration */
   configver = xfconf_channel_get_int (application->xfconf, "/configver", -1);

commit 4a1c2b9a5613f75ce1e369ade6486b17137dd55d

Reintroduce panel_properties_get_channel()
It's actually needed since plugins can be run as external.

Fixes: 005aeb2ddeead5b5fadd4c40eeb92206e09a80c4

common/panel-xfconf.c
@@ -65,6 +65,29 @@ panel_properties_store_value (XfconfChannel *channel,
 
 
 
+XfconfChannel *
+panel_properties_get_channel (GObject *object_for_weak_ref)
+{
+  GError        *error = NULL;
+  XfconfChannel *channel;
+
+  panel_return_val_if_fail (G_IS_OBJECT (object_for_weak_ref), NULL);
+
+  if (!xfconf_init (&error))
+    {
+      g_critical ("Failed to initialize Xfconf: %s", error->message);
+      g_error_free (error);
+      return NULL;
+    }
+
+  channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
+  g_object_weak_ref (object_for_weak_ref, (GWeakNotify) xfconf_shutdown, NULL);
+
+  return channel;
+}
+
+
+
 void
 panel_properties_bind (XfconfChannel       *channel,
                        GObject             *object,
@@ -81,7 +104,8 @@ panel_properties_bind (XfconfChannel       *channel,
   panel_return_if_fail (properties != NULL);
 
   if (G_LIKELY (channel == NULL))
-    channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
+    channel = panel_properties_get_channel (object);
+  panel_return_if_fail (channel != NULL);
 
   /* walk the properties array */
   for (prop = properties; prop->property != NULL; prop++)

common/panel-xfconf.h
@@ -32,6 +32,8 @@ struct _PanelProperty
 
 
 
+XfconfChannel *panel_properties_get_channel          (GObject             *object_for_weak_ref);
+
 void           panel_properties_bind                 (XfconfChannel       *channel,
                                                       GObject             *object,
                                                       const gchar         *property_base,

plugins/clock/clock-digital.c
@@ -384,8 +384,9 @@ xfce_clock_digital_anchored (XfceClockDigital *digital)
   g_signal_handlers_disconnect_by_func (digital, xfce_clock_digital_anchored, NULL);
 
   plugin = gtk_widget_get_ancestor (GTK_WIDGET (digital), XFCE_TYPE_PANEL_PLUGIN);
-  channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
+  channel = panel_properties_get_channel (G_OBJECT (plugin));
   prop_base = xfce_panel_plugin_get_property_base (XFCE_PANEL_PLUGIN (plugin));
+  panel_return_if_fail (channel != NULL);
 
   /* see if any of the new properties are set */
   for (guint n = 0; n < G_N_ELEMENTS (props); n++)