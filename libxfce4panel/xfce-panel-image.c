xfce-mirror/xfce4-panel

commit 0c56766b0f6c73a666889f07d063993e77f002eb

Clean up and sort includes
Better make a separate commit for that too, as I'm still removing quite
a lot, beyond just sorting.

libxfce4panel/xfce-panel-image.c
@@ -17,24 +17,18 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
 
-#ifdef HAVE_MATH_H
-#include <math.h>
-#endif
-#ifdef HAVE_STRING_H
-#include <string.h>
-#endif
+#include "libxfce4panel/xfce-panel-convenience.h"
+#include "libxfce4panel/xfce-panel-image.h"
+#include "libxfce4panel/xfce-panel-macros.h"
 
-#include <gtk/gtk.h>
+#include "libxfce4panel/libxfce4panel-alias.h"
+
+#include "common/panel-private.h"
+
 #include <libxfce4util/libxfce4util.h>
-
-#include <common/panel-private.h>
-#include <libxfce4panel/xfce-panel-macros.h>
-#include <libxfce4panel/xfce-panel-image.h>
-#include <libxfce4panel/xfce-panel-convenience.h>
-#include <libxfce4panel/libxfce4panel-alias.h>
 
 
 
@@ -856,4 +850,4 @@ xfce_panel_image_clear (XfcePanelImage *image)
 
 
 #define __XFCE_PANEL_IMAGE_C__
-#include <libxfce4panel/libxfce4panel-aliasdef.c>
+#include "libxfce4panel/libxfce4panel-aliasdef.c"

commit 0f786ce451297f8d26d41dc8492c10db08d8f87f

build: Add missing math.h include
Fixes: 0c56766b0f6c73a666889f07d063993e77f002eb
Closes: #833

libxfce4panel/xfce-panel-image.c
@@ -30,6 +30,10 @@
 
 #include <libxfce4util/libxfce4util.h>
 
+#ifdef HAVE_MATH_H
+#include <math.h>
+#endif
+
 
 
 /**