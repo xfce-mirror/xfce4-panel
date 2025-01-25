xfce-mirror/xfce4-panel

commit 3811371b020a23085f9d2fc59d4c2a5922d1ac7c

actions: Use xfce_message_dialog_new in confirmation dialogs
To make the UX more consistent with other confirmation dialogs.

Fixes https://github.com/shimmerproject/elementary-xfce/issues/450.

plugins/actions/actions.c
@@ -153,6 +153,8 @@ typedef struct
 {
   ActionEntry *entry;
   GtkWidget *dialog;
+  GtkLabel *label;
+  gchar *initial_label_text;
   gint time_left;
   guint unattended : 1;
 } ActionTimeout;
@@ -831,14 +833,36 @@ actions_plugin_action_confirmation_time (gpointer data)
       gtk_dialog_response (GTK_DIALOG (timeout->dialog),
                            GTK_RESPONSE_ACCEPT);
     }
-  else
+  else if (timeout->label != NULL)
     {
-      gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (timeout->dialog),
-                                                _(timeout->entry->status),
-                                                timeout->time_left);
+      gchar *new_label_text = g_strdup_printf (_(timeout->entry->status), timeout->time_left);
+      gtk_label_set_text (GTK_LABEL (timeout->label), new_label_text);
+      g_free (new_label_text);
     }
 
   return --timeout->time_left >= 0;
+}
+
+
+
+static void
+actions_plugin_action_confirmation_dialog_get_label (GtkWidget *widget,
+                                                     gpointer data)
+{
+  ActionTimeout *timeout = data;
+
+  panel_return_if_fail (widget);
+  panel_return_if_fail (timeout && timeout->initial_label_text);
+
+  if (GTK_IS_LABEL (widget) && g_strcmp0 (timeout->initial_label_text, gtk_label_get_text (GTK_LABEL (widget))) == 0)
+    {
+      if (timeout->label)
+        g_warning ("%s: Found multiple labels with text value '%s'", G_STRFUNC, timeout->initial_label_text);
+      else
+        timeout->label = GTK_LABEL (widget);
+    }
+  else if (GTK_IS_BOX (widget))
+    gtk_container_foreach (GTK_CONTAINER (widget), actions_plugin_action_confirmation_dialog_get_label, data);
 }
 
 
@@ -849,38 +873,39 @@ actions_plugin_action_confirmation (ActionsPlugin *plugin,
                                     gboolean *unattended)
 {
   GtkWidget *dialog;
-  GtkWidget *button;
   gint result;
-  GtkWidget *image;
+  const gchar *icon_name;
   ActionTimeout *timeout;
   guint timeout_id;
 
   panel_return_val_if_fail (entry->question != NULL, FALSE);
   panel_return_val_if_fail (entry->status != NULL, FALSE);
 
-  dialog = gtk_message_dialog_new (NULL, 0,
-                                   GTK_MESSAGE_QUESTION, GTK_BUTTONS_CANCEL,
-                                   "%s", _(entry->question));
-  gtk_window_set_keep_above (GTK_WINDOW (dialog), TRUE);
-  gtk_window_stick (GTK_WINDOW (dialog));
-  gtk_window_set_skip_taskbar_hint (GTK_WINDOW (dialog), TRUE);
-  gtk_window_set_title (GTK_WINDOW (dialog), _(entry->display_name));
-
-  button = gtk_dialog_add_button (GTK_DIALOG (dialog), _(entry->mnemonic), GTK_RESPONSE_ACCEPT);
-  gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
-
   if (gtk_icon_theme_has_icon (gtk_icon_theme_get_default (), entry->icon_name))
-    image = gtk_image_new_from_icon_name (entry->icon_name, GTK_ICON_SIZE_BUTTON);
+    icon_name = entry->icon_name;
   else
-    image = gtk_image_new_from_icon_name (entry->fallback_icon_name, GTK_ICON_SIZE_BUTTON);
-
-  gtk_button_set_image (GTK_BUTTON (button), image);
+    icon_name = entry->fallback_icon_name;
 
   timeout = g_slice_new0 (ActionTimeout);
   timeout->entry = entry;
   timeout->time_left = DEFAULT_TIMEOUT;
-  timeout->dialog = dialog;
+  timeout->label = NULL;
+  timeout->initial_label_text = g_strdup_printf (_(entry->status), DEFAULT_TIMEOUT);
   timeout->unattended = FALSE;
+
+  dialog = xfce_message_dialog_new (NULL, _(entry->display_name), "dialog-question", _(entry->question),
+                                    timeout->initial_label_text, _("_Cancel"), GTK_RESPONSE_CANCEL,
+                                    XFCE_BUTTON_TYPE_MIXED, icon_name, _(entry->mnemonic), GTK_RESPONSE_ACCEPT,
+                                    NULL);
+  gtk_window_set_keep_above (GTK_WINDOW (dialog), TRUE);
+  gtk_window_stick (GTK_WINDOW (dialog));
+  gtk_window_set_skip_taskbar_hint (GTK_WINDOW (dialog), TRUE);
+
+  timeout->dialog = dialog;
+  /* obtain timeout->label, used in actions_plugin_action_confirmation_time */
+  gtk_container_foreach (GTK_CONTAINER (dialog), actions_plugin_action_confirmation_dialog_get_label, timeout);
+  if (timeout->label == NULL)
+    g_warning ("%s: Could not find a label with the given text '%s'", G_STRFUNC, timeout->initial_label_text);
 
   /* first second looks out of sync with a second timer */
   timeout_id = g_timeout_add (1000, actions_plugin_action_confirmation_time, timeout);
@@ -893,6 +918,7 @@ actions_plugin_action_confirmation (ActionsPlugin *plugin,
 
   g_source_remove (timeout_id);
   gtk_widget_destroy (dialog);
+  g_free (timeout->initial_label_text);
   g_slice_free (ActionTimeout, timeout);
 
   return result == GTK_RESPONSE_ACCEPT;

commit aeadc9c3b2c0a2d4dc69f949f218093e60f3b7aa

actions: Restore dialog accept on Enter behavior
Commit 3811371b020a ("actions: Use xfce_message_dialog_new in confirmation dialogs")
rewrote action dialogs to use libxfce4ui functions instead of the GTK ones,
however it deleted a call to gtk_dialog_set_default_response (),
apparently accidentally.

Due to this, now these actions can no longer be accepted by simply pressing
Enter, which is quite annoying and breaks user's muscle memory.

Restore the old behavior by adding that gtk_dialog_set_default_response ()
call back to the code.

Fixes: 3811371b020a ("actions: Use xfce_message_dialog_new in confirmation dialogs")

plugins/actions/actions.c
@@ -870,6 +870,7 @@ actions_plugin_action_confirmation (ActionsPlugin *plugin,
                                     initial_label_text, _("_Cancel"), GTK_RESPONSE_CANCEL,
                                     XFCE_BUTTON_TYPE_MIXED, icon_name, _(entry->mnemonic), GTK_RESPONSE_ACCEPT,
                                     NULL);
+  gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
   gtk_window_set_keep_above (GTK_WINDOW (dialog), TRUE);
   gtk_window_stick (GTK_WINDOW (dialog));
   gtk_window_set_skip_taskbar_hint (GTK_WINDOW (dialog), TRUE);