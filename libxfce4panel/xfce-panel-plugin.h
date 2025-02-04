xfce-mirror/xfce4-panel

commit 585e2762fbf65419ada277e575142dc2265210dc

Add an hidden-event signal for plugins
Related: panel-plugins/xfce4-genmon-plugin!20

libxfce4panel/xfce-panel-plugin.h
@@ -100,6 +100,7 @@ G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfcePanelPlugin, g_object_unref)
  * @screen_position_changed : See #XfcePanelPlugin::screen-position-changed for more information.
  * @size_changed :            See #XfcePanelPlugin::size-changed for more information.
  * @orientation_changed :     See #XfcePanelPlugin::orientation-changed for more information.
+ * @hidden-event :            See #XfcePanelPlugin::hidden-event for more information.
  * @free_data :               See #XfcePanelPlugin::free-data for more information.
  * @save :                    See #XfcePanelPlugin::save for more information.
  * @about :                   See #XfcePanelPlugin::about for more information.
@@ -127,6 +128,8 @@ struct _XfcePanelPluginClass
                             gint size);
   void (*orientation_changed) (XfcePanelPlugin *plugin,
                                GtkOrientation orientation);
+  void (*hidden_event) (XfcePanelPlugin *plugin,
+                        gboolean hidden);
   void (*free_data) (XfcePanelPlugin *plugin);
   void (*save) (XfcePanelPlugin *plugin);
   void (*about) (XfcePanelPlugin *plugin);

docs/reference/libxfce4panel-docs.xml
@@ -43,7 +43,7 @@
 
         <para>In 4.6 you had to choose to compile a plugin as an executable to run it external (with
         the appropriate macro to register external plugins) and the same for internal plugins that
-        were compiled a modules. This worked quite good, but had a couple of disadvantages:</para>
+        were compiled as modules. This worked quite good, but had a couple of disadvantages:</para>
 
         <itemizedlist>
           <listitem>
@@ -70,8 +70,8 @@
         <varname>X-XFCE-Internal</varname> key in the plugins desktop file. No need to change the macros
         and the registration macro code can be kept to an absolute minimum.</para>
 
-        <para>This does not mean the 4.6 executable plugins are no supported anymore. However if you write
-        a new plugin or you plugin depends on libxfce4panel 4.8, it is recommended to switch to the new
+        <para>This does not mean the 4.6 executable plugins are not supported anymore. However if you write
+        a new plugin or your plugin depends on libxfce4panel 4.8, it is recommended to switch to the new
         registration functions and compile your plugin as a module. To make this move obvious the old
         macros are all marked as deprecated in this API reference.</para>
       </sect2>
@@ -93,14 +93,14 @@
 
         <itemizedlist>
           <listitem>
-            <para>You can easily cast you plugin to an <structname>XfcePanelPlugin</structname>, so
+            <para>You can easily cast your plugin to an <structname>XfcePanelPlugin</structname>, so
             no need for custom structures. You also get all the GOBject features like type checking.</para>
           </listitem>
         </itemizedlist>
 
-        <para>Downside is that you have to register all other types you create inside you plugin and that it
+        <para>Downside is that you have to register all other types you create inside your plugin and that it
         could be a bit difficult (when running internal) with special libraries. To work around the latter
-        you can make you plugin resident.</para>
+        you can make your plugin resident.</para>
 
         <para>All the plugins shipped with the panel are written as GObjects, so you can find enough examples
         in the <filename>plugins/</filename> folder.</para>
@@ -136,6 +136,10 @@
   <index id="api-index-deprecated">
     <title>Index of deprecated symbols</title>
     <xi:include href="xml/api-index-deprecated.xml"><xi:fallback /></xi:include>
+  </index>
+  <index id="api-index-4-22">
+    <title>Index of new symbols in 4.22</title>
+    <xi:include href="xml/api-index-4.21.0.xml"><xi:fallback /></xi:include>
   </index>
   <index id="api-index-4-20">
     <title>Index of new symbols in 4.20</title>

libxfce4panel/libxfce4panel.symbols
@@ -147,6 +147,7 @@ xfce_panel_plugin_provider_set_mode
 xfce_panel_plugin_provider_set_nrows
 xfce_panel_plugin_provider_set_screen_position
 xfce_panel_plugin_provider_save
+xfce_panel_plugin_provider_emit_hidden_event
 xfce_panel_plugin_provider_emit_signal
 xfce_panel_plugin_provider_get_show_configure
 xfce_panel_plugin_provider_show_configure

libxfce4panel/xfce-panel-plugin-provider.c
@@ -76,6 +76,17 @@ xfce_panel_plugin_provider_get_unique_id (XfcePanelPluginProvider *provider)
 
 
 void
+xfce_panel_plugin_provider_emit_hidden_event (XfcePanelPluginProvider *provider,
+                                              gboolean hidden)
+{
+  panel_return_if_fail (XFCE_IS_PANEL_PLUGIN_PROVIDER (provider));
+
+  XFCE_PANEL_PLUGIN_PROVIDER_GET_IFACE (provider)->hidden_event (provider, hidden);
+}
+
+
+
+void
 xfce_panel_plugin_provider_set_size (XfcePanelPluginProvider *provider,
                                      gint size)
 {

libxfce4panel/xfce-panel-plugin-provider.h
@@ -45,6 +45,8 @@ struct _XfcePanelPluginProviderInterface
   /*<public >*/
   const gchar *(*get_name) (XfcePanelPluginProvider *provider);
   gint (*get_unique_id) (XfcePanelPluginProvider *provider);
+  void (*hidden_event) (XfcePanelPluginProvider *provider,
+                        gboolean hidden);
   void (*set_size) (XfcePanelPluginProvider *provider,
                     gint size);
   void (*set_icon_size) (XfcePanelPluginProvider *provider,
@@ -115,6 +117,7 @@ typedef enum /*< skip >*/
   PROVIDER_PROP_TYPE_ACTION_SHOW_CONFIGURE, /* none */
   PROVIDER_PROP_TYPE_ACTION_SHOW_ABOUT, /* none */
   PROVIDER_PROP_TYPE_ACTION_ASK_REMOVE, /* none */
+  PROVIDER_PROP_TYPE_EVENT_HIDDEN, /* gboolean */
 
   /* WrapperPlug, using the same enum and D-Bus signal for simplicity */
   PROVIDER_PROP_TYPE_SET_OPACITY, /* gdouble */
@@ -159,6 +162,10 @@ xfce_panel_plugin_provider_get_name (XfcePanelPluginProvider *provider);
 
 gint
 xfce_panel_plugin_provider_get_unique_id (XfcePanelPluginProvider *provider);
+
+void
+xfce_panel_plugin_provider_emit_hidden_event (XfcePanelPluginProvider *provider,
+                                              gboolean hidden);
 
 void
 xfce_panel_plugin_provider_set_size (XfcePanelPluginProvider *provider,

libxfce4panel/xfce-panel-plugin.c
@@ -136,6 +136,9 @@ static void
 xfce_panel_plugin_show_about (XfcePanelPluginProvider *provider);
 static void
 xfce_panel_plugin_removed (XfcePanelPluginProvider *provider);
+static void
+xfce_panel_plugin_hidden_event (XfcePanelPluginProvider *provider,
+                                gboolean hidden);
 static gboolean
 xfce_panel_plugin_remote_event (XfcePanelPluginProvider *provider,
                                 const gchar *name,
@@ -175,6 +178,7 @@ enum
   ABOUT,
   CONFIGURE_PLUGIN,
   FREE_DATA,
+  HIDDEN_EVENT,
   ORIENTATION_CHANGED,
   REMOTE_EVENT,
   REMOVED,
@@ -207,6 +211,7 @@ struct _XfcePanelPluginPrivate
   gint size; /* single row size */
   gint icon_size;
   gboolean dark_mode;
+  gboolean hidden;
   guint expand : 1;
   guint shrink : 1;
   guint nrows;
@@ -317,6 +322,23 @@ xfce_panel_plugin_class_init (XfcePanelPluginClass *klass)
                                             NULL, NULL,
                                             g_cclosure_marshal_VOID__VOID,
                                             G_TYPE_NONE, 0);
+
+  /**
+   * XfcePanelPlugin::hidden-event
+   * @plugin : an #XfcePanelPlugin.
+   *
+   * This signal is emmitted when the panel the @plugin is on
+   * is transitioning between hidden/visble.
+   *
+   * Since: 4.21.0
+   **/
+  plugin_signals[HIDDEN_EVENT] = g_signal_new (g_intern_static_string ("hidden-event"),
+                                               G_TYPE_FROM_CLASS (klass),
+                                               G_SIGNAL_RUN_LAST,
+                                               G_STRUCT_OFFSET (XfcePanelPluginClass, hidden_event),
+                                               NULL, NULL,
+                                               g_cclosure_marshal_VOID__BOOLEAN,
+                                               G_TYPE_NONE, 1, G_TYPE_BOOLEAN);
 
   /**
    * XfcePanelPlugin::orientation-changed
@@ -730,6 +752,7 @@ xfce_panel_plugin_init (XfcePanelPlugin *plugin)
   plugin->priv->size = 0;
   plugin->priv->icon_size = 0;
   plugin->priv->dark_mode = FALSE;
+  plugin->priv->hidden = FALSE;
   plugin->priv->small = FALSE;
   plugin->priv->expand = FALSE;
   plugin->priv->shrink = FALSE;
@@ -764,6 +787,7 @@ xfce_panel_plugin_provider_init (XfcePanelPluginProviderInterface *iface)
 {
   iface->get_name = (ProviderToPluginChar) xfce_panel_plugin_get_name;
   iface->get_unique_id = (ProviderToPluginInt) xfce_panel_plugin_get_unique_id;
+  iface->hidden_event = xfce_panel_plugin_hidden_event;
   iface->set_size = xfce_panel_plugin_set_size;
   iface->set_icon_size = xfce_panel_plugin_set_icon_size;
   iface->set_dark_mode = xfce_panel_plugin_set_dark_mode;
@@ -1466,6 +1490,25 @@ xfce_panel_plugin_set_dark_mode (XfcePanelPluginProvider *provider,
                     "gtk-application-prefer-dark-theme",
                     dark_mode,
                     NULL);
+    }
+}
+
+
+static void
+xfce_panel_plugin_hidden_event (XfcePanelPluginProvider *provider,
+                                gboolean hidden)
+{
+  XfcePanelPlugin *plugin = XFCE_PANEL_PLUGIN (provider);
+
+  panel_return_if_fail (XFCE_IS_PANEL_PLUGIN (provider));
+
+  /* check if update is required */
+  if (G_LIKELY (plugin->priv->hidden != hidden))
+    {
+      plugin->priv->hidden = hidden;
+
+      g_signal_emit (G_OBJECT (plugin),
+                     plugin_signals[HIDDEN_EVENT], 0, hidden);
     }
 }
 

panel/panel-plugin-external.c
@@ -79,6 +79,9 @@ panel_plugin_external_get_name (XfcePanelPluginProvider *provider);
 static gint
 panel_plugin_external_get_unique_id (XfcePanelPluginProvider *provider);
 static void
+panel_plugin_external_hidden_event (XfcePanelPluginProvider *provider,
+                                    gboolean hidden);
+static void
 panel_plugin_external_set_size (XfcePanelPluginProvider *provider,
                                 gint size);
 static void
@@ -229,6 +232,7 @@ panel_plugin_external_provider_init (XfcePanelPluginProviderInterface *iface)
 {
   iface->get_name = panel_plugin_external_get_name;
   iface->get_unique_id = panel_plugin_external_get_unique_id;
+  iface->hidden_event = panel_plugin_external_hidden_event;
   iface->set_size = panel_plugin_external_set_size;
   iface->set_icon_size = panel_plugin_external_set_icon_size;
   iface->set_dark_mode = panel_plugin_external_set_dark_mode;
@@ -822,6 +826,26 @@ panel_plugin_external_get_unique_id (XfcePanelPluginProvider *provider)
   panel_return_val_if_fail (XFCE_IS_PANEL_PLUGIN_PROVIDER (provider), -1);
 
   return get_instance_private (provider)->unique_id;
+}
+
+
+
+static void
+panel_plugin_external_hidden_event (XfcePanelPluginProvider *provider,
+                                    gboolean hidden)
+{
+  GValue value = G_VALUE_INIT;
+
+  panel_return_if_fail (PANEL_IS_PLUGIN_EXTERNAL (provider));
+  panel_return_if_fail (XFCE_IS_PANEL_PLUGIN_PROVIDER (provider));
+
+  g_value_init (&value, G_TYPE_BOOLEAN);
+  g_value_set_boolean (&value, hidden);
+
+  panel_plugin_external_queue_add (PANEL_PLUGIN_EXTERNAL (provider),
+                                   PROVIDER_PROP_TYPE_EVENT_HIDDEN, &value);
+
+  g_value_unset (&value);
 }
 
 

panel/panel-window.c
@@ -224,6 +224,9 @@ static void
 panel_window_plugins_update (PanelWindow *window,
                              PluginProp prop);
 static void
+panel_window_plugin_emit_hidden_event (GtkWidget *widget,
+                                       gpointer user_data);
+static void
 panel_window_plugin_set_mode (GtkWidget *widget,
                               gpointer user_data);
 static void
@@ -267,6 +270,7 @@ enum
 
 enum _PluginProp
 {
+  PLUGIN_PROP_HIDDEN_EVENT,
   PLUGIN_PROP_MODE,
   PLUGIN_PROP_SCREEN_POSITION,
   PLUGIN_PROP_NROWS,
@@ -3150,15 +3154,22 @@ panel_window_autohide_timeout (gpointer user_data)
   /* update the status */
   if (window->autohide_state == AUTOHIDE_POPDOWN
       || window->autohide_state == AUTOHIDE_POPDOWN_SLOW)
-    window->autohide_state = AUTOHIDE_HIDDEN;
+    {
+      window->autohide_state = AUTOHIDE_HIDDEN;
+      panel_window_plugins_update (window, PLUGIN_PROP_HIDDEN_EVENT);
+    }
   else if (window->autohide_state == AUTOHIDE_POPUP)
-    window->autohide_state = AUTOHIDE_VISIBLE;
+    {
+      window->autohide_state = AUTOHIDE_VISIBLE;
+      panel_window_plugins_update (window, PLUGIN_PROP_HIDDEN_EVENT);
+    }
 
   /* needs a recheck when timeout is over on Wayland, see panel_window_pointer_is_outside() */
   if (gtk_layer_is_supported () && window->autohide_state == AUTOHIDE_HIDDEN
       && !panel_window_pointer_is_outside (window))
     {
       window->autohide_state = AUTOHIDE_VISIBLE;
+      panel_window_plugins_update (window, PLUGIN_PROP_HIDDEN_EVENT);
       return FALSE;
     }
 
@@ -3304,6 +3315,7 @@ panel_window_autohide_queue (PanelWindow *window,
     {
       /* queue a resize to make sure the panel is visible */
       gtk_widget_queue_resize (GTK_WIDGET (window));
+      panel_window_plugins_update (window, PLUGIN_PROP_HIDDEN_EVENT);
     }
   else
     {
@@ -3822,6 +3834,10 @@ panel_window_plugins_update (PanelWindow *window,
 
   switch (prop)
     {
+    case PLUGIN_PROP_HIDDEN_EVENT:
+      func = panel_window_plugin_emit_hidden_event;
+      break;
+
     case PLUGIN_PROP_MODE:
       func = panel_window_plugin_set_mode;
       break;
@@ -3857,6 +3873,19 @@ panel_window_plugins_update (PanelWindow *window,
       panel_return_if_fail (GTK_IS_CONTAINER (itembar));
       gtk_container_foreach (GTK_CONTAINER (itembar), func, window);
     }
+}
+
+
+
+static void
+panel_window_plugin_emit_hidden_event (GtkWidget *widget,
+                                       gpointer user_data)
+{
+  panel_return_if_fail (XFCE_IS_PANEL_PLUGIN_PROVIDER (widget));
+  panel_return_if_fail (PANEL_IS_WINDOW (user_data));
+
+  xfce_panel_plugin_provider_emit_hidden_event (XFCE_PANEL_PLUGIN_PROVIDER (widget),
+                                                PANEL_WINDOW (user_data)->autohide_state == AUTOHIDE_HIDDEN);
 }
 
 

commit d35d56a0f8bdd491e4d7445c3c463b0fdc231e66

libxfce4panel: Fix ABI break
Only partially fixed in ea938314: the order of the signals accounts,
obviously…

Closes: panel-plugins/xfce4-pulseaudio-plugin#114
Fixes: 585e2762fbf65419ada277e575142dc2265210dc
Completes: ea93831413ba608c4f9a5c163ee002163a3578ff

libxfce4panel/xfce-panel-plugin.h
@@ -132,8 +132,6 @@ struct _XfcePanelPluginClass
                             gint size);
   void (*orientation_changed) (XfcePanelPlugin *plugin,
                                GtkOrientation orientation);
-  void (*hidden_event) (XfcePanelPlugin *plugin,
-                        gboolean hidden);
   void (*free_data) (XfcePanelPlugin *plugin);
   void (*save) (XfcePanelPlugin *plugin);
   void (*about) (XfcePanelPlugin *plugin);
@@ -148,6 +146,10 @@ struct _XfcePanelPluginClass
                         XfcePanelPluginMode mode);
   void (*nrows_changed) (XfcePanelPlugin *plugin,
                          guint rows);
+
+  /* new in 4.20.1 */
+  void (*hidden_event) (XfcePanelPlugin *plugin,
+                        gboolean hidden);
 
   /*< private >*/
   void (*reserved1) (void);