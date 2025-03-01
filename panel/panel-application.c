xfce-mirror/xfce4-panel

commit 6b671b0d03a353fae93ff09a769cab27a66018ac

panel: Mitigate a memory leak when removing items
Sometimes, maybe most of the time, panel items are not finalized when
they are removed from the panel. For lack of being able to find the
reason why extra references accumulate, this commit eliminates them
after the item has been removed from the panel and all priority
operations have been performed, until triggering the destruction of the
item.

Beyond the beneficial effect on memory, this also avoids potential
crashes due to the reuse of data supposed to be freed, such as
typically the dbus path.

See !46 for more details.

panel/panel-application.c
@@ -722,7 +722,8 @@ panel_application_plugin_provider_signal (XfcePanelPluginProvider       *provide
           name = g_strdup (xfce_panel_plugin_provider_get_name (provider));
 
           /* destroy the plugin */
-          gtk_widget_destroy (GTK_WIDGET (provider));
+          itembar = gtk_bin_get_child (GTK_BIN (window));
+          gtk_container_remove (GTK_CONTAINER (itembar), GTK_WIDGET (provider));
 
           /* remove the plugin configuration */
           panel_application_plugin_delete_config (application, name, unique_id);

panel/panel-itembar.c
@@ -26,6 +26,8 @@
 
 #include <common/panel-private.h>
 #include <libxfce4panel/libxfce4panel.h>
+#include <common/panel-debug.h>
+#include <libxfce4panel/xfce-panel-plugin-provider.h>
 
 #include <panel/panel-itembar.h>
 
@@ -842,12 +844,47 @@ panel_itembar_add (GtkContainer *container,
 
 
 
+/* for lack of being able to find the reason why extra references accumulate, let's
+ * eliminate them after the item has been removed from the panel and all priority
+ * operations have been performed, until triggering the destruction of the item */
+static gboolean
+panel_itembar_unref (gpointer data)
+{
+  XfcePanelPluginProvider **provider = data;
+  gint                      n = 0, id;
+  gchar                    *name;
+
+  /* should always be true if there were no memory leak */
+  if (*provider == NULL)
+    return FALSE;
+
+  name = g_strdup (xfce_panel_plugin_provider_get_name (*provider));
+  id = xfce_panel_plugin_provider_get_unique_id (*provider);
+
+  while (*provider != NULL)
+    {
+      g_object_unref (*provider);
+      n++;
+    }
+
+  panel_debug (PANEL_DEBUG_ITEMBAR,
+               "%s-%d: number of extra references removed to release the plugin: %d",
+               name, id, n);
+  g_free (name);
+
+  return FALSE;
+}
+
+
+
 static void
 panel_itembar_remove (GtkContainer *container,
                       GtkWidget    *widget)
 {
   PanelItembarChild *child;
   PanelItembar      *itembar = PANEL_ITEMBAR (container);
+
+  static GtkWidget *swidget;
 
   panel_return_if_fail (PANEL_IS_ITEMBAR (itembar));
   panel_return_if_fail (GTK_IS_WIDGET (widget));
@@ -858,6 +895,10 @@ panel_itembar_remove (GtkContainer *container,
   if (G_LIKELY (child != NULL))
     {
       itembar->children = g_slist_remove (itembar->children, child);
+
+      swidget = widget;
+      g_signal_connect (widget, "destroy", G_CALLBACK (gtk_widget_destroyed), &swidget);
+      g_idle_add (panel_itembar_unref, &swidget);
 
       gtk_widget_unparent (widget);
 

common/panel-debug.c
@@ -54,7 +54,8 @@ static const GDebugKey panel_debug_keys[] =
   { "struts", PANEL_DEBUG_STRUTS },
   { "systray", PANEL_DEBUG_SYSTRAY },
   { "tasklist", PANEL_DEBUG_TASKLIST },
-  { "pager", PANEL_DEBUG_PAGER }
+  { "pager", PANEL_DEBUG_PAGER },
+  { "itembar", PANEL_DEBUG_ITEMBAR }
 };
 
 

common/panel-debug.h
@@ -42,7 +42,8 @@ typedef enum
   PANEL_DEBUG_STRUTS           = 1 << 12,
   PANEL_DEBUG_SYSTRAY          = 1 << 13,
   PANEL_DEBUG_TASKLIST         = 1 << 14,
-  PANEL_DEBUG_PAGER            = 1 << 15
+  PANEL_DEBUG_PAGER            = 1 << 15,
+  PANEL_DEBUG_ITEMBAR          = 1 << 16
 }
 PanelDebugFlag;
 

libxfce4panel/xfce-panel-plugin-provider.c
@@ -185,8 +185,10 @@ xfce_panel_plugin_provider_emit_signal (XfcePanelPluginProvider       *provider,
 {
   panel_return_if_fail (XFCE_IS_PANEL_PLUGIN_PROVIDER (provider));
 
-  /* emit the signal */
-  g_signal_emit (G_OBJECT (provider), provider_signals[PROVIDER_SIGNAL], 0, provider_signal);
+  /* emit the signal only if the plugin has not already been unparented (see
+   * panel_itembar_unref()) */
+  if (gtk_widget_get_parent (GTK_WIDGET (provider)) != NULL)
+    g_signal_emit (provider, provider_signals[PROVIDER_SIGNAL], 0, provider_signal);
 }
 
 

panel/panel-plugin-external.c
@@ -248,6 +248,11 @@ panel_plugin_external_finalize (GObject *object)
 {
   PanelPluginExternal *external = PANEL_PLUGIN_EXTERNAL (object);
 
+  panel_debug (PANEL_DEBUG_EXTERNAL,
+               "%s-%d: plugin is being finalized",
+               panel_module_get_name (external->module),
+               external->unique_id);
+
   if (external->priv->spawn_timeout_id != 0)
     g_source_remove (external->priv->spawn_timeout_id);
 

panel/panel-preferences-dialog.c
@@ -1471,7 +1471,11 @@ panel_preferences_dialog_item_selection_changed (GtkTreeSelection       *selecti
   panel_return_if_fail (PANEL_IS_PREFERENCES_DIALOG (dialog));
 
   provider = panel_preferences_dialog_item_get_selected (dialog, NULL);
-  if (G_LIKELY (provider != NULL))
+
+  /* when removing an item from the panel, this handler is called when the item is
+   * still selected but has already been unparented, so we also have to check its parent
+   * (see panel_itembar_unref()) */
+  if (G_LIKELY (provider != NULL && gtk_widget_get_parent (GTK_WIDGET (provider)) != NULL))
     {
       /* get the current position and the items on the bar */
       itembar = gtk_bin_get_child (GTK_BIN (dialog->active));

commit f5c264de03aaa49d8d1ad0e334b401a7ac1e7323

itembar: Fix memory leak mitigation on plugins
A reference widget must be allocated for each plugin instead of using a
static variable, otherwise when several plugins are removed at once
(e.g. when removing a panel), the memory leak mitigation only applies to
the first of them.

On the other hand, panel destruction must also be deferred, so that
plugins can refer to it when they are destroyed.

Fixes: 6b671b0d03a353fae93ff09a769cab27a66018ac

configure.ac
@@ -15,7 +15,7 @@ m4_define([copyright_year], [2025])
 XDT_VERSION_INIT([4.20.0], [git])
 
 dnl minimum required versions
-m4_define([glib_minimum_version], [2.72.0])
+m4_define([glib_minimum_version], [2.74.0])
 m4_define([gtk_minimum_version], [3.24.0])
 m4_define([cairo_minimum_version], [1.16])
 m4_define([libxfce4util_minimum_version], [4.17.2])

meson.build
@@ -22,7 +22,7 @@ dependency_versions = {
   'cairo': '>= 1.16.0',
   'exo': '>= 0.11.2',
   'garcon': '>= 4.17.0',
-  'glib': '>= 2.72.0',
+  'glib': '>= 2.74.0',
   'gtk': '>= 3.24.0',
   'libxfce4ui': '>= 4.17.1',
   'libxfce4util': '>= 4.17.2',

panel/panel-application.c
@@ -1663,8 +1663,8 @@ panel_application_remove_window (PanelApplication *application,
   itembar = gtk_bin_get_child (GTK_BIN (window));
   gtk_container_foreach (GTK_CONTAINER (itembar), panel_application_plugin_remove, NULL);
 
-  /* destroy */
-  gtk_widget_destroy (GTK_WIDGET (window));
+  /* destroy window after all plugins are released, see panel_itembar_unref() */
+  g_idle_add_once ((GSourceOnceFunc) gtk_widget_destroy, window);
 
   /* remove the panel settings */
   property = g_strdup_printf (PANELS_PROPERTY_BASE, panel_id);

panel/panel-itembar.c
@@ -843,9 +843,17 @@ panel_itembar_add (GtkContainer *container,
 
 
 
-/* for lack of being able to find the reason why extra references accumulate, let's
+/*
+ * For lack of being able to find the reason why extra references accumulate, let's
  * eliminate them after the item has been removed from the panel and all priority
- * operations have been performed, until triggering the destruction of the item */
+ * operations have been performed, until triggering the destruction of the item.
+ * As a reminder: references accumulate particularly when plugins are moved (simply
+ * via the preferences dialog, no need for dnd). So by calling panel_itembar_reorder_child(),
+ * which does nothing in particular. So it could be that the problem is internal to GTK,
+ * and/or that we're implementing something wrong in this container, but it's not just
+ * a matter of a missing g_object_unref() somewhere: the number of accumulated references
+ * can quickly rise to several dozen.
+ */
 static gboolean
 panel_itembar_unref (gpointer data)
 {
@@ -855,12 +863,16 @@ panel_itembar_unref (gpointer data)
 
   /* should always be true if there were no memory leak, except in the case below */
   if (*provider == NULL)
-    return FALSE;
+    {
+      g_free (provider);
+      return FALSE;
+    }
 
   /* leave if the widget was reparented (drag and drop should be the only case) */
   if (gtk_widget_get_parent (GTK_WIDGET (*provider)) != NULL)
     {
       g_signal_handlers_disconnect_by_func (*provider, gtk_widget_destroyed, provider);
+      g_free (provider);
       return FALSE;
     }
 
@@ -877,6 +889,7 @@ panel_itembar_unref (gpointer data)
                "%s-%d: number of extra references removed to release the plugin: %d",
                name, id, n);
   g_free (name);
+  g_free (provider);
 
   return FALSE;
 }
@@ -890,8 +903,6 @@ panel_itembar_remove (GtkContainer *container,
   PanelItembarChild *child;
   PanelItembar *itembar = PANEL_ITEMBAR (container);
 
-  static GtkWidget *swidget;
-
   panel_return_if_fail (PANEL_IS_ITEMBAR (itembar));
   panel_return_if_fail (GTK_IS_WIDGET (widget));
   panel_return_if_fail (gtk_widget_get_parent (widget) == GTK_WIDGET (container));
@@ -900,11 +911,13 @@ panel_itembar_remove (GtkContainer *container,
   child = panel_itembar_get_child (itembar, widget);
   if (G_LIKELY (child != NULL))
     {
+      GtkWidget **provider = g_new (GtkWidget *, 1);
+
       itembar->children = g_slist_remove (itembar->children, child);
 
-      swidget = widget;
-      g_signal_connect (widget, "destroy", G_CALLBACK (gtk_widget_destroyed), &swidget);
-      g_idle_add (panel_itembar_unref, &swidget);
+      *provider = widget;
+      g_signal_connect (widget, "destroy", G_CALLBACK (gtk_widget_destroyed), provider);
+      g_idle_add (panel_itembar_unref, provider);
 
       gtk_widget_unparent (widget);
 