xfce-mirror/xfce4-panel

commit 8583e16d89d70ce6ee8d2492d56f7ecf9f10575e

panel: Show window in panel_application_new_window()
Instead of showing it if it hasn't already been done in
panel_window_screen_layout_changed(), which is very odd and
inappropriate. In particular, this avoids unintentionally showing a
panel hidden by autohide on wayland, when display settings change.

panel/panel-window.c
@@ -2793,9 +2793,6 @@ panel_window_screen_layout_changed (GdkScreen *screen,
   /* update the struts if needed (ie. we need to reset the struts) */
   if (force_struts_update)
     panel_window_screen_struts_set (window);
-
-  if (!gtk_widget_get_visible (GTK_WIDGET (window)))
-    gtk_widget_show (GTK_WIDGET (window));
 }
 
 

panel/panel-application.c
@@ -1626,6 +1626,8 @@ panel_application_new_window (PanelApplication *application,
   if (new_window)
     panel_application_save (application, SAVE_PANEL_IDS);
 
+  gtk_widget_show (GTK_WIDGET (window));
+
   return PANEL_WINDOW (window);
 }
 

panel/panel-preferences-dialog.c
@@ -1068,9 +1068,6 @@ panel_preferences_dialog_panel_add (GtkWidget *widget,
   window = panel_application_new_window (dialog->application,
                                          gtk_widget_get_screen (widget), -1, TRUE);
 
-  /* show window */
-  gtk_widget_show (GTK_WIDGET (window));
-
   /* rebuild the selector */
   panel_id = panel_window_get_id (window);
   panel_preferences_dialog_panel_combobox_rebuild (dialog, panel_id);

commit ace49302d03e22fa2e87e08a8e0019c581433271

panel: Show window when new output is connected
Closes: #925
Fixes: 8583e16d89d70ce6ee8d2492d56f7ecf9f10575e

panel/panel-window.c
@@ -2793,6 +2793,17 @@ panel_window_screen_layout_changed (GdkScreen *screen,
   /* update the struts if needed (ie. we need to reset the struts) */
   if (force_struts_update)
     panel_window_screen_struts_set (window);
+
+  /*
+   * The panel window may be hidden if the output to which it is assigned is disconnected,
+   * and must be shown when that output is reconnected. This is always true on X11, but on
+   * Wayland it should only be done if the window has not been autohidden.
+   */
+  if (!gtk_widget_get_visible (GTK_WIDGET (window))
+      && (!gtk_layer_is_supported ()
+          || window->autohide_behavior == AUTOHIDE_BEHAVIOR_NEVER
+          || window->autohide_state == AUTOHIDE_VISIBLE))
+    gtk_widget_show (GTK_WIDGET (window));
 }
 
 