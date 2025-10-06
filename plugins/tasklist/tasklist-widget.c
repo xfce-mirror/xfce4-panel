xfce-mirror/xfce4-panel

commit 1538d8b72bf7127cc299ac4d2e5f264c22d1896f

wayland: Support multiple workspace groups where it makes sense
Related: https://github.com/YaLTeR/niri/pull/1800

plugins/tasklist/tasklist-widget.c
@@ -1680,6 +1680,9 @@ xfce_tasklist_connect_screen (XfceTasklist *tasklist)
   tasklist->display = gtk_widget_get_display (GTK_WIDGET (tasklist));
   tasklist->screen = xfw_screen_get_default ();
   manager = xfw_screen_get_workspace_manager (tasklist->screen);
+
+  /* window button visibility based on window<->workspace association only works on X11,
+   * where there is only one workspace group */
   tasklist->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
 
   /* add all existing windows on this screen */

plugins/windowmenu/windowmenu.c
@@ -438,6 +438,8 @@ window_menu_plugin_screen_changed (GtkWidget *widget,
   /* set the new screen */
   plugin->screen = screen;
   manager = xfw_screen_get_workspace_manager (screen);
+
+  /* window<->workspace association only works on X11, where there is only one workspace group */
   plugin->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
 
   /* connect signal to monitor this screen */
@@ -866,11 +868,22 @@ static void
 window_menu_plugin_workspace_add (GtkWidget *mi,
                                   WindowMenuPlugin *plugin)
 {
+  GdkMonitor *monitor;
+  GList *groups;
+
   panel_return_if_fail (WINDOW_MENU_IS_PLUGIN (plugin));
   panel_return_if_fail (XFW_IS_WORKSPACE_GROUP (plugin->workspace_group));
 
+  /* adding/removing workspaces is also available on wayland and it seems reasonable
+   * to do it on the last workspace group for the monitor the plugin is on;
+   * see also: window_menu_plugin_menu_new() */
+  monitor = panel_utils_get_monitor_at_widget (GTK_WIDGET (plugin));
+  groups = panel_utils_list_workspace_groups_for_monitor (plugin->screen, monitor);
+
   /* increase the number of workspaces */
-  xfw_workspace_group_create_workspace (plugin->workspace_group, NULL, NULL);
+  xfw_workspace_group_create_workspace (g_list_last (groups)->data, NULL, NULL);
+
+  g_list_free (groups);
 }
 
 
@@ -880,12 +893,23 @@ window_menu_plugin_workspace_remove (GtkWidget *mi,
                                      WindowMenuPlugin *plugin)
 {
   XfwWorkspace *workspace;
+  GdkMonitor *monitor;
+  GList *groups;
+
   panel_return_if_fail (WINDOW_MENU_IS_PLUGIN (plugin));
   panel_return_if_fail (XFW_IS_WORKSPACE_GROUP (plugin->workspace_group));
 
+  /* adding/removing workspaces is also available on wayland and it seems reasonable
+   * to do it on the last workspace group for the monitor the plugin is on;
+   * see also: window_menu_plugin_menu_new() */
+  monitor = panel_utils_get_monitor_at_widget (GTK_WIDGET (plugin));
+  groups = panel_utils_list_workspace_groups_for_monitor (plugin->screen, monitor);
+
   /* decrease the number of workspaces */
-  workspace = g_list_last (xfw_workspace_group_list_workspaces (plugin->workspace_group))->data;
+  workspace = g_list_last (xfw_workspace_group_list_workspaces (g_list_last (groups)->data))->data;
   xfw_workspace_remove (workspace, NULL);
+
+  g_list_free (groups);
 }
 
 
@@ -1373,13 +1397,19 @@ window_menu_plugin_menu_new (WindowMenuPlugin *plugin)
       XfwWorkspaceGroupCapabilities gcapabilities;
       XfwWorkspaceCapabilities wcapabilities;
 
+      /* adding/removing workspaces is also available on wayland and it seems reasonable
+       * to do it on the last workspace group for the monitor the plugin is on;
+       * see also: window_menu_plugin_workspace_add/remove() */
+      GdkMonitor *monitor = panel_utils_get_monitor_at_widget (GTK_WIDGET (plugin));
+      GList *groups = panel_utils_list_workspace_groups_for_monitor (plugin->screen, monitor);
+
       mi = gtk_separator_menu_item_new ();
       gtk_menu_shell_append (GTK_MENU_SHELL (menu), mi);
       gtk_widget_show (mi);
 
       mi = panel_image_menu_item_new_with_label (_("Add Workspace"));
       gtk_menu_shell_append (GTK_MENU_SHELL (menu), mi);
-      gcapabilities = xfw_workspace_group_get_capabilities (plugin->workspace_group);
+      gcapabilities = xfw_workspace_group_get_capabilities (g_list_last (groups)->data);
       gtk_widget_set_sensitive (mi, gcapabilities & XFW_WORKSPACE_GROUP_CAPABILITIES_CREATE_WORKSPACE);
       g_signal_connect (G_OBJECT (mi), "activate",
                         G_CALLBACK (window_menu_plugin_workspace_add), plugin);
@@ -1405,18 +1435,20 @@ window_menu_plugin_menu_new (WindowMenuPlugin *plugin)
 
       mi = panel_image_menu_item_new_with_label (label);
       gtk_menu_shell_append (GTK_MENU_SHELL (menu), mi);
-      wcapabilities = xfw_workspace_get_capabilities (g_list_last (workspaces)->data);
+      workspace = g_list_last (xfw_workspace_group_list_workspaces (g_list_last (groups)->data))->data;
+      wcapabilities = xfw_workspace_get_capabilities (workspace);
       gtk_widget_set_sensitive (mi, n_workspaces > 1 && wcapabilities & XFW_WORKSPACE_CAPABILITIES_REMOVE);
       g_signal_connect (G_OBJECT (mi), "activate",
                         G_CALLBACK (window_menu_plugin_workspace_remove), plugin);
       gtk_widget_show (mi);
 
-      g_free (label);
-      g_free (utf8);
-
       image = gtk_image_new_from_icon_name ("list-remove", GTK_ICON_SIZE_MENU);
       panel_image_menu_item_set_image (mi, image);
       gtk_widget_show (mi);
+
+      g_free (label);
+      g_free (utf8);
+      g_list_free (groups);
     }
 
   pango_font_description_free (italic);

common/panel-utils.c
@@ -24,7 +24,6 @@
 #include "panel-utils.h"
 
 #include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4windowing/libxfce4windowing.h>
 
 
 
@@ -318,4 +317,96 @@ panel_utils_compare_xfw_gdk_monitors (gconstpointer a,
                                       gconstpointer b)
 {
   return xfw_monitor_get_gdk_monitor ((XfwMonitor *) a) == b ? 0 : 1;
+}
+
+
+
+GdkMonitor *
+panel_utils_get_monitor_at_widget (GtkWidget *widget)
+{
+  GdkDisplay *display = gdk_display_get_default ();
+  GdkWindow *window = gtk_widget_get_window (widget);
+  if (window != NULL)
+    return gdk_display_get_monitor_at_window (display, window);
+
+  return gdk_display_get_monitor (display, 0);
+}
+
+
+
+GList *
+panel_utils_list_workspace_groups_for_monitor (XfwScreen *xfw_screen,
+                                               GdkMonitor *monitor)
+{
+  XfwWorkspaceManager *manager = xfw_screen_get_workspace_manager (xfw_screen);
+  GList *groups = NULL;
+  for (GList *lp = xfw_workspace_manager_list_workspace_groups (manager); lp != NULL; lp = lp->next)
+    if (g_list_find_custom (xfw_workspace_group_get_monitors (lp->data), monitor, panel_utils_compare_xfw_gdk_monitors))
+      groups = g_list_prepend (groups, lp->data);
+
+  return g_list_reverse (groups);
+}
+
+
+
+GList *
+panel_utils_list_workspaces_for_monitor (XfwScreen *xfw_screen,
+                                         GdkMonitor *monitor)
+{
+  GList *groups = panel_utils_list_workspace_groups_for_monitor (xfw_screen, monitor);
+  GList *workspaces = NULL;
+  for (GList *lp = groups; lp != NULL; lp = lp->next)
+    for (GList *lq = xfw_workspace_group_list_workspaces (lp->data); lq != NULL; lq = lq->next)
+      workspaces = g_list_prepend (workspaces, lq->data);
+
+  g_list_free (groups);
+  return g_list_reverse (workspaces);
+}
+
+
+
+XfwWorkspace *
+panel_utils_get_active_workspace_for_monitor (XfwScreen *xfw_screen,
+                                              GdkMonitor *monitor)
+{
+  GList *groups = panel_utils_list_workspace_groups_for_monitor (xfw_screen, monitor);
+  XfwWorkspace *workspace = NULL;
+  for (GList *lp = groups; lp != NULL; lp = lp->next)
+    {
+      workspace = xfw_workspace_group_get_active_workspace (lp->data);
+      if (workspace != NULL)
+        break;
+    }
+
+  g_list_free (groups);
+  return workspace;
+}
+
+
+
+guint
+panel_utils_get_workspace_count_for_monitor (XfwScreen *xfw_screen,
+                                             GdkMonitor *monitor)
+{
+  GList *groups = panel_utils_list_workspace_groups_for_monitor (xfw_screen, monitor);
+  guint count = 0;
+  for (GList *lp = groups; lp != NULL; lp = lp->next)
+    count += xfw_workspace_group_get_workspace_count (lp->data);
+
+  g_list_free (groups);
+  return count;
+}
+
+
+
+gint
+panel_utils_get_workspace_number_for_monitor (XfwScreen *xfw_screen,
+                                              GdkMonitor *monitor,
+                                              XfwWorkspace *workspace)
+{
+  GList *workspaces = panel_utils_list_workspaces_for_monitor (xfw_screen, monitor);
+  gint number = g_list_index (workspaces, workspace);
+
+  g_list_free (workspaces);
+  return number;
 }

common/panel-utils.h
@@ -22,6 +22,7 @@
 #include "libxfce4panel/libxfce4panel.h"
 
 #include <gtk/gtk.h>
+#include <libxfce4windowing/libxfce4windowing.h>
 
 G_BEGIN_DECLS
 
@@ -63,6 +64,30 @@ panel_utils_gtk_dialog_find_label_by_text (GtkDialog *dialog,
 gint
 panel_utils_compare_xfw_gdk_monitors (gconstpointer a,
                                       gconstpointer b);
+
+GdkMonitor *
+panel_utils_get_monitor_at_widget (GtkWidget *widget);
+
+GList *
+panel_utils_list_workspace_groups_for_monitor (XfwScreen *xfw_screen,
+                                               GdkMonitor *monitor);
+
+GList *
+panel_utils_list_workspaces_for_monitor (XfwScreen *xfw_screen,
+                                         GdkMonitor *monitor);
+
+XfwWorkspace *
+panel_utils_get_active_workspace_for_monitor (XfwScreen *xfw_screen,
+                                              GdkMonitor *monitor);
+
+guint
+panel_utils_get_workspace_count_for_monitor (XfwScreen *xfw_screen,
+                                             GdkMonitor *monitor);
+
+gint
+panel_utils_get_workspace_number_for_monitor (XfwScreen *xfw_screen,
+                                              GdkMonitor *monitor,
+                                              XfwWorkspace *workspace);
 
 G_END_DECLS
 

plugins/actions/Makefile.am
@@ -18,6 +18,7 @@ libactions_la_CFLAGS = \
 	$(GTK_CFLAGS) \
 	$(LIBXFCE4UTIL_CFLAGS) \
 	$(LIBXFCE4UI_CFLAGS) \
+	$(LIBXFCE4WINDOWING_CFLAGS) \
 	$(XFCONF_CFLAGS) \
 	$(GIO_CFLAGS) \
 	$(PLATFORM_CFLAGS)
@@ -35,6 +36,7 @@ libactions_la_LIBADD = \
 	$(GTK_LIBS) \
 	$(LIBXFCE4UTIL_LIBS) \
 	$(LIBXFCE4UI_LIBS) \
+	$(LIBXFCE4WINDOWING_LIBS) \
 	$(GIO_LIBS) \
 	$(XFCONF_LIBS)
 

plugins/actions/meson.build
@@ -20,6 +20,7 @@ shared_module(
     gtk,
     libxfce4util,
     libxfce4ui,
+    libxfce4windowing,
     xfconf,
   ],
   link_with: [

plugins/applicationsmenu/Makefile.am
@@ -19,6 +19,7 @@ libapplicationsmenu_la_CFLAGS = \
 	$(XFCONF_CFLAGS) \
 	$(LIBXFCE4UTIL_CFLAGS) \
 	$(LIBXFCE4UI_CFLAGS) \
+	$(LIBXFCE4WINDOWING_CFLAGS) \
 	$(GARCON_CFLAGS) \
 	$(GARCON_GTK3_CFLAGS) \
 	$(PLATFORM_CFLAGS)
@@ -36,6 +37,7 @@ libapplicationsmenu_la_LIBADD = \
 	$(GTK_LIBS) \
 	$(LIBXFCE4UTIL_LIBS) \
 	$(LIBXFCE4UI_LIBS) \
+	$(LIBXFCE4WINDOWING_LIBS) \
 	$(GARCON_LIBS) \
 	$(GARCON_GTK3_LIBS) \
 	$(XFCONF_LIBS)

plugins/applicationsmenu/meson.build
@@ -22,6 +22,7 @@ shared_module(
     garcon_gtk3,
     libxfce4util,
     libxfce4ui,
+    libxfce4windowing,
     xfconf,
   ],
   link_with: [

commit 9c829ae45981274c26436268dd373ba97847ef44

wayland: Minimalist management of workspace group change when unused
Fixes: 1538d8b72bf7127cc299ac4d2e5f264c22d1896f
Related: xfce/libxfce4windowing#53

plugins/tasklist/tasklist-widget.c
@@ -1659,6 +1659,23 @@ xfce_tasklist_arrow_button_toggled (GtkWidget *button,
 
 
 static void
+workspace_group_destroyed (XfwWorkspaceManager *manager,
+                           XfwWorkspaceGroup *group,
+                           XfceTasklist *tasklist)
+{
+  if (group == tasklist->workspace_group)
+    {
+      tasklist->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+      g_signal_connect (G_OBJECT (tasklist->workspace_group), "active-workspace-changed",
+                        G_CALLBACK (xfce_tasklist_active_workspace_changed), tasklist);
+      g_signal_connect (G_OBJECT (tasklist->workspace_group), "viewports-changed",
+                        G_CALLBACK (xfce_tasklist_viewports_changed), tasklist);
+    }
+}
+
+
+
+static void
 xfce_tasklist_connect_screen (XfceTasklist *tasklist)
 {
   GList *windows, *li;
@@ -1678,8 +1695,10 @@ xfce_tasklist_connect_screen (XfceTasklist *tasklist)
   manager = xfw_screen_get_workspace_manager (tasklist->screen);
 
   /* window button visibility based on window<->workspace association only works on X11,
-   * where there is only one workspace group */
-  tasklist->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+   * where there is only one workspace group, but it can be destroyed on wayland, so let's
+   * manage this in a minimalist way */
+  g_signal_connect_object (manager, "workspace-group-destroyed", G_CALLBACK (workspace_group_destroyed), tasklist, 0);
+  workspace_group_destroyed (manager, NULL, tasklist);
 
   /* add all existing windows on this screen */
   windows = xfw_screen_get_windows (tasklist->screen);
@@ -1695,14 +1714,10 @@ xfce_tasklist_connect_screen (XfceTasklist *tasklist)
   xfce_tasklist_active_window_changed (tasklist->screen, NULL, tasklist);
   g_signal_connect (G_OBJECT (tasklist->screen), "active-window-changed",
                     G_CALLBACK (xfce_tasklist_active_window_changed), tasklist);
-  g_signal_connect (G_OBJECT (tasklist->workspace_group), "active-workspace-changed",
-                    G_CALLBACK (xfce_tasklist_active_workspace_changed), tasklist);
   g_signal_connect (G_OBJECT (tasklist->screen), "window-opened",
                     G_CALLBACK (xfce_tasklist_window_added), tasklist);
   g_signal_connect (G_OBJECT (tasklist->screen), "window-closed",
                     G_CALLBACK (xfce_tasklist_window_removed), tasklist);
-  g_signal_connect (G_OBJECT (tasklist->workspace_group), "viewports-changed",
-                    G_CALLBACK (xfce_tasklist_viewports_changed), tasklist);
 
   /* update the viewport if not all monitors are shown */
   if (!tasklist->all_monitors)

plugins/windowmenu/windowmenu.c
@@ -403,6 +403,17 @@ window_menu_plugin_style_updated (GtkWidget *widget)
 
 
 static void
+workspace_group_destroyed (XfwWorkspaceManager *manager,
+                           XfwWorkspaceGroup *group,
+                           WindowMenuPlugin *plugin)
+{
+  if (group == plugin->workspace_group)
+    plugin->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+}
+
+
+
+static void
 window_menu_plugin_screen_changed (GtkWidget *widget,
                                    GdkScreen *previous_screen)
 {
@@ -436,8 +447,10 @@ window_menu_plugin_screen_changed (GtkWidget *widget,
   plugin->screen = screen;
   manager = xfw_screen_get_workspace_manager (screen);
 
-  /* window<->workspace association only works on X11, where there is only one workspace group */
-  plugin->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+  /* window<->workspace association only works on X11, where there is only one workspace group,
+   * but it can be destroyed on wayland, so let's manage this in a minimalist way */
+  g_signal_connect_object (manager, "workspace-group-destroyed", G_CALLBACK (workspace_group_destroyed), plugin, 0);
+  workspace_group_destroyed (manager, NULL, plugin);
 
   /* connect signal to monitor this screen */
   g_signal_connect (G_OBJECT (plugin->screen), "active-window-changed",