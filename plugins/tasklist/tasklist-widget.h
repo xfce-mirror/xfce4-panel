98b384a6b10d014dac1b49138179adf2051264cc
m0ntana <m0ntanoid@gmail.com>
2025-10-29 13:08:12 -0500

    manually apply patch: tasklist-widget

--- plugins/tasklist/tasklist-widget.h
+++ plugins/tasklist/tasklist-widget.h
@@ -28,6 +28,113 @@ G_BEGIN_DECLS
 #define XFCE_TYPE_TASKLIST (xfce_tasklist_get_type ())
 G_DECLARE_FINAL_TYPE (XfceTasklist, xfce_tasklist, XFCE, TASKLIST, GtkContainer)
 
+struct _XfceTasklist
+{
+  GtkContainer __parent__;
+
+  /* lock counter */
+  gint locked;
+
+  /* the screen of this tasklist */
+  XfwScreen *screen;
+  XfwWorkspaceGroup *workspace_group;
+  GdkDisplay *display;
+
+  /* window children in the tasklist */
+  GList *windows;
+
+  /* windows we monitor, but that are excluded from the tasklist */
+  GSList *skipped_windows;
+
+  /* arrow button of the overflow menu */
+  GtkWidget *arrow_button;
+
+  /* applications of all the windows in the taskbar */
+  GHashTable *apps;
+
+  /* normal or iconbox style */
+  guint show_labels : 1;
+
+  /* size of the panel pluin */
+  gint size;
+
+  /* mode (orientation) of the tasklist */
+  XfcePanelPluginMode mode;
+
+  /* relief of the tasklist buttons */
+  GtkReliefStyle button_relief;
+
+  /* whether we show windows from all workspaces or
+   * only the active workspace */
+  guint all_workspaces : 1;
+
+  /* whether we switch to another workspace when we try to
+   * unminimize a window on another workspace */
+  guint switch_workspace : 1;
+
+  /* whether we only show monimized windows in the
+   * tasklist */
+  guint only_minimized : 1;
+
+  /* number of rows of window buttons */
+  gint nrows;
+
+  /* switch window with the mouse wheel */
+  guint window_scrolling : 1;
+  guint wrap_windows : 1;
+
+  /* whether we show blinking windows from all workspaces
+   * or only the active workspace */
+  guint all_blinking : 1;
+
+  /* action to preform when middle clicking */
+  XfceTasklistMClick middle_click;
+
+  /* whether decorate labels when window is not visible */
+  guint label_decorations : 1;
+
+  /* whether we only show windows that are in the geometry of
+   * the monitor the tasklist is on */
+  guint all_monitors : 1;
+  guint n_monitors;
+
+  /* whether we show wireframes when hovering a button in
+   * the tasklist */
+  guint show_wireframes : 1;
+
+  /* icon geometries update timeout */
+  guint update_icon_geometries_id;
+
+  /* idle monitor geometry update */
+  guint update_monitor_geometry_id;
+
+  /* button grouping */
+  guint grouping : 1;
+
+  /* sorting order of the buttons */
+  XfceTasklistSortOrder sort_order;
+
+  /* dummy properties */
+  guint show_handle : 1;
+  guint show_tooltips : 1;
+
+#ifdef ENABLE_X11
+  /* wireframe window */
+  Window wireframe_window;
+#endif
+
+  /* gtk style properties */
+  gint max_button_length;
+  gint min_button_length;
+  gint max_button_size;
+  PangoEllipsizeMode ellipsize_mode;
+  gint minimized_icon_lucency;
+  gint menu_max_width_chars;
+
+  gint n_windows;
+};
+
+
 void
 xfce_tasklist_set_mode (XfceTasklist *tasklist,
                         XfcePanelPluginMode mode);
