xfce-mirror/xfce4-panel

commit 8b43d158b1195e4f92ab77689c2da49338181f56

Implement a keep below option
For the people who don't want the panel to cover windows. Also, helpful
in wayland compositors who don't support wlr protocols to overlay other
panels that have the support, so it can look like one panel.

panel/panel-window.c
@@ -261,6 +261,7 @@ enum
   PROP_OUTPUT_NAME,
   PROP_POSITION,
   PROP_ENABLE_STRUTS,
+  PROP_KEEP_BELOW,
   PROP_DARK_MODE
 };
 
@@ -369,6 +370,7 @@ struct _PanelWindow
   StrutsEgde struts_edge;
   gulong struts[N_STRUTS];
   guint struts_enabled : 1;
+  guint keep_below : 1;
 
   /* dark mode */
   gboolean dark_mode;
@@ -577,6 +579,12 @@ panel_window_class_init (PanelWindowClass *klass)
                                                          TRUE,
                                                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
+  g_object_class_install_property (gobject_class,
+                                   PROP_KEEP_BELOW,
+                                   g_param_spec_boolean ("keep-below", NULL, NULL,
+                                                         FALSE,
+                                                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
   gtk_widget_class_install_style_property (gtkwidget_class,
                                            g_param_spec_uint ("popup-delay",
                                                               NULL,
@@ -638,6 +646,47 @@ panel_window_is_active_changed (PanelWindow *window)
 
 
 static void
+panel_window_keep_below (PanelWindow *window)
+{
+  XfconfChannel *channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
+  if (WINDOWING_IS_WAYLAND () && !xfconf_channel_get_bool (channel, "/force-all-internal", FALSE))
+    return;
+
+  gboolean should_keep_below = window->keep_below && window->autohide_behavior == AUTOHIDE_BEHAVIOR_NEVER;
+
+#ifdef HAVE_GTK_LAYER_SHELL
+  if (gtk_layer_is_supported ())
+    {
+      if (should_keep_below)
+        gtk_layer_set_layer (GTK_WINDOW (window), GTK_LAYER_SHELL_LAYER_BOTTOM);
+      else
+        gtk_layer_set_layer (GTK_WINDOW (window), GTK_LAYER_SHELL_LAYER_TOP);
+    }
+#endif
+
+  if (WINDOWING_IS_X11 ())
+    {
+      if (should_keep_below)
+        gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_UTILITY);
+      else
+        gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DOCK);
+
+      /* Set motif hint to only close, so that WM won't Minimize window on "Showing Desktop" */
+      gdk_window_set_functions (gtk_widget_get_window (GTK_WIDGET (window)), GDK_FUNC_CLOSE);
+
+      /* Send proper hints */
+      gtk_window_set_keep_below (GTK_WINDOW (window), should_keep_below);
+      gtk_window_set_skip_pager_hint (GTK_WINDOW (window), should_keep_below);
+      gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), should_keep_below);
+      gtk_window_stick (GTK_WINDOW (window));
+    }
+
+  panel_utils_widget_remap (GTK_WIDGET (window));
+}
+
+
+
+static void
 panel_window_init (PanelWindow *window)
 {
   window->id = -1;
@@ -648,6 +697,7 @@ panel_window_init (PanelWindow *window)
   window->xfw_active_window = NULL;
   window->struts_edge = STRUTS_EDGE_NONE;
   window->struts_enabled = TRUE;
+  window->keep_below = FALSE;
   window->mode = XFCE_PANEL_PLUGIN_MODE_HORIZONTAL;
   window->size = 48;
   window->icon_size = 0;
@@ -788,6 +838,10 @@ panel_window_get_property (GObject *object,
 
     case PROP_ENABLE_STRUTS:
       g_value_set_boolean (value, window->struts_enabled);
+      break;
+
+    case PROP_KEEP_BELOW:
+      g_value_set_boolean (value, window->keep_below);
       break;
 
     default:
@@ -988,6 +1042,11 @@ panel_window_set_property (GObject *object,
           window->struts_enabled = val_bool;
           panel_window_screen_layout_changed (window->screen, window);
         }
+      break;
+
+    case PROP_KEEP_BELOW:
+      window->keep_below = g_value_get_boolean (value);
+      panel_window_keep_below (window);
       break;
 
     default:
@@ -3518,6 +3577,8 @@ panel_window_set_autohide_behavior (PanelWindow *window,
 {
   GtkWidget *popup;
   guint i;
+  gboolean should_remap;
+
   const gchar *properties[] = { "enter-opacity", "leave-opacity",
                                 "borders", "background-style",
                                 "background-rgba",
@@ -3528,6 +3589,10 @@ panel_window_set_autohide_behavior (PanelWindow *window,
   /* do nothing if the behavior hasn't changed at all */
   if (window->autohide_behavior == behavior)
     return;
+
+  should_remap = window->keep_below
+                 && ((window->autohide_behavior == AUTOHIDE_BEHAVIOR_NEVER && behavior != AUTOHIDE_BEHAVIOR_NEVER)
+                     || (window->autohide_behavior != AUTOHIDE_BEHAVIOR_NEVER && behavior == AUTOHIDE_BEHAVIOR_NEVER));
 
   /* remember the new behavior */
   window->autohide_behavior = behavior;
@@ -3628,6 +3693,10 @@ panel_window_set_autohide_behavior (PanelWindow *window,
       gtk_widget_destroy (window->autohide_window);
       window->autohide_window = NULL;
     }
+
+  /* change stacking order if autohide changed */
+  if (should_remap)
+    panel_window_keep_below (window);
 }
 
 

common/resources/panel-preferences-dialog.glade
@@ -454,7 +454,7 @@
                             <property name="visible">True</property>
                             <property name="can-focus">True</property>
                             <property name="receives-default">False</property>
-                            <property name="tooltip-text" translatable="yes">Select this option if you don't want maximized windows to cover the area behind the panel. This only works when the panel is attached to a screen edge.</property>
+                            <property name="tooltip-text" translatable="yes">Select this option if you don't want maximized windows to overlap the panel area. This only works when the panel is attached to a screen edge and autohide is disabled.</property>
                             <property name="margin-left">12</property>
                             <property name="use-underline">True</property>
                             <property name="draw-indicator">True</property>
@@ -462,6 +462,23 @@
                           <packing>
                             <property name="left-attach">0</property>
                             <property name="top-attach">6</property>
+                            <property name="width">2</property>
+                          </packing>
+                        </child>
+                        <child>
+                          <object class="GtkCheckButton" id="keep-below">
+                            <property name="label" translatable="yes">_Keep panel below windows</property>
+                            <property name="visible">True</property>
+                            <property name="can-focus">True</property>
+                            <property name="receives-default">False</property>
+                            <property name="tooltip-text" translatable="yes">Select this option if you want windows to cover the panel. When autohide is enabled, panel is always on top. On wayland option is greyed out unless /force-all-internal property is set.</property>
+                            <property name="margin-left">12</property>
+                            <property name="use-underline">True</property>
+                            <property name="draw-indicator">True</property>
+                          </object>
+                          <packing>
+                            <property name="left-attach">0</property>
+                            <property name="top-attach">7</property>
                             <property name="width">2</property>
                           </packing>
                         </child>

panel/panel-application.c
@@ -296,6 +296,7 @@ panel_application_xfconf_window_bindings (PanelApplication *application,
     { "output-name", G_TYPE_STRING },
     { "position", G_TYPE_STRING },
     { "enable-struts", G_TYPE_BOOLEAN },
+    { "keep-below", G_TYPE_BOOLEAN },
     { NULL }
   };
   const PanelProperty global_properties[] = {

panel/panel-preferences-dialog.c
@@ -265,6 +265,16 @@ panel_preferences_dialog_init (PanelPreferencesDialog *dialog)
   g_free (path_new);
   connect_signal ("panel-switch", "clicked", panel_preferences_dialog_panel_switch);
 
+  /* general tab */
+  if (WINDOWING_IS_WAYLAND ())
+    {
+      XfconfChannel *channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
+      object = gtk_builder_get_object (GTK_BUILDER (dialog), "keep-below");
+      panel_return_if_fail (G_IS_OBJECT (object));
+      gtk_widget_set_sensitive (GTK_WIDGET (object),
+                                xfconf_channel_get_bool (channel, "/force-all-internal", FALSE));
+    }
+
   /* appearance tab */
   object = gtk_builder_get_object (GTK_BUILDER (dialog), "background-style");
   panel_return_if_fail (G_IS_OBJECT (object));
@@ -521,6 +531,7 @@ panel_preferences_dialog_bindings_update (PanelPreferencesDialog *dialog)
   panel_preferences_dialog_bindings_add (dialog, "position-locked", "active", 0);
   panel_preferences_dialog_bindings_add (dialog, "autohide-behavior", "active", 0);
   panel_preferences_dialog_bindings_add (dialog, "enable-struts", "active", 0);
+  panel_preferences_dialog_bindings_add (dialog, "keep-below", "active", 0);
   panel_preferences_dialog_bindings_add (dialog, "size", "value", 0);
   panel_preferences_dialog_bindings_add (dialog, "nrows", "value", 0);
   panel_preferences_dialog_bindings_add (dialog, "length-adjust", "active", 0);
@@ -727,20 +738,31 @@ static void
 panel_preferences_dialog_autohide_changed (GtkComboBox *combobox,
                                            PanelPreferencesDialog *dialog)
 {
-  GObject *object;
+  GObject *struts_object, *below_object;
 
   panel_return_if_fail (GTK_IS_COMBO_BOX (combobox));
   panel_return_if_fail (PANEL_IS_PREFERENCES_DIALOG (dialog));
   panel_return_if_fail (PANEL_WINDOW (dialog->active));
 
-  object = gtk_builder_get_object (GTK_BUILDER (dialog), "enable-struts");
-  panel_return_if_fail (GTK_IS_WIDGET (object));
+  struts_object = gtk_builder_get_object (GTK_BUILDER (dialog), "enable-struts");
+  panel_return_if_fail (GTK_IS_WIDGET (struts_object));
+
+  below_object = gtk_builder_get_object (GTK_BUILDER (dialog), "keep-below");
+  panel_return_if_fail (GTK_IS_WIDGET (struts_object));
 
   /* make "don't reserve space on borders" sensitive only when autohide is disabled */
   if (gtk_combo_box_get_active (combobox) == 0)
-    gtk_widget_set_sensitive (GTK_WIDGET (object), TRUE);
+    {
+      XfconfChannel *channel = xfconf_channel_get (XFCE_PANEL_CHANNEL_NAME);
+      gtk_widget_set_sensitive (GTK_WIDGET (struts_object), TRUE);
+      if (WINDOWING_IS_X11 () || xfconf_channel_get_bool (channel, "/force-all-internal", FALSE))
+        gtk_widget_set_sensitive (GTK_WIDGET (below_object), TRUE);
+    }
   else
-    gtk_widget_set_sensitive (GTK_WIDGET (object), FALSE);
+    {
+      gtk_widget_set_sensitive (GTK_WIDGET (struts_object), FALSE);
+      gtk_widget_set_sensitive (GTK_WIDGET (below_object), FALSE);
+    }
 }
 
 

commit dceb0c7051447a2c55ac4df95dea28dd0132c775

panel: Don't set GdkWMFunction if not realized
This causes a crash at startup if the panel is hidden.

Fixes: 8b43d158b1195e4f92ab77689c2da49338181f56

panel/panel-window.c
@@ -675,7 +675,15 @@ panel_window_keep_below (PanelWindow *window)
         gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DOCK);
 
       /* Set motif hint to only close, so that WM won't Minimize window on "Showing Desktop" */
-      gdk_window_set_functions (gtk_widget_get_window (GTK_WIDGET (window)), GDK_FUNC_CLOSE);
+      if (gtk_widget_get_realized (GTK_WIDGET (window)))
+        {
+          g_signal_handlers_disconnect_by_func (window, panel_window_keep_below, NULL);
+          gdk_window_set_functions (gtk_widget_get_window (GTK_WIDGET (window)), GDK_FUNC_CLOSE);
+        }
+      else
+        {
+          g_signal_connect (window, "realize", G_CALLBACK (panel_window_keep_below), NULL);
+        }
 
       /* Send proper hints */
       gtk_window_set_keep_below (GTK_WINDOW (window), should_keep_below);