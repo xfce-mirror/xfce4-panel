xfce-mirror/xfce4-panel

commit 9c829ae45981274c26436268dd373ba97847ef44

wayland: Minimalist management of workspace group change when unused
Fixes: 1538d8b72bf7127cc299ac4d2e5f264c22d1896f
Related: xfce/libxfce4windowing#53

plugins/tasklist/tasklist-widget.c
@@ -1659,6 +1659,23 @@ xfce_tasklist_arrow_button_toggled (GtkWidget *button,
 
 
 static void
+workspace_group_destroyed (XfwWorkspaceManager *manager,
+                           XfwWorkspaceGroup *group,
+                           XfceTasklist *tasklist)
+{
+  if (group == tasklist->workspace_group)
+    {
+      tasklist->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+      g_signal_connect (G_OBJECT (tasklist->workspace_group), "active-workspace-changed",
+                        G_CALLBACK (xfce_tasklist_active_workspace_changed), tasklist);
+      g_signal_connect (G_OBJECT (tasklist->workspace_group), "viewports-changed",
+                        G_CALLBACK (xfce_tasklist_viewports_changed), tasklist);
+    }
+}
+
+
+
+static void
 xfce_tasklist_connect_screen (XfceTasklist *tasklist)
 {
   GList *windows, *li;
@@ -1678,8 +1695,10 @@ xfce_tasklist_connect_screen (XfceTasklist *tasklist)
   manager = xfw_screen_get_workspace_manager (tasklist->screen);
 
   /* window button visibility based on window<->workspace association only works on X11,
-   * where there is only one workspace group */
-  tasklist->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+   * where there is only one workspace group, but it can be destroyed on wayland, so let's
+   * manage this in a minimalist way */
+  g_signal_connect_object (manager, "workspace-group-destroyed", G_CALLBACK (workspace_group_destroyed), tasklist, 0);
+  workspace_group_destroyed (manager, NULL, tasklist);
 
   /* add all existing windows on this screen */
   windows = xfw_screen_get_windows (tasklist->screen);
@@ -1695,14 +1714,10 @@ xfce_tasklist_connect_screen (XfceTasklist *tasklist)
   xfce_tasklist_active_window_changed (tasklist->screen, NULL, tasklist);
   g_signal_connect (G_OBJECT (tasklist->screen), "active-window-changed",
                     G_CALLBACK (xfce_tasklist_active_window_changed), tasklist);
-  g_signal_connect (G_OBJECT (tasklist->workspace_group), "active-workspace-changed",
-                    G_CALLBACK (xfce_tasklist_active_workspace_changed), tasklist);
   g_signal_connect (G_OBJECT (tasklist->screen), "window-opened",
                     G_CALLBACK (xfce_tasklist_window_added), tasklist);
   g_signal_connect (G_OBJECT (tasklist->screen), "window-closed",
                     G_CALLBACK (xfce_tasklist_window_removed), tasklist);
-  g_signal_connect (G_OBJECT (tasklist->workspace_group), "viewports-changed",
-                    G_CALLBACK (xfce_tasklist_viewports_changed), tasklist);
 
   /* update the viewport if not all monitors are shown */
   if (!tasklist->all_monitors)

plugins/windowmenu/windowmenu.c
@@ -403,6 +403,17 @@ window_menu_plugin_style_updated (GtkWidget *widget)
 
 
 static void
+workspace_group_destroyed (XfwWorkspaceManager *manager,
+                           XfwWorkspaceGroup *group,
+                           WindowMenuPlugin *plugin)
+{
+  if (group == plugin->workspace_group)
+    plugin->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+}
+
+
+
+static void
 window_menu_plugin_screen_changed (GtkWidget *widget,
                                    GdkScreen *previous_screen)
 {
@@ -436,8 +447,10 @@ window_menu_plugin_screen_changed (GtkWidget *widget,
   plugin->screen = screen;
   manager = xfw_screen_get_workspace_manager (screen);
 
-  /* window<->workspace association only works on X11, where there is only one workspace group */
-  plugin->workspace_group = xfw_workspace_manager_list_workspace_groups (manager)->data;
+  /* window<->workspace association only works on X11, where there is only one workspace group,
+   * but it can be destroyed on wayland, so let's manage this in a minimalist way */
+  g_signal_connect_object (manager, "workspace-group-destroyed", G_CALLBACK (workspace_group_destroyed), plugin, 0);
+  workspace_group_destroyed (manager, NULL, plugin);
 
   /* connect signal to monitor this screen */
   g_signal_connect (G_OBJECT (plugin->screen), "active-window-changed",

commit 29d1ea798f6e51ea12989b3637d43482543a3aef

plugins: Reset workspace group when needed
Fixes: 9c829ae45981274c26436268dd373ba97847ef44
Closes: #966

plugins/tasklist/tasklist-widget.c
@@ -1815,6 +1815,7 @@ xfce_tasklist_disconnect_screen (XfceTasklist *tasklist)
   panel_assert (tasklist->skipped_windows == NULL);
 
   g_clear_object (&tasklist->screen);
+  tasklist->workspace_group = NULL;
   tasklist->display = NULL;
 }
 

plugins/windowmenu/windowmenu.c
@@ -477,6 +477,7 @@ window_menu_plugin_screen_changed (GtkWidget *widget,
       g_signal_handlers_disconnect_by_func (G_OBJECT (plugin->screen),
                                             window_menu_plugin_active_window_changed, plugin);
       g_object_unref (plugin->screen);
+      plugin->workspace_group = NULL;
     }
 
   /* set the new screen */