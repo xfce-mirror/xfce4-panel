xfce-mirror/xfce4-panel

commit bf69d537db38769977767a7f982fb164c15d11ac

Fix string comparison when searching for plugin dirs
A '/' at the end of a path prevented the addition of a valid directory,
and as the comparison via g_strcmp0() was also reversed, the main
directory $prefix/share was not added if it was specified as
$prefix/share/.

Fixes: 894c6d9f2804092333c0134cebdf293cfefe98a0
Related: https://bbs.archlinux.org/viewtopic.php?id=306991

panel/panel-module-factory.c
@@ -215,18 +215,23 @@ panel_module_factory_load_modules (PanelModuleFactory *factory)
 
           for (const gchar *const *p = sys_datadirs; *p != NULL; p++)
             {
-              if (g_hash_table_add (unique, (gpointer) *p) && g_str_has_suffix (*p, datadir_suffix))
+              /* avoid trailing '/' or similar that could break string comparison below */
+              gchar *path = g_canonicalize_filename (*p, NULL);
+
+              if (g_hash_table_add (unique, (gpointer) path) && g_str_has_suffix (path, datadir_suffix))
                 {
-                  gsize prefix_len = g_strrstr_len (*p, -1, datadir_suffix) - *p;
-                  gchar *prefix = g_strndup (*p, prefix_len);
-                  gchar *datadir = g_strconcat (*p, plugin_dir_suffix, NULL);
+                  gsize prefix_len = g_strrstr_len (path, -1, datadir_suffix) - path;
+                  gchar *prefix = g_strndup (path, prefix_len);
+                  gchar *datadir = g_strconcat (path, plugin_dir_suffix, NULL);
                   gchar *libdir = g_strconcat (prefix, libdir_suffix, plugin_dir_suffix, NULL);
                   datadirs = g_list_prepend (datadirs, datadir);
                   libdirs = g_list_prepend (libdirs, libdir);
-                  if (!build_dirs_added && g_strcmp0 (prefix, PREFIX))
+                  if (!build_dirs_added && g_strcmp0 (prefix, PREFIX) == 0)
                     build_dirs_added = TRUE;
                   g_free (prefix);
                 }
+
+              g_free (path);
             }
 
           g_hash_table_destroy (unique);

commit 054b3f174be83ab04cf2be1d6fb2dd79caddab5b

panel: Fix wrong memory management
Fixes: bf69d537db38769977767a7f982fb164c15d11ac

panel/panel-module-factory.c
@@ -211,7 +211,7 @@ panel_module_factory_load_modules (PanelModuleFactory *factory)
         {
           const gchar *const *sys_datadirs = g_get_system_data_dirs ();
           const gchar *user_datadir = g_get_user_data_dir ();
-          GHashTable *unique = g_hash_table_new (g_str_hash, g_str_equal);
+          GHashTable *unique = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
 
           for (const gchar *const *p = sys_datadirs; *p != NULL; p++)
             {
@@ -230,8 +230,6 @@ panel_module_factory_load_modules (PanelModuleFactory *factory)
                     build_dirs_added = TRUE;
                   g_free (prefix);
                 }
-
-              g_free (path);
             }
 
           g_hash_table_destroy (unique);